8e598f828257b954cf9e1fd7fdb3bf27cb9af797
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-31 21:35:09 +0200

    Added primary and decoration colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,11 @@
-export const theme = {};
+export const theme = {
+    colors: {
+        green: "#57B660",
+        white: "#FFFFFF",
+        black: "#181414",
+        blueMoon: "#649AED",
+        redWine:"#EB5640",
+        mellowYellow:"#F6C874",
+        
+    }
+};
