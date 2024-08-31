d2ef95cdf69da091e51216ce397d682aa09eaeaf
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-31 22:21:09 +0200

    Declared Panel and NavItem styled-components

---
+++ src/components/SideBar/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Panel = styled.nav`
+    width: 320px;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.colors.black};
+`;
+
+export const NacItem = styled.a`
+
+`;
