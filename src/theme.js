5e55c0e994e39060c93821b88acacf8f1adb5599
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-02 16:29:29 +0200

    new colors added

--- src/theme.js
+++ src/theme.js
@@ -3,9 +3,11 @@ export const theme = {
         green: "#57B660",
         white: "#FFFFFF",
         black: "#181414",
+        codGray:"#090707",
         blueMoon: "#649AED",
-        redWine:"#EB5640",
-        mellowYellow:"#F6C874",
-        
-    }
+        redWine: "#EB5640",
+        mellowYellow: "#F6C874",
+        nobel: "#B3B3B3",
+        mineShaft: "#282828",
+    },
 };
