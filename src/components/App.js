993a99c76f668f2c669b830a76134c013ed30db2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-02 16:31:42 +0200

    created routes structure, moved it inside HashRouter, NavigationPanel are always display

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { NavigationPanel } from "./NavigationPanel";
+import { toAlbum, toHome, toSearch } from "../routes";
+import { Home } from "./features/Home";
+import { Search } from "./features/Search";
+import { Album } from "./features/Album";
+
+export const App = () => {
+  return (
+      <HashRouter>
+        <NavigationPanel />
+        <Routes>
+          <Route path={toHome()} element={<Home />} />
+          <Route path={toSearch()} element={<Search />} />
+          <Route path={toAlbum()} element={<Album />} />
+          <Route path="*" element={<Navigate to={toHome()} />} />
+        </Routes>
+      </HashRouter>
+  );
+};
