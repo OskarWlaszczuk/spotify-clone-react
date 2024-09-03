d8200135452298a0bcc88886dd969909740e817d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-03 06:43:20 +0200

    Removed unneccesery StyledIcons, and their imports, changed NavBox HTML element, removed unneccesery props, like underlined and thick

--- src/components/NavigationPanel/styled.js
+++ src/components/NavigationPanel/styled.js
@@ -1,30 +1,30 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { ReactComponent as SearchIcon } from "./icons/Search.svg";
 import { ReactComponent as HomeIcon } from "./icons/Home.svg";
 import { ReactComponent as MyLibraryIcon } from "./icons/MyLibrary.svg";
-import { ReactComponent as LikedIcon } from "./icons/Liked.svg";
-import { ReactComponent as AddIcon } from "./icons/Add.svg";
 
 export const NavPanel = styled.nav`
+    display: grid;
+    /* display: grid;
+    align-content: start;
+    grid-gap: 24px;
+    width: 230px;
+    padding: 33px 21px;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.black}; */
+`;
+
+export const NavFrame = styled.div`
     display: grid;
     align-content: start;
     grid-gap: 24px;
     width: 230px;
     padding: 33px 21px;
-    background-color: ${({ theme }) => theme.colors.codGray};
-`;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.black};
+`
 
-export const NavFrame = styled.ul`
-    margin: 0;
-    padding-left: 0;
-    list-style-type: none;
-
-    ${({ underlined }) => underlined && css`
-        border-bottom: 2px solid ${({ theme }) => theme.colors.mineShaft};
-    `};
-`;
-
-export const NavBox = styled.li`
+export const NavBox = styled.div`
     display: flex;
     align-items: center;
     gap: 18px;
@@ -35,10 +35,6 @@ export const NavItem = styled.a`
     color: ${({ theme }) => theme.colors.nobel};
     font-size: 13px;
     font-weight: bold;
-
-    ${({ thick }) => thick && css`
-      font-weight: normal;
-    `};
 `;
 
 export const IconContainer = styled.div`
@@ -49,14 +45,6 @@ export const IconContainer = styled.div`
     height: 24px;
     background: ${({ background }) => background};
     border-radius: 4px;
-`;
-
-export const StyledAddIcon = styled(AddIcon)`
-
-`;
-
-export const StyledLikedIcon = styled(LikedIcon)`
-
 `;
 
 export const StyledSearchIcon = styled(SearchIcon)`
