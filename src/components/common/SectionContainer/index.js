e50cfd16fe013436abef6dffc269ac569a437b4b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-03 08:00:42 +0200

    Declared generic SectionContainer styled-component, which wraps content of selected page section

---
+++ src/components/common/SectionContainer/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SectionContainer = styled.section`
+    padding: 15px 20px;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.black};
+`;
