b5332a9885941ee5caacc170a9829c02af333424
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-03 08:03:46 +0200

    Replaced NavFrame component, with generic IconBox component in NavigationPanel

--- src/components/NavigationPanel/index.js
+++ src/components/NavigationPanel/index.js
@@ -1,35 +1,29 @@
-import {
-    NavBox,
-    NavFrame,
-    NavItem,
-    NavPanel,
-    StyledHomeIcon,
-    StyledSearchIcon,
-} from "./styled";
+import { NavPanel } from "./styled";
 import { useNavigationToPage } from "../../useNavigationToPage";
 import { toHome, toSearch } from "../../routes";
+import { SectionContainer } from "../common/SectionContainer";
+import { IconBox } from "../common/IconBox";
+import { ReactComponent as HomeIcon } from "./icons/Home.svg";
+import { ReactComponent as SearchIcon } from "./icons/Search.svg";
 
 export const NavigationPanel = () => {
     const navigateToPage = useNavigationToPage();
 
     return (
-        <NavPanel>
-            <NavFrame>
-                <NavBox onClick={() => navigateToPage(toHome)}>
-                    <StyledHomeIcon />
-                    <NavItem>Home</NavItem>
-                </NavBox>
-                <NavBox onClick={() => navigateToPage(toSearch)}>
-                    <StyledSearchIcon />
-                    <NavItem>Search</NavItem>
-                </NavBox>
-            </NavFrame>
-            <NavFrame>
-            <NavBox onClick={() => navigateToPage(toHome)}>
-                    <StyledHomeIcon />
-                    <NavItem>Home</NavItem>
-                </NavBox>
-            </NavFrame>
-        </NavPanel>
+        <SectionContainer>
+            <NavPanel>
+                <IconBox
+                    navigateTo={() => navigateToPage(toHome)}
+                    Icon={HomeIcon}
+                    content="Home"
+                />
+                <IconBox
+                    navigateTo={() => navigateToPage(toSearch)}
+                    Icon={SearchIcon}
+                    content="Search"
+                    extraContent="+"
+                />
+            </NavPanel>
+        </SectionContainer>
     );
 };
