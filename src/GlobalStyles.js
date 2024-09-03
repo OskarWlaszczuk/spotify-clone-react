97db79205a99dcc89a7bb2924f30b130582497e0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-03 06:39:40 +0200

    Added padding and switched background-color for body

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -11,8 +11,9 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     margin: 0;
+    padding: 5px 10px;
     color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.codGray};
     font-family: "Roboto", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
