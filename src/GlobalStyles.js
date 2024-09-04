32b6be2f0788c3756da65c36bc532d585e883184
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-04 06:57:11 +0200

    Created grid-layout, using grid-areas, added height: 100vh; to take the whole available height on screen

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -10,11 +10,20 @@ export const GlobalStyles = createGlobalStyle`
   };
 
   body {
+    display: grid;
+    grid-template-rows: auto 1fr;
+    grid-template-areas: 
+      "nav main main main"
+      "aside main main main"
+      "aside main main main";
+    grid-gap: 8px;
     margin: 0;
+    height: 100vh;
     padding: 8px 10px;
+    word-break: break-word;
     color: ${({ theme }) => theme.colors.white};
     background-color: ${({ theme }) => theme.colors.codGray};
-    font-family: "Roboto", sans-serif;
+    font-family: "Lexend Deca", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   };
