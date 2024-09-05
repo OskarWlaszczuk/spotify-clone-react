1ad62b8a323b7de95673b30b8ed8f85bd7faa21e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-05 18:36:54 +0200

    Added Main component in Home, which gets as content TilesList components

--- src/components/features/Home/index.js
+++ src/components/features/Home/index.js
@@ -1,5 +1,31 @@
+import { toPopularList } from "../../../routes";
+import { useNavigationToPage } from "../../../useNavigationToPage";
+import { Main } from "../../common/Main"
+import { TilesList } from "../../common/TilesList";
+import { artists } from "../../common/TilesList/artists";
+
 export const Home = () => {
+    const navigateToPage = useNavigationToPage();
+
     return (
-        <></>
+        <Main
+            content={
+                <>
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular artists"
+                        list={artists}
+                        hideRestListPart
+                        artistsList
+                    />
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular albums"
+                        list={artists}
+                        hideRestListPart
+                    />
+                </>
+            }
+        />
     );
 };
