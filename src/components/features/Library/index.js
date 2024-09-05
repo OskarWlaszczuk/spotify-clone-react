2278eddd2e91d4c50386d499980d2221ba5111bb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-05 18:38:27 +0200

    Replaced test Library content, with common component, which are wrapped into AsidePanel

--- src/components/features/Library/index.js
+++ src/components/features/Library/index.js
@@ -1,5 +1,21 @@
+import { SectionContainer } from "../../common/SectionContainer";
+import { IconBoxList } from "../../common/IconBoxList";
+import { IconBox } from "../../common/IconBox";
+import { ReactComponent as MyLibrary } from "./icons/MyLibrary.svg";
+import { AsidePanel } from "./styled";
+
 export const Library = () => {
-    return (
-        <>Library</>
-    );
+  return (
+    <AsidePanel>
+      <SectionContainer>
+        <IconBoxList>
+          <IconBox
+            Icon={MyLibrary}
+            content="My library"
+            extraContent="+"
+          />
+        </IconBoxList>
+      </SectionContainer>
+    </AsidePanel>
+  );
 };
