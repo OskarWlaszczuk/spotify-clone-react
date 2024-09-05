fc303b8e223b4356f7be119e0c9c60a380cd1d5a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-05 18:34:44 +0200

    declared generic TilesList component, if 'hideRestListPart' prop was passed, it's displays only number of Tiles, that will fit into one row, the whole tiles list is available only if hideRestListPart is not passed to component

---
+++ src/components/common/TilesList/index.js
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import { Tile } from "../Tile";
+import { List, HeaderContent, Header, PopularListLink } from "./styled";
+
+export const TilesList = ({ header, hideRestListPart, toPopularList, artistsList, list }) => {
+    const [tilesPerRow, setTilesPerRow] = useState(0);
+    const containerRef = useRef(null);
+
+    const calculateTilesPerRow = () => {
+        const containerWidth = containerRef.current.offsetWidth;
+        const tileWidth = 150;
+
+        const effectiveTileWidth = tileWidth + 10;
+        const count = Math.floor(containerWidth / effectiveTileWidth);
+        setTilesPerRow(count);
+    };
+
+    useEffect(() => {
+        calculateTilesPerRow();
+        window.addEventListener('resize', calculateTilesPerRow);
+
+        return () => {
+            window.removeEventListener('resize', calculateTilesPerRow);
+        };
+    }, []);
+
+    return (
+        <>
+            <HeaderContent>
+                <Header as={hideRestListPart ? "h2" : "h1"}>{header}</Header>
+                {hideRestListPart && <PopularListLink onClick={toPopularList}>Pokaż więcej</PopularListLink>}
+            </HeaderContent>
+            <List ref={containerRef}>
+                {
+                    hideRestListPart ?
+                        list.slice(0, tilesPerRow).map(({ name, image, job }) => (
+                            <Tile
+                                useArtistLayout={artistsList}
+                                title={name}
+                                picture={image}
+                                subInfo={job}
+                            />
+                        )) :
+                        list.map(({ name, image, job }) => (
+                            <Tile
+                            useArtistLayout={artistsList}
+                                title={name}
+                                picture={image}
+                                subInfo={job}
+                            />
+                        ))
+                }
+            </List>
+        </>
+    );
+}
