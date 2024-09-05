468ce4d894955ea212376295ffc2eccef2d51f55
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-05 18:31:20 +0200

    artists array declared for tests, with example artists datas

---
+++ src/components/common/TilesList/artists.js
@@ -0,0 +1,82 @@
+export const artists = [
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+    {
+        name: "Taco Hemingway",
+        image: "https://images.genius.com/2908e12de84e4da9579953b64e929e0c.700x700x1.jpg",
+        job: "singer",
+    },
+];
