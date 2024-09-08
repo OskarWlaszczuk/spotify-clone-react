c837df3977149ca787f65b4029fa09edc2a7b64e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 09:05:35 +0200

    Dispaching fetchArtists action using useEffect

--- src/components/features/Home/index.js
+++ src/components/features/Home/index.js
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import { toPopularList } from "../../../routes";
 import { useNavigationToPage } from "../../../useNavigationToPage";
 import { Main } from "../../common/Main"
 import { TilesList } from "../../common/TilesList";
-import { artists } from "../../common/TilesList/artists";
+import { artistsList } from "../../common/TilesList/artists";
+import { fetchArtists, selectArtists, selectArtistsState, selectArtistsFetchStatus } from "../../../slices/artistsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAccessToken } from "../../../fetchAccessToken";
+import { success } from "../../../fetchStatuses";
 
 export const Home = () => {
     const navigateToPage = useNavigationToPage();
+    const dispatch = useDispatch();
+    const artistsFetchStatus = useSelector(selectArtistsFetchStatus);
+    const artists = useSelector(selectArtists);
+
+    console.log(artistsFetchStatus, artists);
+
+    if (artistsFetchStatus === success) {
+        console.log(artists)
+    }
+
+    useEffect(() => {
+        dispatch(fetchArtists());
+    }, [dispatch])
 
     return (
         <Main
@@ -14,14 +32,38 @@ export const Home = () => {
                     <TilesList
                         toPopularList={() => navigateToPage(toPopularList)}
                         header="Popular artists"
-                        list={artists}
+                        list={artistsList}
                         hideRestListPart
                         artistsList
                     />
                     <TilesList
                         toPopularList={() => navigateToPage(toPopularList)}
                         header="Popular albums"
-                        list={artists}
+                        list={artistsList}
+                        hideRestListPart
+                    />
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular albums"
+                        list={artistsList}
+                        hideRestListPart
+                    />
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular albums"
+                        list={artistsList}
+                        hideRestListPart
+                    />
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular albums"
+                        list={artistsList}
+                        hideRestListPart
+                    />
+                    <TilesList
+                        toPopularList={() => navigateToPage(toPopularList)}
+                        header="Popular albums"
+                        list={artistsList}
                         hideRestListPart
                     />
                 </>
