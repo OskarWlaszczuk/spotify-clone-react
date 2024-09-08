4dd9ce21b860754473c66b6aca901b89bc478e2e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 09:02:06 +0200

    temporally removed id from Container in Tile

--- src/components/common/Tile/index.js
+++ src/components/common/Tile/index.js
@@ -1,9 +1,8 @@
-import { nanoid } from "nanoid";
 import { Container, Picture, SubInfo, Title } from "./styled";
 
 export const Tile = ({ picture, title, subInfo, useArtistLayout }) => {
     return (
-        <Container id={nanoid}>
+        <Container>
             <Picture picture={picture} artistLayout={useArtistLayout} />
             <Title>{title}</Title>
             <SubInfo>{subInfo}</SubInfo>
