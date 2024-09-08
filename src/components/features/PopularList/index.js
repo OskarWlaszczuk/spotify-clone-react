aca62be35e976a90b81b5608d81c3bf387d8bbe3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 09:07:37 +0200

    in popularList, the 'list' prop gets artistsList

--- src/components/features/PopularList/index.js
+++ src/components/features/PopularList/index.js
@@ -1,17 +1,20 @@
 import { Main } from "../../common/Main";
 import { TilesList } from "../../common/TilesList";
-import { artists } from "../../common/TilesList/artists";
+import { artistsList } from "../../common/TilesList/artists";
 
 export const PopularList = () => {
+
     return (
-        <Main
-            content={
-                <TilesList
-                    header="Popular artists"
-                    list={artists}
-                    artistsList
-                />
-            }
-        />
+        <>
+            <Main
+                content={
+                    <TilesList
+                        header="Popular artists"
+                        list={artistsList}
+                        artistsList
+                    />
+                }
+            />
+        </>
     );
 };
