28507ed292eb20455531189227aa56fb779316cc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 18:26:57 +0200

    moved hidding/showing list elements in TilesList to Home component, added new prop extraContentText, which contains text to display for ExtraContent, renamed toPopularList prop to extraContentLink, passes new props - listContent, which contains Tiles, and ref

--- src/components/common/TilesList/index.js
+++ src/components/common/TilesList/index.js
@@ -1,57 +1,17 @@
-import { useEffect, useRef, useState } from "react";
-import { Tile } from "../Tile";
-import { List, TitleContent, Title, PopularListLink } from "./styled";
+import { forwardRef } from "react";
+import { List, TitleContent, Title, ExtraContent } from "./styled";
 
-export const TilesList = ({ header, hideRestListPart, toPopularList, artistsList, list, listType }) => {
-
-    const [tilesPerRow, setTilesPerRow] = useState(0);
-    const containerRef = useRef(null);
-
-    const calculateTilesPerRow = () => {
-        const containerWidth = containerRef.current.offsetWidth;
-        const tileWidth = 150;
-
-        const effectiveTileWidth = tileWidth + 10;
-        const count = Math.floor(containerWidth / effectiveTileWidth);
-        setTilesPerRow(count);
-    };
-
-    useEffect(() => {
-        calculateTilesPerRow();
-        window.addEventListener('resize', calculateTilesPerRow);
-
-        return () => {
-            window.removeEventListener('resize', calculateTilesPerRow);
-        };
-    }, []);
+export const TilesList = forwardRef(({ title, hideRestListPart, listContent, extraContentText, extraContentLink }, ref = null) => {
 
     return (
         <>
             <TitleContent>
-                {header && <Title as={hideRestListPart ? "h2" : "h1"}>{header}</Title>}
-                {hideRestListPart && <PopularListLink onClick={toPopularList}>Show more</PopularListLink>}
+                <Title as={hideRestListPart ? "h2" : "h1"}>{title}</Title>
+                {hideRestListPart && <ExtraContent onClick={extraContentLink}>{extraContentText}</ExtraContent>}
             </TitleContent>
-            <List ref={containerRef}>
-                {
-                    hideRestListPart ?
-                        list.slice(0, tilesPerRow).map(({ name, image, job }) => (
-                            <Tile
-                                useArtistLayout={artistsList}
-                                title={name}
-                                picture={image}
-                                subInfo={job}
-                            />
-                        )) :
-                        list.map(({ name, image, job }) => (
-                            <Tile
-                                useArtistLayout={artistsList}
-                                title={name}
-                                picture={image}
-                                subInfo={job}
-                            />
-                        ))
-                }
+            <List ref={ref}>
+                {listContent}
             </List>
         </>
     );
-}
+})
