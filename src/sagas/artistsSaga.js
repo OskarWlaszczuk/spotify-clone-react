7abfb4021da0119b85cbc6461cc15718971eeeaa
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 09:11:52 +0200

    managing artists API respone in asrtistsSaga

---
+++ src/sagas/artistsSaga.js
@@ -0,0 +1,18 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { fetchArtists, fetchArtistsError, fetchArtistsSuccess } from '../slices/artistsSlice';
+import { fetchApi } from '../fetchApi';
+// import { BASE_URL } from "../config";
+
+function* fetchArtistsHandler() {
+    try {
+        const response = yield call(fetchApi, "artists.json");
+        console.log(response)
+        yield put(fetchArtistsSuccess(response));
+    } catch {
+        yield put(fetchArtistsError());
+    }
+}
+
+export function* artistsSaga() {
+    yield takeLatest(fetchArtists.type, fetchArtistsHandler);
+}
