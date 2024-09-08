15bc666bdbc6b05b8d87879ab55ae72fb070e074
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 18:46:53 +0200

    added popularList reducer in store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { rootSaga } from './sagas/rootSaga';
 import { artistsReducer } from './slices/artistsSlice';
+import { popularListReducer } from './slices/popularListSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         artists: artistsReducer,
+        popularList: popularListReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
