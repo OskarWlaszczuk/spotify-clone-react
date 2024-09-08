e0075337f0a3e0405367d1343b2a066827b18d4f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 18:48:27 +0200

    selecting title from popularListSlice in PopularList

--- src/components/features/PopularList/index.js
+++ src/components/features/PopularList/index.js
@@ -1,15 +1,18 @@
+import { useSelector } from "react-redux";
 import { Main } from "../../common/Main";
 import { TilesList } from "../../common/TilesList";
 import { artistsList } from "../../common/TilesList/artists";
+import { selectPopularListTitle } from "../../../slices/popularListSlice";
 
 export const PopularList = () => {
+    const popularListTitle = useSelector(selectPopularListTitle);
 
     return (
         <>
             <Main
                 content={
                     <TilesList
-                        title="Popular artists"
+                        title={popularListTitle}
                         list={artistsList}
                         artistsList
                     />
