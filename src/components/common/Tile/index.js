95f0abd539330c7ef6aa5d12316d6a366e1fb535
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 18:21:08 +0200

    renamed useArtistLayout to useArtistPictureStyle, setting first letter of subInfo ro upper case automatically

--- src/components/common/Tile/index.js
+++ src/components/common/Tile/index.js
@@ -1,11 +1,11 @@
 import { Container, Picture, SubInfo, Title } from "./styled";
 
-export const Tile = ({ picture, title, subInfo, useArtistLayout }) => {
+export const Tile = ({ picture, title, subInfo, useArtistPictureStyle }) => {
     return (
         <Container>
-            <Picture picture={picture} artistLayout={useArtistLayout} />
+            <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
             <Title>{title}</Title>
-            <SubInfo>{subInfo}</SubInfo>
+            <SubInfo>{subInfo.slice(0, 1).toUpperCase() + subInfo.slice(1)}</SubInfo>
         </Container>
     );
 };
