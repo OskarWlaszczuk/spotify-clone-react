f72482df1cf0eb23f2fdd2877501793febdedba3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-08 09:01:14 +0200

    removed popularList reducer, added reducer of artists slice,created and  configured middlewar, with store, and run rootSaga

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { popularListReducer } from './slices/popularListSlice';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './sagas/rootSaga';
+import { artistsReducer } from './slices/artistsSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
-        popularList: popularListReducer,
+        artists: artistsReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
