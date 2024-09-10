03a64d47e5184761c4a29f4ed7084a25e93c7729
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-10 09:41:19 +0200

    connected albumsReducer, with store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import createSagaMiddleware from 'redux-saga';
 import { rootSaga } from './sagas/rootSaga';
 import { artistsReducer } from './slices/artistsSlice';
 import { popularListReducer } from './slices/popularListSlice';
+import { albumsReducer } from './slices/albumsSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         artists: artistsReducer,
+        albums: albumsReducer,
         popularList: popularListReducer,
     },
     middleware: (getDefaultMiddleware) =>
