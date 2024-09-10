3a694dc4f5a792600a07f749edb20735380dc1bc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-10 09:40:56 +0200

    renamed toPopularList function to popularListPathname, declared new toPopularList function, which gets navigate function, and its datas to set in location state

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ export const toHome = () => "/home";
 export const toArtist = () => "/artist";
 export const toSearch = () => "/search";
 export const toAlbum = () => "/album";
-export const toPopularList = () => "/popularList";
+export const popularListPathname = () => "/popularList";
+
+export const toPopularList = (navigate, datas) => {
+    navigate("/popularList", datas)
+};
