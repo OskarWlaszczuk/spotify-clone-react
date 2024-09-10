37bfa571e8c228730e722247db17da58ad4f4bed
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-10 09:45:04 +0200

    removed popular list reducers from store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { rootSaga } from './sagas/rootSaga';
 import { artistsReducer } from './slices/artistsSlice';
-import { popularListReducer } from './slices/popularListSlice';
 import { albumsReducer } from './slices/albumsSlice';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -11,7 +10,6 @@ export const store = configureStore({
     reducer: {
         artists: artistsReducer,
         albums: albumsReducer,
-        popularList: popularListReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
