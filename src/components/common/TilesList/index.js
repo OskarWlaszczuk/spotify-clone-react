eac3c7f7c4a41d0211c2d5c125ef1f99b6150ee3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-10 08:39:16 +0200

    moved back to TilesList logic of hidding/showing Tiles elemenets in the list, depending on available space in the row, removed forwardRef, and ref prop, moved selecting the header element above component, TilesList now gets new props - list and renderItem, delcared iterateOnList function which gets list prop in parameter and return its part

--- src/components/common/TilesList/index.js
+++ src/components/common/TilesList/index.js
@@ -1,17 +1,55 @@
-import { forwardRef } from "react";
-import { List, TitleContent, Title, ExtraContent } from "./styled";
+import { useEffect, useRef, useState } from "react";
+import { List, TitleContent, ExtraContent } from "./styled";
+import { useSelector } from "react-redux";
+import { selectArtistsFetchStatus } from "../../../slices/artistsSlice";
+import { success } from "../../../fetchStatuses";
 
-export const TilesList = forwardRef(({ title, hideRestListPart, listContent, extraContentText, extraContentLink }, ref = null) => {
+export const TilesList = ({ title, hideRestListPart, list, renderItem, extraContentText, extraContentLink }) => {
+    const artistsFetchStatus = useSelector(selectArtistsFetchStatus);
+
+    const [tilesPerRow, setTilesPerRow] = useState(0);
+    const containerRef = useRef(null);
+
+    const calculateTilesPerRow = () => {
+        const containerWidth = containerRef.current.offsetWidth;
+        const tileWidth = 150;
+
+        const effectiveTileWidth = tileWidth + 10;
+        const count = Math.floor(containerWidth / effectiveTileWidth);
+        setTilesPerRow(count);
+    };
+
+    useEffect(() => {
+        if (artistsFetchStatus === success) {
+            calculateTilesPerRow();
+            window.addEventListener('resize', calculateTilesPerRow);
+        }
+
+        return () => {
+            window.removeEventListener('resize', calculateTilesPerRow);
+        };
+    }, [artistsFetchStatus]);
+
+    const previewList = list.slice(0, tilesPerRow);
+    const wholeList = list;
+
+    const iterateOnList = list => {
+        return list.map((item) => (
+            renderItem(item)
+        ))
+    };
+
+    const headerElement = hideRestListPart ? <h2>{title}</h2> : <h1>{title}</h1>;
 
     return (
         <>
             <TitleContent>
-                <Title as={hideRestListPart ? "h2" : "h1"}>{title}</Title>
+                {headerElement}
                 {hideRestListPart && <ExtraContent onClick={extraContentLink}>{extraContentText}</ExtraContent>}
             </TitleContent>
-            <List ref={ref}>
-                {listContent}
+            <List ref={containerRef}>
+                {iterateOnList(hideRestListPart ? previewList : wholeList)}
             </List>
         </>
     );
-})
+};
