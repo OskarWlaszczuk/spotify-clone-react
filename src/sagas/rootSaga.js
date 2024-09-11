dfa82029da63d7a9040f27973f4ef1953d04c86f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-11 15:16:04 +0200

    merged artist saga to root saga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { artistsSaga } from "./artistsSaga";
 import { albumsSaga } from "./albumsSaga";
+import { artistSaga } from "./artistSaga";
 
 export function* rootSaga() {
     yield all([
         artistsSaga(),
-        albumsSaga()
+        albumsSaga(),
+        artistSaga(),
     ]);
 };
