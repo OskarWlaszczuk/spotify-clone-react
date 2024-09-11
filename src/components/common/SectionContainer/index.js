719480bdf2db2efe35139258d95a008da51bbe41
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-11 15:08:12 +0200

    added new props - main and bannerAvailable to SectionContainer

--- src/components/common/SectionContainer/index.js
+++ src/components/common/SectionContainer/index.js
@@ -1,8 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SectionContainer = styled.section`
     padding: 15px 20px;
     border-radius: 10px;
-    background-color: ${({ theme }) => theme.colors.black};
     height: 100%;
+    background-color: ${({theme}) => theme.colors.black};
+
+    ${({ main }) => main && css`
+        background: linear-gradient(0deg, rgba(24, 20, 20, 1) 52%, rgb(39 39 39) 100%);
+    `};
+
+    ${({ bannerAvailable }) => bannerAvailable && css`
+        border-radius: unset;
+    `};
 `;
