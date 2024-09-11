c276e943634d8fc024557fd1d8bef6bb9b4a8882
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-11 15:09:58 +0200

    Tile gets new props, id, with unique id for artist or album, and navigateTo, that are pasted to onClick attrubude, and contains route function

--- src/components/common/Tile/index.js
+++ src/components/common/Tile/index.js
@@ -1,8 +1,8 @@
 import { Container, Picture, SubInfo, Title } from "./styled";
 
-export const Tile = ({ picture, title, subInfo, useArtistPictureStyle }) => {
+export const Tile = ({ picture, title, subInfo, id, navigateTo, useArtistPictureStyle }) => {
     return (
-        <Container>
+        <Container id={id} onClick={navigateTo}>
             <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
             <Title>{title}</Title>
             <SubInfo>{subInfo.slice(0, 1).toUpperCase() + subInfo.slice(1)}</SubInfo>
