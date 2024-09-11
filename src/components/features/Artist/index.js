f3d0de024a369ce37ca14a125ca788453bd16b71
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-11 06:43:19 +0200

    Add Artist component with artist fetch and display test data using Redux

--- src/components/features/Artist/index.js
+++ src/components/features/Artist/index.js
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchArtist, selectArtist, selectArtistFetchStatus } from "../../../slices/artistSlice";
+import { success } from "../../../fetchStatuses";
+
 export const Artist = () => {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+
+    const fetchArtistStatus = useSelector(selectArtistFetchStatus);
+    const artist = useSelector(selectArtist);
+
+    useEffect(() => {
+        const fetchDelayId = setTimeout(() => {
+            dispatch(fetchArtist(id));
+        }, 500);
+
+        return () => clearTimeout(fetchDelayId);
+    }, [dispatch, id]);
+
     return (
-        <>Artist</>
+        <>
+            {
+                fetchArtistStatus === success && (
+                    <>{artist.name}</>
+                )
+            }
+        </>
     );
 };
