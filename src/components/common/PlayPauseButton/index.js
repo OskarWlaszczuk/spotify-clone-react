3a0b0867ad5a41c6a7f42d176fb04b092891a049
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-14 08:46:18 +0200

    created initial look of PlayPauseButton component

---
+++ src/components/common/PlayPauseButton/index.js
@@ -0,0 +1,9 @@
+import { Button, StyledPlayIcon } from "./styled"
+
+export const PlayPauseButton = () => {
+    return (
+        <Button>
+            <StyledPlayIcon />
+        </Button>
+    )
+}
