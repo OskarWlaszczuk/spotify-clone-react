283173cf8b727599d210a0bf8182655ec0749c05
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-14 10:14:39 +0200

    removed testing logs on console, removed unneccesery error parameter from carch

--- src/sagas/artistSaga.js
+++ src/sagas/artistSaga.js
@@ -6,14 +6,12 @@ import { BASE_URL } from "../config";
 function* fetchArtistHandler(action) {
     try {
         const response = yield call(fetchApi, `${BASE_URL}artists/${action.payload}`);
-        console.log(response)
         yield put(fetchArtistSuccess(response));
-    } catch (error) {
+    } catch {
         yield put(fetchArtistError());
-        console.log(error)
     }
-}
+};
 
 export function* artistSaga() {
     yield takeLatest(fetchArtist.type, fetchArtistHandler);
-}
+};
