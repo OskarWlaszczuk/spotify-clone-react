aafe4ec8ad2e170105978315d937a4c668e95605
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-14 10:01:36 +0200

    now Banner displays datas sent by props, removed destructering artist datas

--- src/components/common/Banner/index.js
+++ src/components/common/Banner/index.js
@@ -1,17 +1,14 @@
-import { useSelector } from "react-redux"
-import { selectArtist } from "../../../slices/artistSlice";
 import { Caption, Container, Details, MetaDatas, Picture, Title } from "./styled";
 
 export const Banner = ({ picture, metaDatas, title, caption }) => {
-    const { name, followers, images } = useSelector(selectArtist)
 
     return (
         <Container>
-            <Picture banner picture={images[0].url} artistPictureStyle={true} />
+            <Picture banner picture={picture} artistPictureStyle={true} />
             <Details>
-                <Caption>Verified artist</Caption>
-                <Title>{name}</Title>
-                <MetaDatas>{followers.total.toLocaleString()} followers</MetaDatas>
+                <Caption>{caption}</Caption>
+                <Title>{title}</Title>
+                <MetaDatas>{metaDatas} followers</MetaDatas>
             </Details>
         </Container>
     );
