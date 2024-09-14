bb4ea62c17eb2fa9fd193cbe64027cd0b3c753ca
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-14 08:43:40 +0200

    added artistTopTracks and artistAlbums reducers to store

--- src/store.js
+++ src/store.js
@@ -4,6 +4,8 @@ import { rootSaga } from './sagas/rootSaga';
 import { artistsReducer } from './slices/artistsSlice';
 import { albumsReducer } from './slices/albumsSlice';
 import { artistReducer } from './slices/artistSlice';
+import { artistTopTracksReducer } from './slices/artistTopTracksSlice';
+import { artistAlbumsReducer } from './slices/artistAlbumsSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -11,7 +13,9 @@ export const store = configureStore({
     reducer: {
         artists: artistsReducer,
         albums: albumsReducer,
-        artist: artistReducer
+        artist: artistReducer,
+        artistTopTracks: artistTopTracksReducer,
+        artistAlbums: artistAlbumsReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
