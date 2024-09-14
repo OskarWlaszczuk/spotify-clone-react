220ade46be4080ad804264c95c5e0a98a38de8ea
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-14 08:52:33 +0200

    code formatted in Tile

--- src/components/common/Tile/index.js
+++ src/components/common/Tile/index.js
@@ -2,10 +2,10 @@ import { Container, Picture, SubInfo, Title } from "./styled";
 
 export const Tile = ({ picture, title, subInfo, id, navigateTo, useArtistPictureStyle }) => {
     return (
-        <Container id={id} onClick={navigateTo}>
-            <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
-            <Title>{title}</Title>
-            <SubInfo>{subInfo.slice(0, 1).toUpperCase() + subInfo.slice(1)}</SubInfo>
-        </Container>
+            <Container id={id} onClick={navigateTo}>
+                <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
+                <Title>{title}</Title>
+                <SubInfo>{subInfo.slice(0, 1).toUpperCase() + subInfo.slice(1)}</SubInfo>
+            </Container>
     );
 };
