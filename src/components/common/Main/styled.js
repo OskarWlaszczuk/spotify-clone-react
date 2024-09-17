31d88d211cca987f0fd6d17d0403cf9d53e61a17
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-17 19:15:44 +0200

    added background-color to Main, set height to 100vh and width to 100% to fill whole available space

--- src/components/common/Main/styled.js
+++ src/components/common/Main/styled.js
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const MainContent = styled.main`
     grid-area: main;
+    background-color: ${({theme}) => theme.colors.black};
+    width: 100%;
+    height: 100vh;
 `;
