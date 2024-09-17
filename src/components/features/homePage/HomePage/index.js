8a2708e2f7b0d836a6c9276b0dc45ff4753b775c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-17 19:27:14 +0200

    depending on statemants, like isLoading, isSucces... dispalying specific content, selecting states from artists and albums slices

---
+++ src/components/features/homePage/HomePage/index.js
@@ -0,0 +1,88 @@
+import { useEffect } from "react";
+import { Main } from "../../../common/Main";
+import { useDispatch, useSelector } from "react-redux";
+import { error, initial, loading, success } from "../../../../fetchStatuses";
+import { albumsSelectors, albumsActions } from "../albums/albumsSlice";
+import { artistsSelectors, artistsActions } from "../artists/artistsSlice";
+import { checkFetchStatuses } from "../../../common/functions/checkFetchStatuses";
+import { TilesList } from "../../../common/TilesList";
+import { Tile } from "../../../common/Tile";
+
+export const HomePage = () => {
+
+    const dispatch = useDispatch();
+
+    const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
+    const { fetch: fetchArtists, clear: clearArtists } = artistsActions;
+
+    const albumsStatus = useSelector(albumsSelectors.selectStatus);
+    const artistsStatus = useSelector(artistsSelectors.selectStatus);
+
+    const albums = useSelector(albumsSelectors.selectDatas)?.datas.albums;
+    const artists = useSelector(artistsSelectors.selectDatas)?.datas.artists;
+
+    const isInitial = checkFetchStatuses([albumsStatus, artistsStatus], initial);
+    const isLoading = checkFetchStatuses([albumsStatus, artistsStatus], loading);
+    const isError = checkFetchStatuses([albumsStatus, artistsStatus], error);
+    const isSucces = checkFetchStatuses([albumsStatus, artistsStatus], success, true) && Boolean(artists) && Boolean(albums);
+
+    useEffect(() => {
+        const fetchDelayID = setTimeout(() => {
+            dispatch(fetchAlbums());
+            dispatch(fetchArtists());
+        }, 1000);
+
+        return () => {
+            clearTimeout(fetchDelayID);
+            clearAlbums();
+            clearArtists();
+        };
+
+    }, [dispatch, fetchAlbums, fetchArtists, clearAlbums, clearArtists]);
+
+    if (isLoading) return <Main content={<>loading</>} />;
+    if (isError) return <Main content={<>error</>} />;
+    if (isInitial) return <Main content={<>Initial</>} />;
+
+    if (isSucces) {
+        return (
+            <Main
+                gradientAvailable
+                content={
+                    <>
+                        <TilesList
+                            title="Popular albums"
+                            list={albums}
+                            renderItem={({ images, name, artists, id }) => (
+                                <Tile
+                                    id={id}
+                                    picture={images[0].url}
+                                    title={name}
+                                    subInfo={artists.map(({ name }) => name).join(",")}
+                                />
+                            )}
+                            hideRestListPart
+                            extraContentText="Show more"
+                        />
+                        <TilesList
+                            title="Popular artists"
+                            list={artists}
+                            renderItem={({ images, name, type, id }) => (
+                                <Tile
+                                    id={id}
+                                    picture={images[0].url}
+                                    title={name}
+                                    subInfo={type}
+                                    useArtistPictureStyle
+                                />
+                            )}
+                            hideRestListPart
+                            artistsList
+                            extraContentText="Show more"
+                        />
+                    </>
+                }
+            />
+        );
+    }
+};
