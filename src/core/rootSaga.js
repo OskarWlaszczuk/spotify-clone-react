af70dddd6385f0fd3160697e0f2ee624188dd183
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-20 08:50:54 +0200

    renamed artistAlbumssSaga to watchFetchArtistAlbums

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -3,7 +3,7 @@ import { watchFetchAsrtistsSaga } from "../features/homePage/artists/artistsSaga
 import { watchFetchAlbumsSaga } from "../features/homePage/albums/albumsSaga";
 import { artistSaga } from "../features/artistDetailsPage/artistDetails/artistSaga";
 import { artistTopTracksSaga } from "../features/artistDetailsPage/topTracks/artistTopTracksSaga";
-import { artistAlbumssSaga } from "../features/artistDetailsPage/albums/artistAlbumsSaga";
+import { watchFetchArtistAlbums } from "../features/artistDetailsPage/albums/artistAlbumsSaga";
 import { artistRelatedArtistsSaga } from "../features/artistDetailsPage/relatedArtists/relatedArtistsSaga";
 
 export function* rootSaga() {
@@ -12,7 +12,7 @@ export function* rootSaga() {
         watchFetchAlbumsSaga(),
         artistSaga(),
         artistTopTracksSaga(),
-        artistAlbumssSaga(),
+        watchFetchArtistAlbums(),
         artistRelatedArtistsSaga(),
     ]);
 };
