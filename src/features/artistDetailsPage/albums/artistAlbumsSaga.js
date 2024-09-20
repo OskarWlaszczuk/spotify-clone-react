1b94d689a8bd45596a4741b735aa04a10d991cfb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-20 08:51:24 +0200

    recreated saga for artist's albums using createSaga function

--- src/features/artistDetailsPage/albums/artistAlbumsSaga.js
+++ src/features/artistDetailsPage/albums/artistAlbumsSaga.js
@@ -1,21 +1,10 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { fetchFromAPI } from  "../../../common/functions/fetchFromAPI";
-import { BASE_URL } from  "../../../common/constants/config";
-import { actions } from './artistAlbumsSlice';
+import { getArtistAlbums } from "./getArtistAlbums";
+import { artistAlbumsActions } from "./artistAlbumsSlice";
+import { createSaga } from "../../../common/functions/createSaga";
 
-const { fetch, fetchSuccess, fetchError } = actions;
-
-function* fetchArtistAlbumsHandler({ payload }) {
-
-    try {
-        const response = yield call(fetchFromAPI, `${BASE_URL}artists/${payload}/albums?include_groups=album&limit=20`);
-
-        yield put(fetchSuccess(response));
-    } catch {
-        yield put(fetchError());
-    }
-};
-
-export function* artistAlbumssSaga() {
-    yield takeLatest(fetch.type, fetchArtistAlbumsHandler);
+export function* watchFetchArtistAlbums() {
+    yield createSaga({
+        getDatas: getArtistAlbums,
+        actions: artistAlbumsActions,
+    });
 };
