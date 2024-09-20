ae638dbf0bcd7af6f22b4dc56c22fcfd389cf06a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-20 16:45:56 +0200

    updated renamed sagas of artist's top tracks and related artists in rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -2,17 +2,17 @@ import { all } from "redux-saga/effects";
 import { watchFetchArtistsSaga } from "../features/homePage/artists/artistsSaga";
 import { watchFetchAlbumsSaga } from "../features/homePage/albums/albumsSaga";
 import { watchFetchArtistDetails } from "../features/artistDetailsPage/artistDetails/artistDetailsSaga";
-import { artistTopTracksSaga } from "../features/artistDetailsPage/topTracks/artistTopTracksSaga";
+import { watchFetchArtistTopTracks } from "../features/artistDetailsPage/topTracks/artistTopTracksSaga";
 import { watchFetchArtistAlbums } from "../features/artistDetailsPage/albums/artistAlbumsSaga";
-import { artistRelatedArtistsSaga } from "../features/artistDetailsPage/relatedArtists/relatedArtistsSaga";
+import { watchFetchRelatedArtists } from "../features/artistDetailsPage/relatedArtists/relatedArtistsSaga";
 
 export function* rootSaga() {
     yield all([
         watchFetchArtistsSaga(),
         watchFetchAlbumsSaga(),
         watchFetchArtistDetails(),
-        artistTopTracksSaga(),
+        watchFetchArtistTopTracks(),
         watchFetchArtistAlbums(),
-        artistRelatedArtistsSaga(),
+        watchFetchRelatedArtists(),
     ]);
 };
