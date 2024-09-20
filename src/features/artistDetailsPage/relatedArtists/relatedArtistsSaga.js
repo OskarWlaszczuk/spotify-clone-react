1ce05cfa4c03723e5e95a68a597612c89e6c7d76
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-20 16:52:25 +0200

    recreated related artist saga using createSaga function

--- src/features/artistDetailsPage/relatedArtists/relatedArtistsSaga.js
+++ src/features/artistDetailsPage/relatedArtists/relatedArtistsSaga.js
@@ -1,17 +1,10 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { fetchArtistRelatedArtists, fetchArtistRelatedArtistsError, fetchArtistRelatedArtistsSuccess } from './artistRelatedArtistsSlice';
-import { fetchFromAPI } from '../../../common/functions/fetchFromAPI';
-import { BASE_URL } from '../../../common/constants/config';
+import { relatedArtistsActions } from "./relatedArtistsSlice";
+import { createSaga } from "../../../common/functions/createSaga";
+import { getRelatedArtists } from "./getRelatedArtists";
 
-function* fetchArtistRelatedArtistsHandler(action) {
-    try {
-        const response = yield call(fetchFromAPI, `${BASE_URL}artists/${action.payload}/related-artists`);
-        yield put(fetchArtistRelatedArtistsSuccess(response));
-    } catch {
-        yield put(fetchArtistRelatedArtistsError());
-    }
-}
-
-export function* artistRelatedArtistsSaga() {
-    yield takeLatest(fetchArtistRelatedArtists.type, fetchArtistRelatedArtistsHandler);
-}
+export function* watchFetchRelatedArtists() {
+    yield createSaga({
+        getDatas: getRelatedArtists,
+        actions: relatedArtistsActions,
+    });
+};
