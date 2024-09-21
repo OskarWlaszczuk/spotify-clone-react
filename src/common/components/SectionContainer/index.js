1b53ffd1065bd5d6df120ded31d11df421961177
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-21 20:45:38 +0200

    changed gradient for SectionContainer

--- src/common/components/SectionContainer/index.js
+++ src/common/components/SectionContainer/index.js
@@ -3,10 +3,10 @@ import styled, { css } from "styled-components";
 export const SectionContainer = styled.section`
     padding: 15px 20px;
     border-radius: 10px;
-    background-color: ${({theme}) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.black};
 
     ${({ gradient }) => gradient && css`
-        background: linear-gradient(180deg, rgba(41,40,40,1) 0%, rgba(24,20,20,1) 17%);
+        background: linear-gradient(180deg, rgba(41,40,40,1) 0%, rgba(24,20,20,1) 6%);
     `};
 
     ${({ bannerAvailable }) => bannerAvailable && css`
