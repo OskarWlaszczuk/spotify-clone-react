7055626d2bc56cfcd9d5c3685cc34458f232de15
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-23 19:40:04 +0200

    declared dispatch function, returned by useDispatch hook, dispatching setNavigationID action after clicking the Tile

--- src/common/components/Tile/index.js
+++ src/common/components/Tile/index.js
@@ -1,11 +1,20 @@
+import { useDispatch } from "react-redux";
+import { capitalizeFirstLetter } from "../../functions/capitalizeFirstLetter";
 import { Container, Picture, SubInfo, Title } from "./styled";
+import { setNavigationID } from "../../../features/ListPage/listSlice";
 
 export const Tile = ({ picture, title, subInfo, id, navigateTo, useArtistPictureStyle }) => {
+
+    const dispatch = useDispatch();
+
     return (
-            <Container id={id} onClick={navigateTo}>
-                <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
-                <Title>{title}</Title>
-                <SubInfo>{subInfo.slice(0, 1).toUpperCase() + subInfo.slice(1)}</SubInfo>
-            </Container>
+        <Container onClick={() => {
+            navigateTo();
+            dispatch(setNavigationID({ id }))
+        }}>
+            <Picture picture={picture} artistPictureStyle={useArtistPictureStyle} />
+            <Title>{title}</Title>
+            <SubInfo>{capitalizeFirstLetter(subInfo)}</SubInfo>
+        </Container>
     );
 };
