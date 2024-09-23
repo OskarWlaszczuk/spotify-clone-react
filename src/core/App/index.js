b6bd58036cdf31f721fee522c9957a84a019f138
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-23 19:49:59 +0200

    renamed PopularList feature to ListPage

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,11 +1,11 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { NavigationPanel } from "../../core/App/NavigationPanel";
-import { toAlbum, toHome, popularListPathname, toSearch } from "../../common/functions/routes";
+import { toAlbum, toHome, toSearch, toListPage } from "../../common/functions/routes";
 import { HomePage } from "../../features/homePage/HomePage";
 import { Search } from "../../features/Search";
 import { Album } from "../../features/Album";
 import { Library } from "./Library";
-import { PopularList } from "../../features/PopularList";
+import { ListPage } from "../../features/ListPage";
 import { ArtistDetailsPage } from "../../features/artistDetailsPage/ArtistDetailsPage";
 
 export const App = () => {
@@ -18,7 +18,7 @@ export const App = () => {
         <Route path={toSearch()} element={<Search />} />
         <Route path={toAlbum()} element={<Album />} />
         <Route path="/artist/:id" element={<ArtistDetailsPage />} />
-        <Route path={popularListPathname()} element={<PopularList />} />
+        <Route path={toListPage()} element={<ListPage />} />
         <Route path="*" element={<Navigate to={toHome()} />} />
       </Routes>
     </HashRouter>
