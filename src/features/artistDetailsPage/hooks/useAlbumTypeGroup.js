0e2cc5b2e2cdc6e4ed89680f7307ca60ddcefdd9
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-24 11:54:37 +0200

    moved findMatchingGroup function inside of useAlbumTypeGroup hook since it's only used there, renamed albumTypeGroup state to currentGroupType, added matchedGroup to returning object

--- src/features/artistDetailsPage/hooks/useAlbumTypeGroup.js
+++ src/features/artistDetailsPage/hooks/useAlbumTypeGroup.js
@@ -1,7 +1,19 @@
 import { useState } from "react";
+import { popularReleasesGroup, albumsGroup, singlesGroup, compilationsGroup } from "../constants/groups";
+import { isAlbumGroupMatch } from "../functions/isAlbumGroupMatch";
 
-export const useAlbumTypeGroup = (newAlbumTypeGroup) => {
-    const [albumTypeGroup, setAlbumTypeGroup] = useState(newAlbumTypeGroup);
+export const useAlbumTypeGroup = (newAlbumTypeGroup, groups = {}) => {
+    const [currentGroupType, setCurrentGroupType] = useState(newAlbumTypeGroup);
+    const { albums, singles, compilations, sortedPopularReleasesWithNewestFirst } = groups;
 
-    return { albumTypeGroup, setAlbumTypeGroup };
+    const findMatchingGroup = () => {
+        if (isAlbumGroupMatch(albumsGroup, currentGroupType)) return { group: albums, title: "Albums" };
+        if (isAlbumGroupMatch(singlesGroup, currentGroupType)) return { group: singles, title: "Singles" };
+        if (isAlbumGroupMatch(compilationsGroup, currentGroupType)) return { group: compilations, title: "Compilations" };
+        if (isAlbumGroupMatch(popularReleasesGroup, currentGroupType)) return { group: sortedPopularReleasesWithNewestFirst, title: "Popular releases" };
+    };
+
+    const matchedGroup = findMatchingGroup();
+
+    return { matchedGroup, currentGroupType, setCurrentGroupType };
 };
