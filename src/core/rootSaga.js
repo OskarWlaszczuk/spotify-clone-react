c65c30fbcab000b6ba6d74a3bf95b8b9f44f0869
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-24 09:13:44 +0200

    moved all sagas of artistDetailsPage feature into one seperated folder 'sagas'

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,12 +1,12 @@
 import { all } from "redux-saga/effects";
 import { watchFetchArtistsSaga } from "../features/homePage/artists/artistsSaga";
 import { watchFetchAlbumsSaga } from "../features/homePage/albums/albumsSaga";
-import { watchFetchArtistDetails } from "../features/artistDetailsPage/artistDetails/artistDetailsSaga";
-import { watchFetchArtistTopTracks } from "../features/artistDetailsPage/topTracks/artistTopTracksSaga";
-import { watchFetchArtistAlbums } from "../features/artistDetailsPage/albums/artistAlbumsSaga";
-import { watchFetchRelatedArtists } from "../features/artistDetailsPage/relatedArtists/relatedArtistsSaga";
-import { watchFetchArtistSinglesSaga } from "../features/artistDetailsPage/artistSingles/artistSinglesSaga";
-import { watchFetchArtistCompilationSaga } from "../features/artistDetailsPage/artistCompilation/artistCompilationSaga";
+import { watchFetchArtistDetails } from "../features/artistDetailsPage/sagas/artistDetailsSaga";
+import { watchFetchArtistTopTracks } from "../features/artistDetailsPage/sagas/artistTopTracksSaga";
+import { watchFetchArtistAlbums } from "../features/artistDetailsPage/sagas/artistAlbumsSaga";
+import { watchFetchRelatedArtists } from "../features/artistDetailsPage/sagas/relatedArtistsSaga";
+import { watchFetchArtistSinglesSaga } from "../features/artistDetailsPage/sagas/artistSinglesSaga";
+import { watchFetchArtistCompilationSaga } from "../features/artistDetailsPage/sagas/artistCompilationSaga";
 
 export function* rootSaga() {
     yield all([
