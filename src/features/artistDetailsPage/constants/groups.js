833d901560ca1349e1de3c7b803fe5de08c3c236
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-24 11:47:54 +0200

    moved constants with groups to seperated file in 'constants' folder

---
+++ src/features/artistDetailsPage/constants/groups.js
@@ -0,0 +1,4 @@
+export const popularReleasesGroup = "popularReleases";
+export const albumsGroup = "albums";
+export const singlesGroup = "singles";
+export const compilationsGroup = "compilations";
