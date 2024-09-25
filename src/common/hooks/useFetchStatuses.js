0ae23b33c4a257d5f046b250f050404db006405c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-25 07:32:54 +0200

    removed datas parameter and areAllDatasExists function return, it is unneccesery to check are the passed datas existing, checking the fetchStatuses is enough

--- src/common/hooks/useFetchStatuses.js
+++ src/common/hooks/useFetchStatuses.js
@@ -1,12 +1,11 @@
 import { initial, loading, success, error } from "../constants/fetchStatuses";
-import { areAllDatasExists } from "../functions/areAllDatasExists";
 import { checkFetchStatuses } from "../functions/checkFetchStatuses";
 
-export const useFetchStatuses = (fetchStatuses = [], datas = []) => {
+export const useFetchStatuses = (fetchStatuses = []) => {
     const isInitial = checkFetchStatuses(fetchStatuses, initial);
     const isLoading = checkFetchStatuses(fetchStatuses, loading);
     const isError = checkFetchStatuses(fetchStatuses, error);
-    const isSucces = (checkFetchStatuses(fetchStatuses, success, true) && areAllDatasExists(datas));
+    const isSucces = (checkFetchStatuses(fetchStatuses, success, true));
 
     return { isInitial, isLoading, isError, isSucces };
 };
