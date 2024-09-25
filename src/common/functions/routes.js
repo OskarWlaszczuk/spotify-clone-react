5402e30b401b4e4f591cd3370f44401338f7627a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-25 07:30:12 +0200

    toArtist now gets new parameter 'additionalPath' to add additional path information in url, by default additionalPath is empty

--- src/common/functions/routes.js
+++ src/common/functions/routes.js
@@ -3,7 +3,7 @@ export const toSearch = () => "/search";
 export const toAlbum = () => "/album";
 export const toListPage = () => "/listPage";
 
-export const toArtist = ({ id } = { id: "id" }) => `/artist/${id}`;
+export const toArtist = ({ id, additionalPath = '' } = { id: 'id', additionalPath: '' }) => `/artist/${id}${additionalPath}`;
 
 // export const toListPage = (navigate, datas) => {
 //     navigate("/listPage", datas)
