872bb7d4da19f211fe02528627ecb4807ea7d61b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-26 15:39:25 +0200

    removed styles for $gradient and $bannerAvailable props from SectionContainer

--- src/common/components/SectionContainer/index.js
+++ src/common/components/SectionContainer/index.js
@@ -1,15 +1,7 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const SectionContainer = styled.section`
-    padding: 15px 20px;
     border-radius: 10px;
     background-color: ${({ theme }) => theme.colors.black};
-
-    ${({ $gradient }) =>$gradient && css`
-        background: linear-gradient(180deg, rgba(41,40,40,1) 0%, rgba(24,20,20,1) 6%);
-    `};
-
-    ${({ $bannerAvailable }) => $bannerAvailable && css`
-        border-radius: unset;
-    `};
+    padding: 15px 20px;
 `;
