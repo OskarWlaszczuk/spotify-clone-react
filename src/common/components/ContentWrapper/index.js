47a2fb741a63369617d0a3b2ec8ddf45e0d64f75
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-26 15:38:03 +0200

    created ContentWrapper generic styled-component to wrap main content, except Banner component

---
+++ src/common/components/ContentWrapper/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.section`
+    padding: 20px 15px;
+    /* background: linear-gradient(180deg, rgba(41,40,40,1) 0%, rgba(24,20,20,1) 5%); */
+`;
