c0369918b26b9065cb17ef339e6e956598522db3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-26 07:56:34 +0200

    replaced extraContentLink props with navigateTo

--- src/features/homePage/HomePage/MainContent/index.js
+++ src/features/homePage/HomePage/MainContent/index.js
@@ -1,75 +1,75 @@
-// import { useNavigate } from "react-router-dom"
-// import { toArtist, toPopularList } from "../../../../../routes"
-// import { TilesList } from "../../../../common/TilesList"
-// import { useSelector } from "react-redux";
-// import { Tile } from "../../../../common/Tile";
-// import { albumsSelectors } from "../../../homePage/albums/albumsSlice";
-// import { artistsSelectors } from "../../artists/artistsSlice";
+import { useNavigate } from "react-router-dom"
+import { toArtist, toPopularList } from "../../../../../routes"
+import { TilesList } from "../../../../common/TilesList"
+import { useSelector } from "react-redux";
+import { Tile } from "../../../../common/Tile";
+import { albumsSelectors } from "../../../homePage/albums/albumsSlice";
+import { artistsSelectors } from "../../artists/artistsSlice";
 
-// export const MainContent = () => {
-//     const navigate = useNavigate();
+export const MainContent = () => {
+    const navigate = useNavigate();
 
-//     const albums = useSelector(albumsSelectors.selectDatas)?.datas.albums;
-//     const artists = useSelector(artistsSelectors.selectDatas)?.datas.artists;
+    const albums = useSelector(albumsSelectors.selectDatas)?.datas.albums;
+    const artists = useSelector(artistsSelectors.selectDatas)?.datas.artists;
 
-//     console.log(albums, artists)
-//     return (
-//         <>
-//             {
-//                 (albums && artists) && (
-//                     <>
+    console.log(albums, artists)
+    return (
+        <>
+            {
+                (albums && artists) && (
+                    <>
 
-//                         <TilesList
-//                             title="Popular albums"
-//                             list={albums}
-//                             renderItem={
-//                                 (({ images, name, artists, id }) => (
-//                                     <Tile
-//                                         id={id}
-//                                         picture={images[0].url}
-//                                         title={name}
-//                                         subInfo={artists.map(({ name }) => name).join(",")}
-//                                     />
-//                                 ))
-//                             }
-//                             hideRestListPart
-//                             extraContentText="Show more"
-//                             extraContentLink={() => toPopularList(navigate, {
-//                                 state: {
-//                                     title: "Popular albums",
-//                                     list: albums,
-//                                     isArtistsList: false,
-//                                 }
-//                             })}
-//                         />
+                        <TilesList
+                            title="Popular albums"
+                            list={albums}
+                            renderItem={
+                                (({ images, name, artists, id }) => (
+                                    <Tile
+                                        id={id}
+                                        picture={images[0].url}
+                                        title={name}
+                                        subInfo={artists.map(({ name }) => name).join(",")}
+                                    />
+                                ))
+                            }
+                            hideRestListPart
+                            extraContentText="Show more"
+                            navigateTo={() => toPopularList(navigate, {
+                                state: {
+                                    title: "Popular albums",
+                                    list: albums,
+                                    isArtistsList: false,
+                                }
+                            })}
+                        />
 
-//                         <TilesList
-//                             title="Popular artists"
-//                             list={artists}
-//                             renderItem={({ images, name, type, id }) => (
-//                                 <Tile
-//                                     id={id}
-//                                     picture={images[0].url}
-//                                     title={name}
-//                                     subInfo={type}
-//                                     useArtistPictureStyle
-//                                     navigateTo={() => navigate(toArtist({ id }))}
-//                                 />
-//                             )}
-//                             hideRestListPart
-//                             artistsList
-//                             extraContentText="Show more"
-//                             extraContentLink={() => toPopularList(navigate, {
-//                                 state: {
-//                                     title: "Popular artists",
-//                                     list: artists,
-//                                     isArtistsList: true,
-//                                 }
-//                             })}
-//                         />
-//                     </>
-//                 )
-//             }
-//         </>
-//     )
-// }
+                        <TilesList
+                            title="Popular artists"
+                            list={artists}
+                            renderItem={({ images, name, type, id }) => (
+                                <Tile
+                                    id={id}
+                                    picture={images[0].url}
+                                    title={name}
+                                    subInfo={type}
+                                    useArtistPictureStyle
+                                    navigateTo={() => navigate(toArtist({ id }))}
+                                />
+                            )}
+                            hideRestListPart
+                            artistsList
+                            extraContentText="Show more"
+                            navigateTo={() => toPopularList(navigate, {
+                                state: {
+                                    title: "Popular artists",
+                                    list: artists,
+                                    isArtistsList: true,
+                                }
+                            })}
+                        />
+                    </>
+                )
+            }
+        </>
+    )
+}
