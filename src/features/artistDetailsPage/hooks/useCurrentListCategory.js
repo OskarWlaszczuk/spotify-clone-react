62bb682b4a35c98f29b8f270c5b7e66b2e5ac448
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-27 19:08:57 +0200

    passing to findMatchingValueByKey generic list params instead of array with specific object's values in useCurrentListCategory

--- src/features/artistDetailsPage/hooks/useCurrentListCategory.js
+++ src/features/artistDetailsPage/hooks/useCurrentListCategory.js
@@ -1,18 +1,11 @@
 import { useState } from "react";
-import { popularReleasesCategory, albumsCategory, singlesCategory, compilationsCategory } from "../constants/categories";
 import { findMatchingValueByKey } from "../../../common/functions/findMatchingValueByKey";
 
-export const useCurrentListCategory = (initialListCategory, param, availableLists = {}) => {
+export const useCurrentListCategory = (initialListCategory, list) => {
 
     const [currentListCategory, setCurrentListCategory] = useState(initialListCategory);
-    const { albums, singles, compilations, previewAllCategoriesList, allCategoriesList } = availableLists;
 
-    const listMachtedByCategory = findMatchingValueByKey([
-        { key: albumsCategory, value: albums },
-        { key: singlesCategory, value: singles },
-        { key: compilationsCategory, value: compilations },
-        { key: popularReleasesCategory, value: param ? previewAllCategoriesList : allCategoriesList },
-    ], currentListCategory);
+    const listMachtedByCategory = findMatchingValueByKey(list, currentListCategory);
 
     return { listMachtedByCategory, currentListCategory, setCurrentListCategory };
 };
