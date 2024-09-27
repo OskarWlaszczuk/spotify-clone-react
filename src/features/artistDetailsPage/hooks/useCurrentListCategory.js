090763cb8034725cd67de728b61affbbf51d0d4d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-27 18:34:20 +0200

    renamed matchedListByCategory to listMachtedByCategory, updated renamed arrays

--- src/features/artistDetailsPage/hooks/useCurrentListCategory.js
+++ src/features/artistDetailsPage/hooks/useCurrentListCategory.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { popularReleasesCategory, albumsCategory, singlesCategory, compilationsCategory } from "../constants/categories";
-import { findMatchingItemValue } from "../../../common/functions/findMatchingItemValue";
+import { findMatchingValueByKey } from "../../../common/functions/findMatchingValueByKey";
 
 export const useCurrentListCategory = (initialListCategory, param, availableLists = {}) => {
-    const [currentListCategory, setCurrentListCategory] = useState(initialListCategory);
-    const { albums, singles, compilations, mergedArray, mergedArrayWithNewestFirst } = availableLists;
 
-    const matchedListByCategory = findMatchingItemValue([
+    const [currentListCategory, setCurrentListCategory] = useState(initialListCategory);
+    const { albums, singles, compilations, previewAllCategoriesList, allCategoriesList } = availableLists;
+
+    const listMachtedByCategory = findMatchingValueByKey([
         { key: albumsCategory, value: albums },
         { key: singlesCategory, value: singles },
         { key: compilationsCategory, value: compilations },
-        { key: popularReleasesCategory, value: param ? mergedArrayWithNewestFirst : mergedArray },
+        { key: popularReleasesCategory, value: param ? previewAllCategoriesList : allCategoriesList },
     ], currentListCategory);
 
-    return { matchedListByCategory, currentListCategory, setCurrentListCategory };
+    return { listMachtedByCategory, currentListCategory, setCurrentListCategory };
 };
