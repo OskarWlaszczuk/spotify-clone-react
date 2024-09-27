941b78c5a6ec9c23703fb14391e4a8905f75dfe7
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-27 19:10:30 +0200

    moved paramCategories constants to seperated file called'paramCategories'

---
+++ src/features/artistDetailsPage/constants/paramCategories.js
@@ -0,0 +1,4 @@
+export const allParamCategory = "/all";
+export const albumsParamCategory = "/album";
+export const singleParamCategory = "/single";
+export const compilationParamCategory = "/compilation";
