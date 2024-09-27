635b8b43c5e1c74e17e9ec127fe6f773fa81d893
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-27 08:10:59 +0200

    code formatted in removeDuplicates

--- src/features/artistDetailsPage/functions/removeDuplicates.js
+++ src/features/artistDetailsPage/functions/removeDuplicates.js
@@ -1,5 +1,6 @@
 export const removeDuplicates = (albums = []) => {
     const caughtDuplicates = new Set();
+    
     return albums.filter(({ name }) => {
         const keyValue = name;
         return !caughtDuplicates.has(keyValue) && caughtDuplicates.add(keyValue);
