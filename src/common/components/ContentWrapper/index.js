3c52505838381476410a2265a09b8e692df158d0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-28 11:01:54 +0200

    deleted ContentWrapper component created for tests

--- src/common/components/ContentWrapper/index.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const ContentWrapper = styled.section`
-    padding: 20px 15px;
-    /* background: linear-gradient(180deg, rgba(41,40,40,1) 0%, rgba(24,20,20,1) 5%); */
-`;
