a51deca7adfdd4d8132db7ad00c49ed143e9000f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-28 11:14:06 +0200

    created common ToggleViewButton styled-component which is used for example for hidding/showing some part of artist's top tracks list

---
+++ src/common/components/ToggleViewButton/index.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ToggleViewButton = styled.button`
+    color: ${({ theme }) => theme.colors.nobel};
+    font-size: 13px;
+    font-weight: 600;
+    background-color: transparent;
+    border: none;
+
+    &:hover{
+        filter: brightness(115%);
+    };
+`;
