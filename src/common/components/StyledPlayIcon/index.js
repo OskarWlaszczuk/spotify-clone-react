02ac9f60191360c095449e13a51a4e5a37780a35
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-09-28 11:10:03 +0200

    created seperated styled-component for StyledPlayIcon

---
+++ src/common/components/StyledPlayIcon/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { ReactComponent as PlayIcon } from "../../icons/Play.svg";
+
+export const StyledPlayIcon = styled(PlayIcon)`
+    width: 14px; 
+    height: 14px; 
+    fill: white; 
+`;
