bf9be9fd53c8db74c47d85b0df0321902d756859
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 19:31:16 +0200

    calculating and updating amount of  tiles per row logic moved to useTilesPerRow custom hook

---
+++ src/features/artistDetailsPage/hooks/useTilesPerRow.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useTilesPerRow = () => {
+
+    const [tilesPerRow, setTilesPerRow] = useState(0);
+    const containerRef = useRef(null);
+
+    const calculateTilesPerRow = () => {
+        const containerWidth = containerRef.current.offsetWidth;
+        const tileWidth = 150;
+
+        const effectiveTileWidth = tileWidth + 10;
+        const count = Math.floor(containerWidth / effectiveTileWidth);
+        setTilesPerRow(count);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', calculateTilesPerRow);
+        calculateTilesPerRow();
+
+        return () => {
+            window.removeEventListener('resize', calculateTilesPerRow);
+        };
+    });
+
+    return { tilesPerRow, containerRef };
+};
