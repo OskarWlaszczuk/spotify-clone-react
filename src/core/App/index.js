fe9e9fd1222777bc29cd36c061b398910c0fec4e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 19:19:10 +0200

    removed Route with ListPage component in App

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,11 +1,10 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { NavigationPanel } from "../../core/App/NavigationPanel";
-import { toAlbum, toHome, toSearch, toListPage } from "../../common/functions/routes";
+import { toAlbum, toHome, toSearch } from "../../common/functions/routes";
 import { HomePage } from "../../features/homePage/HomePage";
 import { Search } from "../../features/Search";
 import { Album } from "../../features/Album";
 import { Library } from "./Library";
-import { ListPage } from "../../features/ListPage";
 import { ArtistDetailsPage } from "../../features/artistDetailsPage/components/ArtistDetailsPage";
 
 export const App = () => {
@@ -18,7 +17,6 @@ export const App = () => {
         <Route path={toSearch()} element={<Search />} />
         <Route path={toAlbum()} element={<Album />} />
         <Route path="/artist/:id/:type?" element={<ArtistDetailsPage />} />
-        <Route path={toListPage()} element={<ListPage />} />
         <Route path="*" element={<Navigate to={toHome()} />} />
       </Routes>
     </HashRouter>
