f2a2faf4b1fc7c75cf8ebaaf57f122d11e8e94ed
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 22:28:12 +0200

    deleted unneccesery hook's files

--- src/common/hooks/useNavigationToPage.js
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from "react-router-dom"
-
-export const useNavigationToPage = () => {
-    const navigate = useNavigate();
-
-    const navigateToPage = (toPage, param = "") => navigate(toPage(param));
-    return navigateToPage;
-};
