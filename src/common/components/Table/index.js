224f41291ecb423dd7242c9441a813a24310523e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 19:12:15 +0200

    wrapped table content into tbody element, removed TrackStats, removed popularity table data, replaced TrackStat styled-component  of duration table data with TrackDuration styled-component, added title attribude to RowHeader styled-component, with track's informarmations

--- src/common/components/Table/index.js
+++ src/common/components/Table/index.js
@@ -1,7 +1,8 @@
-import { Image, Row, StyledTable, Caption, TrackOverview, TrackStats, RowHeader, TrackName, TrackStat } from "./styled";
+import { Image, Row, StyledTable, Caption, TrackOverview, RowHeader, TrackName, TrackDuration } from "./styled";
 import { useState } from "react";
 import { ToggleViewButton } from "../ToggleViewButton";
 import { StyledPlayIcon } from "../StyledPlayIcon";
+import { getAlbumArtists } from "../../functions/getAlbumArtists";
 
 export const Table = ({ list }) => {
     const [hideRestTracks, setHideRestTracks] = useState(true);
@@ -11,38 +12,42 @@ export const Table = ({ list }) => {
     const handleOnRowMouseLeave = () => setActiveIndex(undefined);
 
     return (
-        <StyledTable>
-            {list && (
-                <>
-                    <Caption>Popular</Caption>
-                    {
-                        list
-                            .filter((_, index) => (hideRestTracks ? index < 5 : index <= 10))
-                            .map(({ album, name, popularity, duration_ms }, index) => (
-                                <Row
-                                    key={index}
-                                    onMouseEnter={() => handleOnRowMouseEnter(index)}
-                                    onMouseLeave={handleOnRowMouseLeave}
-                                >
-                                    <TrackOverview>
-                                        <RowHeader scope="row">
-                                            {activeIndex === index ? <StyledPlayIcon /> : index + 1}
-                                        </RowHeader>
-                                        <td><Image src={album.images[0].url} /></td>
-                                        <TrackName>{name}</TrackName>
-                                    </TrackOverview>
-                                    <TrackStats>
-                                        <TrackStat>{popularity}/100</TrackStat>
-                                        <TrackStat>{(duration_ms / 60000).toFixed(2).replace(".", ":")}</TrackStat>
-                                    </TrackStats>
-                                </Row>
-                            ))
-                    }
-                    <ToggleViewButton onClick={() => setHideRestTracks(hideRestTracks => !hideRestTracks)}>
-                        {hideRestTracks ? <>Show more</> : <>Show less</>}
-                    </ToggleViewButton>
-                </>
-            )}
-        </StyledTable>
+        <>
+            <StyledTable>
+                {list && (
+                    <>
+                        <Caption>Popular</Caption>
+                        <tbody>
+                            {
+                                list
+                                    .filter((_, index) => (hideRestTracks ? index < 5 : index <= 10))
+                                    .map(({ album, name, duration_ms, artists }, index) => (
+                                        <Row
+                                            key={index}
+                                            onMouseEnter={() => handleOnRowMouseEnter(index)}
+                                            onMouseLeave={handleOnRowMouseLeave}
+                                        >
+                                            <TrackOverview>
+                                                <RowHeader
+                                                    scope="row"
+                                                    title={`Play ${name} by ${getAlbumArtists(artists)}`}
+                                                >
+                                                    {activeIndex === index ? <StyledPlayIcon /> : index + 1}
+                                                </RowHeader>
+                                                <td><Image src={album.images[0].url} /></td>
+                                                <TrackName>{name}</TrackName>
+                                            </TrackOverview>
+                                            <TrackDuration>{(duration_ms / 60000).toFixed(2).replace(".", ":")}</TrackDuration>
+                                        </Row>
+                                    ))
+                            }
+                        </tbody>
+                    </>
+                )}
+            </StyledTable>
+            <ToggleViewButton onClick={() => setHideRestTracks(hideRestTracks => !hideRestTracks)}>
+                {hideRestTracks ? <>Show more</> : <>Show less</>}
+            </ToggleViewButton>
+        </>
     );
 };
