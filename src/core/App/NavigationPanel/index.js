1b0db1978e217646959c971ad167d7bbaca06996
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 22:29:56 +0200

    replaced navigateToPage with toHome in prop navigateTo of IconBox

--- src/core/App/NavigationPanel/index.js
+++ src/core/App/NavigationPanel/index.js
@@ -1,17 +1,15 @@
 import { NavPanel } from "./styled";
-import { useNavigationToPage } from "../../../common/hooks/useNavigationToPage";
 import { toHome } from "../../../common/functions/routes";
 import { IconBox } from "../../../common/components/IconBox";
 import { ReactComponent as HomeIcon } from "./icons/Home.svg";
 import { Input } from "./Input";
 
 export const NavigationPanel = () => {
-    const navigateToPage = useNavigationToPage();
-
+    
     return (
         <NavPanel>
             <IconBox
-                navigateTo={() => navigateToPage(toHome)}
+                navigateTo={toHome()}
                 Icon={HomeIcon}
                 noText
             />
