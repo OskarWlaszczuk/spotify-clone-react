ae1b565819bb12cff679074799b1620cc4e8c104
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 22:25:34 +0200

    now toHome route function gets optional additionalPath parameter and past it after / in returned path

--- src/common/functions/routes.js
+++ src/common/functions/routes.js
@@ -1,4 +1,4 @@
-export const toHome = () => "/home";
+export const toHome = ({ additionalPath = '' } = { additionalPath: '' }) => `/home/${additionalPath}`;
 export const toSearch = () => "/search";
 export const toAlbum = () => "/album";
 export const toArtist = ({ id, additionalPath = '' } = { id: 'id', additionalPath: '' }) => `/artist/${id}/${additionalPath}`;
