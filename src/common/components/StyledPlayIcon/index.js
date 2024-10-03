8ca2b2d808d117459beb1f329654e8a0659a44f6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 19:08:45 +0200

    decreased size of StyledPlayIcon

--- src/common/components/StyledPlayIcon/index.js
+++ src/common/components/StyledPlayIcon/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ReactComponent as PlayIcon } from "../../icons/Play.svg";
 
 export const StyledPlayIcon = styled(PlayIcon)`
-    width: 14px; 
-    height: 14px; 
+    width: 13px; 
+    height: 13px; 
     fill: white; 
 `;
