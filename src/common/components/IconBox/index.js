7692bb31833861682ec19c508739f3b2d167f36f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-03 22:23:28 +0200

    replaced onClick attribude with to attribude in Container of IconBox

--- src/common/components/IconBox/index.js
+++ src/common/components/IconBox/index.js
@@ -3,7 +3,7 @@ import { Container, Content, ContentWrapper, ExtraContent } from "./styled";
 export const IconBox = ({ Icon, text, navigateTo, extraContent, noText }) => {
 
     return (
-        <Container onClick={navigateTo}>
+        <Container to={navigateTo}>
             <ContentWrapper withoutText={noText}>
                 <Icon />
                 <Content>{text}</Content>
