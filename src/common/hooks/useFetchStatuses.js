e2be373792ba0946842d263095292e08dd61d371
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-04 20:27:41 +0200

    moved useEffect inside useFetchStatuses, dispatching passed actions parameter inside,

--- src/common/hooks/useFetchStatuses.js
+++ src/common/hooks/useFetchStatuses.js
@@ -1,11 +1,33 @@
+import { useEffect } from "react";
 import { initial, loading, success, error } from "../constants/fetchStatuses";
 import { checkFetchStatuses } from "../functions/checkFetchStatuses";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
-export const useFetchStatuses = (fetchStatuses = []) => {
+export const useFetchStatuses = (fetchStatuses = [], actions) => {
+    const param = useParams();
+    const id = param?.id;
+    
+    const dispatch = useDispatch();
+
     const isInitial = checkFetchStatuses(fetchStatuses, initial);
     const isLoading = checkFetchStatuses(fetchStatuses, loading);
     const isError = checkFetchStatuses(fetchStatuses, error);
     const isSucces = (checkFetchStatuses(fetchStatuses, success, true));
+
+    useEffect(() => {
+        const fetchDelayId = setTimeout(() => {
+            actions.map(({ fetchAction }) => dispatch(fetchAction({ id } || '')));
+        }, 500);
+
+        return () => {
+            actions.map(({ clearAction }) => dispatch(clearAction()));
+            clearTimeout(fetchDelayId);
+        };
+    }, [
+        dispatch,
+        id,
+    ]);
 
     return { isInitial, isLoading, isError, isSucces };
 };
