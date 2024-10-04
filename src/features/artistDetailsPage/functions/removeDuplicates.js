34cc802ff3a3a49364044abc41b6ad2127f7f2ed
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-04 07:25:36 +0200

    moved replaceReleaseDateIfCurrentYear and removeDuplicates  functions back to MainContnent, because it's only place where they are use

--- src/features/artistDetailsPage/functions/removeDuplicates.js
+++
@@ -1,8 +0,0 @@
-export const removeDuplicates = (albums = []) => {
-    const caughtDuplicates = new Set();
-    
-    return albums.filter(({ name }) => {
-        const keyValue = name;
-        return !caughtDuplicates.has(keyValue) && caughtDuplicates.add(keyValue);
-    });
-};
--- src/features/artistDetailsPage/functions/replaceReleaseDateIfCurrentYear.js
+++
@@ -1,7 +0,0 @@
-import { isLatestReleased } from "./isLatestReleased";
-
-export const replaceReleaseDateIfCurrentYear = album => {
-    return isLatestReleased(album) ?
-        { ...album, release_date: "Latest Release" } :
-        album;
-};
