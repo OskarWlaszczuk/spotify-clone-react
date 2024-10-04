bd0d63182bd55dd5697cd04cab3b34b1da9d1da2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-04 09:26:18 +0200

    renamed prop gradient to useGradient

--- src/common/components/SectionContainer/index.js
+++ src/common/components/SectionContainer/index.js
@@ -5,7 +5,7 @@ export const SectionContainer = styled.section`
     background-color: ${({ theme }) => theme.colors.black};
     padding: 15px 20px;
 
-    ${({ $gradient }) => $gradient && css`
+    ${({ $useGradient }) => $useGradient && css`
         background: linear-gradient(180deg, rgba(41, 40, 40, 1) 0%, rgba(24, 20, 20, 1) 24%);
     `}
 `;
