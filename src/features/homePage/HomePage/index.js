9e75fa1012f16430ebce8d816edec42033d4adb4
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-04 20:35:03 +0200

    replaced useEffect with useFetchStatuses, passed array of API action as second parameter to useFetchStatuses, removed useDisplatch since it's only used inside useFetchStatuses in HomePage

--- src/features/homePage/HomePage/index.js
+++ src/features/homePage/HomePage/index.js
@@ -1,44 +1,35 @@
-import { useEffect } from "react";
 import { Main } from "../../../common/components/Main";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { albumsSelectors, albumsActions } from "../albums/albumsSlice";
 import { artistsSelectors, artistsActions } from "../artists/artistsSlice";
 import { useFetchStatuses } from "../../../common/hooks/useFetchStatuses";
 import { MainContent } from "./MainContent";
 
 export const HomePage = () => {
-    const dispatch = useDispatch();
-
     const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
     const { fetch: fetchArtists, clear: clearArtists } = artistsActions;
 
     const albumsStatus = useSelector(albumsSelectors.selectStatus);
     const artistsStatus = useSelector(artistsSelectors.selectStatus);
 
-    const { isInitial, isLoading, isSucces, isError } = useFetchStatuses([albumsStatus, artistsStatus]);
+    const { isInitial, isLoading, isSucces, isError } = useFetchStatuses(
+        [
+            albumsStatus,
+            artistsStatus
+        ],
+        [
+            { fetchAction: fetchAlbums, clearAction: clearAlbums },
+            { fetchAction: fetchArtists, clearAction: clearArtists },
+        ],
+    );
 
-    useEffect(() => {
-        const fetchDelayID = setTimeout(() => {
-            dispatch(fetchAlbums());
-            dispatch(fetchArtists());
-        }, 1000);
-
-        return () => {
-            clearTimeout(fetchDelayID);
-            clearAlbums();
-            clearArtists();
-        };
-
-    }, [dispatch, fetchAlbums, fetchArtists, clearAlbums, clearArtists]);
-
+    if (isInitial) return <Main content={<>Initial</>} />;
     if (isLoading) return <Main content={<>loading</>} />;
     if (isError) return <Main content={<>error</>} />;
-    if (isInitial) return <Main content={<>Initial</>} />;
-
     if (isSucces) {
         return (
             <Main
-                gradientAvailable
+                isGradientAvailable
                 content={
                     <MainContent />
                 }
