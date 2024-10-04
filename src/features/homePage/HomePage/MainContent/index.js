638b5e6081f9d4a9a4069060b9d09c7a94c0de08
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-04 20:36:08 +0200

    updated prop's names of Tile, removed unneccesery ones in MainContent of HomePage

--- src/features/homePage/HomePage/MainContent/index.js
+++ src/features/homePage/HomePage/MainContent/index.js
@@ -4,15 +4,16 @@ import { useSelector } from "react-redux";
 import { Tile } from "../../../../common/components/Tile";
 import { albumsSelectors } from "../../../homePage/albums/albumsSlice";
 import { artistsSelectors } from "../../artists/artistsSlice";
-import { toHome, toArtist } from "../../../../common/functions/routes.js";
+import { toHome, toArtist, toAlbum } from "../../../../common/functions/routes.js";
 import { selectDataView } from "../../../../common/functions/selectDataView.js";
+import { titleExtraAsideContentText } from "../../../../common/constants/titleExtraAsideContentText.js";
 
 export const MainContent = () => {
     const { type } = useParams();
 
     const popularAlbums = useSelector(albumsSelectors.selectDatas)?.datas.albums;
     const popularArtists = useSelector(artistsSelectors.selectDatas)?.datas.artists;
-    
+
     const popularAlbumsParam = "popular-albums";
     const popularArtistsParam = "popular-artists";
 
@@ -31,12 +32,11 @@ export const MainContent = () => {
                         renderItem={
                             (({ id, name, images, album_type = "" }) => (
                                 <Tile
-                                    navigateTo={toArtist({ id: id })}
-                                    id={id}
+                                    toPage={toArtist({ id: id })}
                                     picture={images[0].url}
                                     title={name}
                                     subInfo={album_type}
-                                    useArtistPictureStyle={isArtistsList}
+                                    isArtistPictureStyle={isArtistsList}
                                 />
                             ))
                         }
@@ -47,12 +47,12 @@ export const MainContent = () => {
                             title="Popular albums"
                             list={popularAlbums}
                             renderItem={
-                                (({ images, name, artists, id }) => (
+                                (({ images, name, artists }) => (
                                     <Tile
-                                        id={id}
                                         picture={images[0].url}
                                         title={name}
                                         subInfo={artists.map(({ name }) => name).join(",")}
+                                        toPage={toAlbum()}
                                     />
                                 ))
                             }
@@ -60,7 +60,7 @@ export const MainContent = () => {
                             titleExtraAsideContent={
                                 {
                                     link: toHome({ additionalPath: popularAlbumsParam }),
-                                    text: "Show more",
+                                    text: titleExtraAsideContentText,
                                 }
                             }
                         />
@@ -70,20 +70,18 @@ export const MainContent = () => {
                             list={popularArtists}
                             renderItem={({ images, name, type, id }) => (
                                 <Tile
-                                    id={id}
                                     picture={images[0].url}
                                     title={name}
                                     subInfo={type}
-                                    useArtistPictureStyle
-                                    navigateTo={toArtist({ id })}
+                                    isArtistPictureStyle
+                                    toPage={toArtist({ id })}
                                 />
                             )}
                             hideRestListPart
-                            artistsList
                             titleExtraAsideContent={
                                 {
                                     link: toHome({ additionalPath: popularArtistsParam }),
-                                    text: "Show more",
+                                    text: titleExtraAsideContentText,
                                 }
                             }
                         />
