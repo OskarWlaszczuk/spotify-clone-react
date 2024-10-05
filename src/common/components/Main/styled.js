b2bb804f975819830d86744a2fc5f515dad48b2d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-05 12:26:45 +0200

    added SectionContainer's styles to MainContent, removed height: 100vh;

--- src/common/components/Main/styled.js
+++ src/common/components/Main/styled.js
@@ -1,8 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const MainContent = styled.main`
     grid-area: main;
     background-color: ${({theme}) => theme.colors.black};
     width: 100%;
-    height: 100vh;
+    border-radius: 10px;
+    padding: 15px 20px;
+
+    ${({ $useGradient }) => $useGradient && css`
+        background: linear-gradient(180deg, rgba(41, 40, 40, 1) 0%, rgba(24, 20, 20, 1) 24%);
+    `};
 `;
