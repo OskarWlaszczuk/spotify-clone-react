1e90ec6596abc2b3960b4f0012ef4bc2c55d9703
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-05 09:16:40 +0200

    removed delay in useEffect caused by setTimeout, created instead async function called fetchAccessTokenAndData that fetches for accessToken and then dispatches every fetch action, with accessToken passed in each, replaced map method with forEach because it's better for iterating in this case

--- src/common/hooks/useFetchStatuses.js
+++ src/common/hooks/useFetchStatuses.js
@@ -3,11 +3,12 @@ import { initial, loading, success, error } from "../constants/fetchStatuses";
 import { checkFetchStatuses } from "../functions/checkFetchStatuses";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchAccessToken } from "../functions/fetchAccessToken";
 
 export const useFetchStatuses = (fetchStatuses = [], actions) => {
     const param = useParams();
     const id = param?.id;
-    
+
     const dispatch = useDispatch();
 
     const isInitial = checkFetchStatuses(fetchStatuses, initial);
@@ -16,18 +17,21 @@ export const useFetchStatuses = (fetchStatuses = [], actions) => {
     const isSucces = (checkFetchStatuses(fetchStatuses, success, true));
 
     useEffect(() => {
-        const fetchDelayId = setTimeout(() => {
-            actions.map(({ fetchAction }) => dispatch(fetchAction({ id } || '')));
-        }, 500);
+        const fetchAccessTokenAndData = async () => {
+            const accessToken = await fetchAccessToken();
+
+            actions.forEach(({ fetchAction }) => {
+                dispatch(fetchAction({ id: id || '', accessToken }));
+            });
+        };
+
+        fetchAccessTokenAndData();
 
         return () => {
-            actions.map(({ clearAction }) => dispatch(clearAction()));
-            clearTimeout(fetchDelayId);
+            actions.forEach(({ clearAction }) => dispatch(clearAction()));
         };
-    }, [
-        dispatch,
-        id,
-    ]);
+    }, [dispatch, id]);
+
 
     return { isInitial, isLoading, isError, isSucces };
 };
