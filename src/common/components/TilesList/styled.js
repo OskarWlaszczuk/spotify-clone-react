5bf5a7b2df8f35fd2edc03bf0498d782747abe42
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-06 10:18:17 +0200

    created StyledSection to create gaps between TilesList's content, created FullListLink, Title and TitleAsLink components

--- src/common/components/TilesList/styled.js
+++ src/common/components/TilesList/styled.js
@@ -1,24 +1,48 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const TitleContent = styled.header`
+export const StyledSection = styled.section`
+    display: grid;
+    grid-gap: 25px;
+`;
+
+export const TitleContent = styled.div`
   display: flex; 
   align-items: center;
   justify-content: space-between;
 `;
 
-export const ExtraContent = styled(Link)`
-    color: ${({ theme }) => theme.colors.nobel};
-    font-size: 13px;
-    cursor: pointer;
+export const Title = styled.header`
+    font-weight: 600;
+    font-size: 30px; 
+`;
+
+export const TitleAsLink = styled(Link)`
+    font-size: 24px;
+    font-weight: 600;
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.white};
 
     &:hover{
         text-decoration: underline;
     };
 `;
 
+export const FullListLink = styled(Link)`
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 14px;
+    font-weight: 450;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover{
+        text-decoration: underline;
+        filter: brightness(105%);
+    };
+`;
+
 export const List = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
     grid-gap: 15px 0;
 `;
