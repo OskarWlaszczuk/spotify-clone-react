d6618ef60cac20d108aca2b9ac238f2bc033604f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-07 07:23:26 +0200

    passing isActive and mouseEventHandlers to each Tile rendered inside TilesList, using functions returned by

--- src/features/homePage/HomePage/MainContent/index.js
+++ src/features/homePage/HomePage/MainContent/index.js
@@ -7,12 +7,14 @@ import { artistsSelectors } from "../../artists/artistsSlice";
 import { toHome, toArtist, toAlbum } from "../../../../common/functions/routes.js";
 import { matchFullListDataByType } from "../../../../common/functions/matchFullListDataByType.js";
 import { nanoid } from "nanoid";
+import { useActiveTile } from "../../../../common/hooks/useActiveTile.js";
 
 export const MainContent = () => {
     const { type } = useParams();
 
     const popularAlbums = useSelector(albumsSelectors.selectDatas)?.datas.albums;
     const popularArtists = useSelector(artistsSelectors.selectDatas)?.datas.artists;
+    const { setActiveTile, isTileActive } = useActiveTile();
 
     const popularAlbumsParam = "popular-albums";
     const popularArtistsParam = "popular-artists";
@@ -30,10 +32,21 @@ export const MainContent = () => {
                         title={fullListTitle}
                         list={fullListContent}
                         renderItem={
-                            (({ id, name, images, album_type = "" }) => (
+                            (({ id, name, images, album_type = "" }, index) => (
                                 <Tile
+                                    isActive={isTileActive(index, 0)}
+                                    mouseEventHandlers={{
+                                        enter: () => setActiveTile({
+                                            activeTileIndex: index,
+                                            activeTilesListID: 0,
+                                        }),
+                                        leave: () => setActiveTile({
+                                            activeTileIndex: undefined,
+                                            activeTilesListID: undefined,
+                                        }),
+                                    }}
                                     key={nanoid()}
-                                    toPage={toArtist({ id: id })}
+                                    toPage={toArtist({ id })}
                                     picture={images[0].url}
                                     title={name}
                                     subInfo={album_type}
@@ -48,8 +61,19 @@ export const MainContent = () => {
                             title="Popular albums"
                             list={popularAlbums}
                             renderItem={
-                                (({ id, images, name, artists }) => (
+                                (({ id, images, name, artists }, index) => (
                                     <Tile
+                                        isActive={isTileActive(index, 1)}
+                                        mouseEventHandlers={{
+                                            enter: () => setActiveTile({
+                                                activeTileIndex: index,
+                                                activeTilesListID: 1,
+                                            }),
+                                            leave: () => setActiveTile({
+                                                activeTileIndex: undefined,
+                                                activeTilesListID: undefined,
+                                            }),
+                                        }}
                                         key={nanoid()}
                                         picture={images[0].url}
                                         title={name}
@@ -65,8 +89,19 @@ export const MainContent = () => {
                         <TilesList
                             title="Popular artists"
                             list={popularArtists}
-                            renderItem={({ images, name, type, id }) => (
+                            renderItem={({ images, name, type, id }, index) => (
                                 <Tile
+                                    isActive={isTileActive(index, 2)}
+                                    mouseEventHandlers={{
+                                        enter: () => setActiveTile({
+                                            activeTileIndex: index,
+                                            activeTilesListID: 2,
+                                        }),
+                                        leave: () => setActiveTile({
+                                            activeTileIndex: undefined,
+                                            activeTilesListID: undefined,
+                                        }),
+                                    }}
                                     key={nanoid()}
                                     picture={images[0].url}
                                     title={name}
