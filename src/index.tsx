86d01fd6deda9f02756c820f003e45142b8dbaad
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-14 08:26:55 +0200

    refactored js index file on tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@ import { App } from "./core/App/index"
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from './core/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './core/theme';
+import { theme } from './theme';
 import { Provider } from 'react-redux';
 import { store } from './core/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
