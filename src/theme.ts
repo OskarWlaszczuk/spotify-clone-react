ff5dbb0f36c3d2697d7ea078b8e0eb8f81a71f52
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-14 08:28:58 +0200

    refactored theme on ts, added as const to treat theme's properties as literal types

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        green: "#57B660",
+        mantis: "#61C86A",
+        white: "#FFFFFF",
+        black: "#181414",
+        codGray:"#090707",
+        blueMoon: "#649AED",
+        redWine: "#EB5640",
+        mellowYellow: "#F6C874",
+        nobel: "#B3B3B3",
+        mineShaft: "#282828",
+        brightMineShaft: "#28282891",
+    },
+} as const;
