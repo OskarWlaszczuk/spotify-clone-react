e2d526fe586b376c325a318421e628a0e9d42ac9
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-14 08:38:04 +0200

    declared constants with albums and artists IDs, added endpoint for each action object

--- src/features/homePage/HomePage/index.js
+++ src/features/homePage/HomePage/index.js
@@ -12,14 +12,17 @@ export const HomePage = () => {
     const albumsStatus = useSelector(albumsSelectors.selectStatus);
     const artistsStatus = useSelector(artistsSelectors.selectStatus);
 
+    const albumsIDs = "382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc,4bNiBmPncdmzzWdeUSs7DF";
+    const artistsIDs = "4tZwfgrHOc3mvqYlEYSvVi,3hteYQFiMFbJY7wS0xDymP,7CJgLPEqiIRuneZSolpawQ,0tdKRrbItnLj40yUFi23jx,1fxbULcd6ryMNc1usHoP0R,0MIG6gMcQTSvFbKvUwK0id,1Xyo4u8uXC1ZmMpatF05PJ";
+
     const { isInitial, isLoading, isSucces, isError } = useFetchStatuses(
         [
             albumsStatus,
             artistsStatus
         ],
         [
-            { fetchAction: fetchAlbums, clearAction: clearAlbums },
-            { fetchAction: fetchArtists, clearAction: clearArtists },
+            { fetchAction: fetchAlbums, clearAction: clearAlbums, endpoint: `albums?ids=${albumsIDs}` },
+            { fetchAction: fetchArtists, clearAction: clearArtists, endpoint: `artists?ids=${artistsIDs}` },
         ],
     );
 
