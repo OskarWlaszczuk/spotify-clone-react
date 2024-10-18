1fc2bf92e966f604cfc065b6255210950d43b9b3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-18 07:42:21 +0200

    added switching between fetch status cases in Main, new string prop fetchStatus added

--- src/common/components/Main/index.tsx
+++ src/common/components/Main/index.tsx
@@ -1,18 +1,33 @@
 import { ReactElement } from "react";
-import { MainContent, MainSection } from "./styled"
+import { MainContent, MainSection } from "./styled";
+import { error, loading, success } from "../../constants/fetchStatuses";
 
 interface MainProps {
     content: ReactElement;
+    fetchStatus: string;
     banner?: ReactElement | false;
-};
+}
 
-export const Main = ({ content, banner }: MainProps) => (
-    <MainSection>
-        {banner}
-        <MainContent
-            $bannerAvailable={!!banner}
-        >
-            {content}
-        </MainContent>
-    </MainSection>
-);
+export const Main = ({ content, banner, fetchStatus }: MainProps) => {
+
+    switch (fetchStatus) {
+        case success:
+            return (
+                <MainSection>
+                    {banner}
+                    <MainContent $bannerAvailable={!!banner}>
+                        {content}
+                    </MainContent>
+                </MainSection>
+            );
+
+        case loading:
+            return <>Ładowanie</>;
+
+        case error:
+            return <>Błąd</>;
+
+        default:
+            return <>Initial</>;
+    }
+};
