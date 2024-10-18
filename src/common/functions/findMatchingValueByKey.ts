52d53a09117d001a81c910821ba0db629924df46
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-18 07:54:14 +0200

    renamed items to groups in findMatchingValueByKey

--- src/common/functions/findMatchingValueByKey.ts
+++ src/common/functions/findMatchingValueByKey.ts
@@ -1,7 +1,6 @@
-import { ListDataGroup } from "../interfaces/ListDataGroup";
+import { DataByGroup } from "../interfaces/ListDataGroup";
 import { isMatch } from "./isMatch";
 
-
-export const findMatchingValueByKey = (items: ListDataGroup[], targetKey: string): ListDataGroup | undefined => (
-    items.find(({ key }) => isMatch(key, targetKey))
-)
+export const findMatchingValueByKey = (groups: DataByGroup[], targetKey: string): DataByGroup | undefined => (
+    groups.find(({ key }) => isMatch(key, targetKey))
+);
