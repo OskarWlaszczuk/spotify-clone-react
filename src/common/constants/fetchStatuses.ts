9d73d2c61f82deb2bf76c24071f63bfd51a73b93
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-18 19:05:46 +0200

    removed types for constants in fetchStatuses

--- src/common/constants/fetchStatuses.ts
+++ src/common/constants/fetchStatuses.ts
@@ -1,4 +1,4 @@
-export const initial: string = "initial";
-export const loading: string = "loading";
-export const success: string = "success";
-export const error: string = "error";
+export const initial = "initial";
+export const loading = "loading";
+export const success = "success";
+export const error = "error";
