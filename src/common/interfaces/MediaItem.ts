de9e99b7142a00ab845944a89608252e0ff443fb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-19 10:11:20 +0200

    renamed MediaItemData to MediaItem, replaced images property with extendend ImageData interface

---
+++ src/common/interfaces/MediaItem.ts
@@ -0,0 +1,12 @@
+import { ArtistName } from "./ArtistName";
+import { ImageData } from "./ImageCollection";
+
+export interface MediaItem extends ImageData {
+    id: string;
+    name: string;
+    release_date: string;
+    album_group?: string;
+    album_type?: string;
+    type?: string;
+    artists?: ArtistName[]
+};
