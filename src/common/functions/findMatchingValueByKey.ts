e330d3cab6f04e6ee4d68c29e2b2fb79bb8f393b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-20 08:40:02 +0200

    added generic parameter called ValueType to findMatchingValueByKey  and passed it further to DataByGroup interface to define generic type of value

--- src/common/functions/findMatchingValueByKey.ts
+++ src/common/functions/findMatchingValueByKey.ts
@@ -1,6 +1,6 @@
 import { DataByGroup } from "../interfaces/DataByGroup";
 import { isMatch } from "./isMatch";
 
-export const findMatchingValueByKey = (groups: DataByGroup[], targetKey: string): DataByGroup | undefined => (
+export const findMatchingValueByKey = <ValueType>(groups: DataByGroup<ValueType>[], targetKey: string): DataByGroup<ValueType> | undefined => (
     groups.find(({ key }) => isMatch(key, targetKey))
 );
