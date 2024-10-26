bf5dc48018aabb049cbc3b0ad75c48e4674696c6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-26 07:23:21 +0200

    moved MainContent's content directly into content prop, fetching album's artist details and more its albums after album details were fetched with success, added 'album' prefix to album's data constants to distinguish them from artist's constant datas

--- src/features/albumPage/components/AlbumPage/index.js
+++ src/features/albumPage/components/AlbumPage/index.js
@@ -5,58 +5,154 @@ import { useFetchAPI } from "../../../../common/hooks/useFetchAPI";
 import { useParams } from "react-router-dom";
 import { Main } from "../../../../common/components/Main";
 import { Banner } from "../../../../common/components/Banner";
-import { MainContent } from "../MainContent";
 import { getYear } from "../../../../common/functions/getYear";
 import { fromMillisecondsToMinutes } from "../../../../common/functions/fromMillisecondsToMinutes";
 import { convertToMinutesAndSeconds } from "../../../../common/functions/convertToMinutesAndSeconds";
 import { convertMinutesToHours } from "../../../../common/functions/convertMinutesToHours";
-import { toArtist } from "../../../../common/functions/routes";
+import { toAlbum, toArtist } from "../../../../common/functions/routes";
 import { ArtistNameLink } from "./styled";
+import { Table } from "../../../../common/components/Table";
+import { useEffect, useState } from "react";
+import { error, initial, loading, success } from "../../../../common/constants/fetchStatuses";
+import { fetchAccessToken } from "../../../../common/functions/fetchAccessToken";
+import { fetchFromAPI } from "../../../../common/functions/fetchFromAPI";
+import { AvatarImage } from "../../../../common/components/AvatarImage";
+import { TilesList } from "../../../../common/components/TilesList";
+import { Tile } from "../../../../common/components/Tile";
+import { useActiveTile } from "../../../../common/hooks/useActiveTile";
+import { getAlbumArtists } from "../../../../common/functions/getAlbumArtists";
 
 export const AlbumPage = () => {
+
     const { id: albumID } = useParams();
     const { fetch: fetchAlbumDetails, clear: clearAlbumDetails } = albumDetailsActions;
 
+    const [albumArtistDatas, setAlbumArtistDatas] = useState({ image: undefined, name: undefined, restArtistAlbumsList: undefined });
+    const { name: artistName, image: artistImage, restArtistAlbumsList } = albumArtistDatas;
+
+    const [albumArtistDatasFetchStatus, setAlbumArtistDatasFetchStatus] = useState(initial);
+
+    const removeDuplicates = (list = []) => {
+        const caughtDuplicates = new Set();
+
+        return list.filter(disc => {
+            const keyValue = disc;
+            return !caughtDuplicates.has(keyValue) && caughtDuplicates.add(keyValue);
+        });
+    };
+
     const albumDetailsStatus = useSelector(albumDetailsSelectors.selectStatus);
-    const albumDetails = useSelector(albumDetailsSelectors.selectDatas)?.datas;
-    const fetchStatus = useFetchStatus([albumDetailsStatus]);
+    const albumDetailsList = useSelector(albumDetailsSelectors.selectDatas)?.datas;
 
-    useFetchAPI([
-        { fetchAction: fetchAlbumDetails, clearAction: clearAlbumDetails, endpoint: `albums/${albumID}` },
-    ]);
+    const fetchStatus = useFetchStatus([albumDetailsStatus, albumArtistDatasFetchStatus]);
 
-    const artists = albumDetails?.artists;
+    useFetchAPI([{ fetchAction: fetchAlbumDetails, clearAction: clearAlbumDetails, endpoint: `albums/${albumID}` }]);
 
-    const image = albumDetails?.images[0].url;
-    const name = albumDetails?.name;
-    const type = albumDetails?.album_type;
+    const artistsList = albumDetailsList?.artists;
+    const artistID = artistsList?.map(({ id }) => id)[0];
+    const isArtistsListLengthEqualsOne = artistsList?.length === 1;
 
-    const releaseDate = getYear(albumDetails?.release_date);
-    const totalTracks = albumDetails?.total_tracks;
-    const tracks = albumDetails?.tracks.items;
+    const albumImage = albumDetailsList?.images[0].url;
+    const albumName = albumDetailsList?.name;
+    const albumType = albumDetailsList?.album_type;
+    const albumReleaseDate = getYear(albumDetailsList?.release_date);
+
+    const totalTracks = albumDetailsList?.total_tracks;
+    const tracks = albumDetailsList?.tracks.items;
+    const trackDiscNumbers = removeDuplicates(tracks?.map(({ disc_number }) => disc_number));
 
     const tracksDurations = tracks?.map(({ duration_ms }) => duration_ms);
     const totalDuration = fromMillisecondsToMinutes(tracksDurations?.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
     const totalDurationConverted = totalDuration >= 60 ? convertMinutesToHours(totalDuration) : convertToMinutesAndSeconds(totalDuration);
 
-    const metaDatasContent = [releaseDate, `${totalTracks} songs, ${totalDurationConverted}`].join(" • ");
-    const subTitleContent = artists?.map(({ name, id }, index) => (
-        <ArtistNameLink to={toArtist({ id })}> {index !== 0 && " • "}{name} </ArtistNameLink>
+    const metaDatasContent = [albumReleaseDate, `${totalTracks} songs, ${totalDurationConverted}`].join(" • ");
+    const subTitleContent = artistsList?.map(({ name, id }, index) => (
+        <>
+            {isArtistsListLengthEqualsOne && (
+                <AvatarImage
+                    src={artistImage}
+                    alt={name}
+                    title={name}
+                    $smaller
+                    $useArtistPictureStyle
+                />
+            )}
+            <ArtistNameLink to={toArtist({ id })}> {index !== 0 && " • "}{name} </ArtistNameLink>
+        </>
     ));
+
+    useEffect(() => {
+        const fetchArtistsDetails = async () => {
+            try {
+                const accessToken = await fetchAccessToken();
+
+                const response = await Promise.all([
+                    fetchFromAPI({ endpoint: `artists?ids=${artistID}`, accessToken }),
+                    fetchFromAPI({ endpoint: `artists/${artistID}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation`, accessToken })
+                ]);
+                console.log(response)
+                setAlbumArtistDatas({ image: response[0].artists[0].images[0].url, name: response[0].artists[0].name, restArtistAlbumsList: response[1] });
+                setAlbumArtistDatasFetchStatus(success);
+
+            } catch {
+                setAlbumArtistDatasFetchStatus(error);
+            }
+        };
+
+        if (albumDetailsStatus === success) {
+            fetchArtistsDetails();
+        }
+        setAlbumArtistDatasFetchStatus(loading)
+    }, [albumDetailsList]);
+
+    const { setActiveTile, isTileActive } = useActiveTile();
 
     return (
         <Main
             fetchStatus={fetchStatus}
             bannerContent={
                 <Banner
-                    picture={image}
+                    picture={albumImage}
                     subTitleContent={subTitleContent}
                     metaDatas={metaDatasContent}
-                    title={name}
-                    caption={type}
+                    title={albumName}
+                    caption={albumType}
                 />
             }
-            content={<MainContent />}
+            content={
+                <>
+                    <Table list={tracks} useAlbumView discsNumbers={trackDiscNumbers} />
+                    <TilesList
+                        title={<>More by {artistName}</>}
+                        hideRestListPart
+                        list={restArtistAlbumsList?.items}
+                        renderItem={
+                            (({ id, images, name, artists = [] }, index) => (
+                                <Tile
+                                    isActive={isTileActive(index, 1)}
+                                    mouseEventHandlers={{
+                                        enter: () => setActiveTile({
+                                            activeTileIndex: index,
+                                            activeTilesListID: 1,
+                                        }),
+                                        leave: () => setActiveTile({
+                                            activeTileIndex: undefined,
+                                            activeTilesListID: undefined,
+                                        }),
+                                    }}
+                                    key={id}
+                                    picture={images[0].url}
+                                    title={name}
+                                    subInfo={getAlbumArtists(artists)}
+                                    toPage={toAlbum({ id })}
+                                />
+                            )
+                            )
+                        }
+                    // fullListPathname={toAlbum({ additionalPath: popularAlbumsParam })}
+                    />
+                </>
+            }
         />
     );
 };
