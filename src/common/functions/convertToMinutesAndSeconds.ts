347d6b69dc7f80dd2985190032cd3984509d12ce
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-27 11:16:38 +0100

    declared convertToMinutesAndSeconds which converts passed parameter to minutes and sec

---
+++ src/common/functions/convertToMinutesAndSeconds.ts
@@ -0,0 +1,12 @@
+export const convertToMinutesAndSeconds = (time: number): string => {
+    const minutes = Math.floor(time);
+    let seconds = Math.round((time % 1) * 100);
+
+    if (seconds >= 60) {
+        const extraMinutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        return `${minutes + extraMinutes} min ${seconds} sec`;
+    }
+
+    return `${minutes} min ${seconds} sec`;
+};
