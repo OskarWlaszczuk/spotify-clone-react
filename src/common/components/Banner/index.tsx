b4632fecf17801c8003bde13ebd2b9d789664206
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-27 11:05:47 +0100

    induced capitalizeFirstLetter with caption as argument, wrapped MetaDatas with SubTitleContent, Banner gets new subTitleContent prop

--- src/common/components/Banner/index.tsx
+++ src/common/components/Banner/index.tsx
@@ -1,23 +1,24 @@
+import { capitalizeFirstLetter } from "../../functions/capitalizeFirstLetter";
 import { Picture } from "../Picture";
-import { Caption, Container, Details, MetaDatas, Title } from "./styled";
+import { Caption, Container, Details, MetaDatas, SubTitleContent, Title } from "./styled";
 
 interface BannerProps {
     picture: string;
-    metaDatas: string;
+    metaDatas?: string;
     title: string;
     caption: string;
     isArtistPictureStyle: boolean;
+    subTitleContent: any;
 };
 
-export const Banner = ({ picture, metaDatas, title, caption, isArtistPictureStyle }: BannerProps) => {
-
+export const Banner = ({ picture, metaDatas, title, caption, isArtistPictureStyle, subTitleContent }: BannerProps) => {
     return (
         <Container>
             <Picture $picture={picture} $useArtistPictureStyle={isArtistPictureStyle} />
             <Details>
-                <Caption>{caption}</Caption>
+                <Caption>{capitalizeFirstLetter(caption)}</Caption>
                 <Title>{title}</Title>
-                <MetaDatas>{metaDatas}</MetaDatas>
+                <SubTitleContent>{subTitleContent}<MetaDatas>{metaDatas && " • "}{metaDatas}</MetaDatas></SubTitleContent>
             </Details>
         </Container>
     );
