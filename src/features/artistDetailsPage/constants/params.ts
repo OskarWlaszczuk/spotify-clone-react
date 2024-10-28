68563ce0a69089e839ef99349a1848f037cb6fbd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-28 08:54:38 +0100

    created speerated files for constants with categories and params

---
+++ src/features/artistDetailsPage/constants/params.ts
@@ -0,0 +1,6 @@
+export const allParamDiscography = "all";
+export const albumsParamDiscography = "album";
+export const singleParamDiscography = "single";
+export const compilationParamDiscography = "compilation";
+export const relatedArtistsParam = "related";
+export const artistAppearsOnParam = "appears-on";
