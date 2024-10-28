957720fb38ad2c40009609dc164d4b50501e0f4a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-28 08:58:06 +0100

    assigned FetchStatus type to albumsStatus and artistsStatus in HomePage

--- src/features/homePage/components/HomePage/index.tsx
+++ src/features/homePage/components/HomePage/index.tsx
@@ -6,6 +6,7 @@ import { useFetchStatus } from "../../../../common/hooks/useFetchStatuses";
 import { MainContent } from "../MainContent";
 import { useFetchAPI } from "../../../../common/hooks/useFetchAPI";
 import { useParams } from "react-router-dom";
+import { FetchStatus } from "../../../../common/types/FetchStatus";
 
 export const HomePage = () => {
     const { type } = useParams();
@@ -13,8 +14,8 @@ export const HomePage = () => {
     const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
     const { fetch: fetchArtists, clear: clearArtists } = artistsActions;
 
-    const albumsStatus = useSelector(albumsSelectors.selectStatus);
-    const artistsStatus = useSelector(artistsSelectors.selectStatus);
+    const albumsStatus: FetchStatus = useSelector(albumsSelectors.selectStatus);
+    const artistsStatus: FetchStatus = useSelector(artistsSelectors.selectStatus);
 
     const albumsIDs = "382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc,4bNiBmPncdmzzWdeUSs7DF";
     const artistsIDs = "4tZwfgrHOc3mvqYlEYSvVi,3hteYQFiMFbJY7wS0xDymP,7CJgLPEqiIRuneZSolpawQ,0tdKRrbItnLj40yUFi23jx,1fxbULcd6ryMNc1usHoP0R,0MIG6gMcQTSvFbKvUwK0id,1Xyo4u8uXC1ZmMpatF05PJ";
