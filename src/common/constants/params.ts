7f98dbfd45df041109dea8396280ebf163020f21
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-31 09:25:25 +0100

    moved params constants file to common folder since they are used globally in application

---
+++ src/common/constants/params.ts
@@ -0,0 +1,7 @@
+export const allParamDiscography = "discography";
+export const albumsParamDiscography = "discography/album";
+export const singleParamDiscography = "discography/single";
+export const compilationParamDiscography = "discography/compilation";
+
+export const relatedArtistsParam = "related";
+export const artistAppearsOnParam = "appears-on";
--- src/features/artistDetailsPage/constants/params.ts
+++
@@ -1,6 +0,0 @@
-export const allParamDiscography = "all";
-export const albumsParamDiscography = "album";
-export const singleParamDiscography = "single";
-export const compilationParamDiscography = "compilation";
-export const relatedArtistsParam = "related";
-export const artistAppearsOnParam = "appears-on";
