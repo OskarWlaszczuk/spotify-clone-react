f7f6d5b1c3451807ae3bc395fe6c48bdcc30b452
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-10-31 09:16:36 +0100

    created StyledVerifiedIcon which styles Verified svg icon

---
+++ src/common/components/StyledVerifiedIcon/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { ReactComponent as VerifiedIcon } from "../../icons/Verified.svg";
+
+export const StyledVerifiedIcon = styled(VerifiedIcon)`
+    
+`;
