add6f79a4e93c7fdc5376a9da9ea153e0ed03e90
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-01 10:47:25 +0100

    replaced "/" with "-", renamed allParamDiscography to allReleaseDiscography

--- src/common/constants/params.ts
+++ src/common/constants/params.ts
@@ -1,7 +1,7 @@
-export const allParamDiscography = "discography";
-export const albumsParamDiscography = "discography/album";
-export const singleParamDiscography = "discography/single";
-export const compilationParamDiscography = "discography/compilation";
+export const allReleaseDiscography = "discography";
+export const albumsParamDiscography = "discography-album";
+export const singleParamDiscography = "discography-single";
+export const compilationParamDiscography = "discography-compilation";
 
 export const relatedArtistsParam = "related";
 export const artistAppearsOnParam = "appears-on";
