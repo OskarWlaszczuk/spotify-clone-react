84c413710e7717894c421d566de85d648d57ca73
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-02 08:08:23 +0100

    removed inducing useFetchAPI, declared and return configs obejct which contains useFetchAPI datas to passed in useArtistDetails and useArtistAllReleases hooks

--- src/common/hooks/useArtistAllReleases.js
+++ src/common/hooks/useArtistAllReleases.js
@@ -1,18 +1,13 @@
 import { useSelector } from "react-redux";
 import { artistAlbumsActions, artistAlbumsSelectors } from "../../features/artistDetailsPage/slices/artistAlbumsSlice";
-import { useFetchAPI } from "./useFetchAPI"
 
 export const useArtistAllReleases = (artistID) => {
     const { fetch: fetchMainArtistAllReleasesList, clear: clearMainArtistAllReleasesList } = artistAlbumsActions;
 
-    useFetchAPI([{
-        fetchAction: fetchMainArtistAllReleasesList,
-        clearAction: clearMainArtistAllReleasesList,
-        endpoint: `artists/${artistID}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation&limit=50`
-    }], [artistID]);
-
     const artistAllReleasesStatus = useSelector(artistAlbumsSelectors.selectStatus);
     const artistAllReleasesList = useSelector(artistAlbumsSelectors.selectDatas)?.datas.items;
 
-    return { artistAllReleasesStatus, artistAllReleasesList };
+    const configs = { fetchAction: fetchMainArtistAllReleasesList, clearAction: clearMainArtistAllReleasesList, endpoint: `artists/${artistID}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation&limit=50` };
+
+    return { configs, artistAllReleasesStatus, artistAllReleasesList };
 };
--- src/common/hooks/useArtistDetails.js
+++ src/common/hooks/useArtistDetails.js
@@ -1,18 +1,13 @@
 import { useSelector } from "react-redux";
 import { artistDetailsActions, artistDetailsSelectors } from "../../features/artistDetailsPage/slices/artistDetailsSlice";
-import { useFetchAPI } from "./useFetchAPI";
 
 export const useArtistDetails = (artistID) => {
     const { fetch: fetchArtistDetails, clear: clearArtistDetails } = artistDetailsActions;
 
-    useFetchAPI([{
-        fetchAction: fetchArtistDetails,
-        clearAction: clearArtistDetails,
-        endpoint: `artists/${artistID}`
-    }], [artistID]);
-
     const artistDetailsStatus = useSelector(artistDetailsSelectors.selectStatus);
     const artistDetails = useSelector(artistDetailsSelectors.selectDatas)?.datas;
 
-    return { artistDetailsStatus, artistDetails };
-}
+    const configs = { fetchAction: fetchArtistDetails, clearAction: clearArtistDetails, endpoint: `artists/${artistID}` };
+
+    return { configs, artistDetails, artistDetailsStatus };
+};
