4a1c2b08a25b3073a12e3f0318b24ea29c777c3d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-02 06:59:40 +0100

    replaced repeatable code of table and tenrary operator with renderTableContent function

--- src/common/components/Table/index.tsx
+++ src/common/components/Table/index.tsx
@@ -1,5 +1,5 @@
 import { ContentRow, StyledTable, Caption, TrackOverview, Index, TrackName, TrackDuration, Wrapper, TrackArtists, TrackDetailsWrapper, HeaderRow, StyledTimer, Header, ArtistName, DiscNumberContainer, ArtistNameContainer } from "./styled";
-import { useState } from "react";
+import React, { useState } from "react";
 import { ToggleViewButton } from "../ToggleViewButton";
 import { StyledPlayIcon } from "../StyledPlayIcon";
 import { getAlbumArtists } from "../../functions/getAlbumArtists";
@@ -22,22 +22,70 @@ export const Table = ({ list, useAlbumView, discsNumbers }: TableProps) => {
 
     const handleOnRowMouseEnter = (trackIndex: number): void => setActiveIndex(list.findIndex((_, index: number) => index === trackIndex));
     const handleOnRowMouseLeave = (): void => setActiveIndex(undefined);
-    // const refs = useRef([]);
-    // useEffect(() => {
-    //     const checkEllipsis = () => {
-    //         if (refs.current.length > 0) {
 
-    //             refs.current.forEach(({ scrollWidth, clientWidth }) => {
-    //                 console.log(scrollWidth, clientWidth)
-    //             });
-    //         }
-    //     };
+    const renderTableContent = ({
+        list,
+        useAlbumView,
+        discsNumbers,
+        hideRestTracks,
+        activeIndex,
+        handleOnRowMouseEnter,
+        handleOnRowMouseLeave
+    }: {
+        list: any
+        useAlbumView: any;
+        discsNumbers: any;
+        hideRestTracks: any
+        activeIndex: any;
+        handleOnRowMouseEnter: any;
+        handleOnRowMouseLeave: any;
+    }) => {
+        const renderRows = (tracks: any) => tracks.map(({ album, name, duration_ms, artists }: { album: any, name: any, duration_ms: any, artists: any }, index: any) => (
+            <ContentRow
+                key={nanoid()}
+                onMouseEnter={() => handleOnRowMouseEnter(index)}
+                onMouseLeave={handleOnRowMouseLeave}
+            >
+                <TrackOverview rowSpan={useAlbumView ? 2 : 1}>
+                    <Index
+                        scope="row"
+                        title={`Play ${name} by ${getAlbumArtists(artists)}`}
+                    >
+                        {activeIndex === index ? <StyledPlayIcon /> : index + 1}
+                    </Index>
+                    {!useAlbumView && <td><AvatarImage src={album.images[0].url} /></td>}
+                    <TrackDetailsWrapper>
+                        <TrackName>{name}</TrackName>
+                        {useAlbumView && (
+                            <TrackArtists>
+                                {artists?.map(({ name, id }: { name: any, id: any }, artistIndex: any) => (
+                                    <ArtistNameContainer key={id}>
+                                        {artistIndex !== 0 && ", "}
+                                        <ArtistName $rowActive={activeIndex === index} to={toArtist({ id })}>{name}</ArtistName>
+                                    </ArtistNameContainer>
+                                ))}
+                            </TrackArtists>
+                        )}
+                    </TrackDetailsWrapper>
+                </TrackOverview>
+                <TrackDuration>{fromMillisecondsToMinutes(duration_ms).replace(".", ":")}</TrackDuration>
+            </ContentRow>
+        ));
 
-    //     checkEllipsis();
+        if (discsNumbers && discsNumbers.length > 1) {
+            return discsNumbers.map((discNumber: any) => (
+                <React.Fragment key={discNumber}>
+                    <DiscNumberContainer>
+                        <StyledDiscIcon /> Disc {discNumber}
+                    </DiscNumberContainer>
+                    {renderRows(list.filter(({ disc_number }: { disc_number: any }) => disc_number === discNumber)
+                        .filter((_: any, index: any) => hideRestTracks ? index < 5 : useAlbumView ? index >= 0 : index <= 10))}
+                </React.Fragment>
+            ));
+        }
 
-    //     window.addEventListener("resize", checkEllipsis);
-    //     return () => window.removeEventListener("resize", checkEllipsis);
-    // }, []);
+        return renderRows(list.filter((_: any, index: any) => hideRestTracks ? index < 5 : useAlbumView || !hideRestTracks ? index >= 0 : index <= 10));
+    };
 
     return (
         <Wrapper>
@@ -55,92 +103,15 @@ export const Table = ({ list, useAlbumView, discsNumbers }: TableProps) => {
                     )
                 }
                 <tbody>
-                    {
-                        discsNumbers && discsNumbers.length > 1 ?
-                            discsNumbers?.map(discNumber => {
-                                return (
-                                    <>
-                                        <DiscNumberContainer><StyledDiscIcon /> Disc {discNumber}</DiscNumberContainer>
-                                        {
-                                            list?.filter((_, index) => (hideRestTracks ? index < 5 : useAlbumView ? index >= 0 : index <= 10))
-                                                .filter(({ disc_number }) => disc_number === discNumber)
-                                                .map(({ album, name, duration_ms, artists }, index: number) => (
-                                                    <>
-                                                        <ContentRow
-                                                            key={nanoid()}
-                                                            onMouseEnter={() => handleOnRowMouseEnter(index)}
-                                                            onMouseLeave={handleOnRowMouseLeave}
-                                                        >
-                                                            <TrackOverview rowSpan={useAlbumView ? 2 : 1}>
-                                                                <Index
-                                                                    scope="row"
-                                                                    title={`Play ${name} by ${getAlbumArtists(artists)}`}
-                                                                >
-                                                                    {activeIndex === index ? <StyledPlayIcon /> : index + 1}
-                                                                </Index>
-                                                                {!useAlbumView && <td><AvatarImage src={album.images[0].url} /></td>}
-                                                                <TrackDetailsWrapper>
-                                                                    <TrackName
-                                                                    // $isEllipsis={isEllipsis}
-                                                                    // ref={(el) => refs.current[index] = el}
-                                                                    >
-                                                                        {name}
-                                                                    </TrackName>
-                                                                    {useAlbumView && (
-                                                                        <TrackArtists>{artists?.map(({ name, id }, artistIndex) => (
-                                                                            <ArtistName $rowActive={activeIndex === index} to={toArtist({ id })}>{artistIndex !== 0 && ","}{name}</ArtistName>
-                                                                        ))}
-                                                                        </TrackArtists>)
-                                                                    }
-                                                                </TrackDetailsWrapper>
-                                                            </TrackOverview>
-                                                            <TrackDuration>{fromMillisecondsToMinutes(duration_ms).replace(".", ":")}</TrackDuration>
-                                                        </ContentRow>
-                                                    </>
-                                                ))
-                                        }
-                                    </>
-                                )
-                            }) :
-                            list?.filter((_, index) => (hideRestTracks ? index < 5 : useAlbumView || !hideRestTracks ? index >= 0 : index <= 10))
-                                .map(({ album, name, duration_ms, artists }, index: number) => (
-                                    <>
-                                        <ContentRow
-                                            key={nanoid()}
-                                            onMouseEnter={() => handleOnRowMouseEnter(index)}
-                                            onMouseLeave={handleOnRowMouseLeave}
-                                        >
-                                            <TrackOverview rowSpan={useAlbumView ? 2 : 1}>
-                                                <Index
-                                                    scope="row"
-                                                    title={`Play ${name} by ${getAlbumArtists(artists)}`}
-                                                >
-                                                    {activeIndex === index ? <StyledPlayIcon /> : index + 1}
-                                                </Index>
-                                                {!useAlbumView && <td><AvatarImage src={album.images[0].url} /></td>}
-                                                <TrackDetailsWrapper>
-                                                    <TrackName
-                                                    // $isEllipsis={isEllipsis}
-                                                    // ref={(el) => refs.current[index] = el}
-                                                    >
-                                                        {name}
-                                                    </TrackName>
-                                                    {useAlbumView && (
-                                                        <TrackArtists>{artists?.map(({ name, id }, artistIndex) => (
-                                                            <ArtistNameContainer>
-                                                                {artistIndex !== 0 && ", "}
-                                                                <ArtistName $rowActive={activeIndex === index} to={toArtist({ id })}>{name}</ArtistName>
-                                                            </ArtistNameContainer>
-                                                        ))}
-                                                        </TrackArtists>)
-                                                    }
-                                                </TrackDetailsWrapper>
-                                            </TrackOverview>
-                                            <TrackDuration>{fromMillisecondsToMinutes(duration_ms).replace(".", ":")}</TrackDuration>
-                                        </ContentRow>
-                                    </>
-                                ))
-                    }
+                    {renderTableContent({
+                        list,
+                        useAlbumView,
+                        discsNumbers,
+                        hideRestTracks,
+                        activeIndex,
+                        handleOnRowMouseEnter,
+                        handleOnRowMouseLeave
+                    })}
                 </tbody>
             </StyledTable>
             {
