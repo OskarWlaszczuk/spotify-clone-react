58955764259fd98a753bd5cdc2fa20a8ac886dc0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-04 16:29:17 +0100

    fetching track lyrics using lyrics-finder in TrackDetailsPage

--- src/features/trackDetailsPage/components/TrackDetailsPage/index.js
+++ src/features/trackDetailsPage/components/TrackDetailsPage/index.js
@@ -10,25 +10,38 @@ import { getYear } from "../../../../common/functions/getYear";
 import { AvatarImage } from "../../../../common/components/AvatarImage";
 import { ArtistNameLink } from "../../../albumPage/components/AlbumPage/styled";
 import { toAlbum, toArtist } from "../../../../common/functions/routes";
+import { useApiData } from "../../../../common/hooks/useApiData";
+import { artistsActions, artistsSelectors } from "../../../homePage/slices/artistsSlice";
+import { useEffect, useState } from "react";
+import { initial, loading, success, error } from "../../../../common/constants/fetchStatuses";
 
 export const TrackDetailsPage = () => {
-    const { trackID } = useParams();
+    const { trackID, artistsIDs } = useParams();
+    const [lyrics, setLyrics] = useState("");
+    const [lyricsFetchStatus, setLyricsFetchStatus] = useState(initial);
 
     const { fetch: fetchTrackDetails, clear: clearTrackDetails } = trackDetailsActions;
+
+    const {
+        configs: artistDetailsConfigs,
+        status: artistDetailsStatus,
+        datas: artistDetails
+    } = useApiData(artistsActions, artistsSelectors, `artists?ids=${artistsIDs}`);
 
     const trackDetailsStatus = useSelector(trackDetailsSelectors.selectStatus);
     const trackDetails = useSelector(trackDetailsSelectors.selectDatas)?.datas;
 
-    const fetchStatus = useFetchStatus([trackDetailsStatus]);
+    const fetchStatus = useFetchStatus([trackDetailsStatus, artistDetailsStatus, lyricsFetchStatus]);
 
     useFetchAPI([
+        artistDetailsConfigs,
         { fetchAction: fetchTrackDetails, clearAction: clearTrackDetails, endpoint: `tracks/${trackID}` },
-        // { fetchAction: fetchArtists, clearAction: clearArtists, endpoint: `artists?ids=${artistsIDs}` },
     ]);
-    console.log(trackDetails)
+
     const track = {
         name: trackDetails?.name,
         type: trackDetails?.type,
+        id: trackDetails?.id,
         duration: fromMillisecondsToMinutes(trackDetails?.duration_ms),
         popularity: trackDetails?.popularity,
     };
@@ -38,30 +51,53 @@ export const TrackDetailsPage = () => {
         image: trackDetails?.album.images[0].url,
         releaseDate: trackDetails?.album.release_date,
         id: trackDetails?.album.id,
-    }
+    };
 
     const artists = {
         artistsList: trackDetails?.artists,
     };
 
     const mainArtist = {
-        name: trackDetails?.artists[0].name,
-        id: trackDetails?.artists[0].id,
+        name: artistDetails?.artists[0].name,
+        id: artistDetails?.artists[0].id,
+        image: artistDetails?.artists[0].images[0].url,
     };
 
     const metaDatasContent = [getYear(album.releaseDate), `${track.duration.replace(".", ":")}, ${track.popularity}/100`].join(" • ");
     const subTitleContent = <>
         <AvatarImage
-            src={""}
+            src={mainArtist.image}
             alt={mainArtist.name}
             title={mainArtist.name}
             $smaller
             $useArtistPictureStyle
-        />
+        />{" "}
         <ArtistNameLink to={toArtist({ id: mainArtist.id })}>{mainArtist.name}</ArtistNameLink>{" • "}
         <ArtistNameLink $thinner to={toAlbum({ albumID: album.id, artistID: mainArtist.id })}>{album.name}</ArtistNameLink>
     </>
 
+    const getLyricsForTrack = async (artist, track) => {
+        try {
+            const response = await fetch(`http://localhost:5000/lyrics?artist=${artist}&track=${track}`);
+            const data = await response.json();
+            setLyrics(data.lyrics);
+            setLyricsFetchStatus(success);
+
+        } catch {
+            setLyricsFetchStatus(error);
+        }
+    };
+
+    useEffect(() => {
+        if (mainArtist.name && track.name) {
+            getLyricsForTrack(mainArtist.name, track.name);
+            setLyricsFetchStatus(loading);
+        }
+    }, [mainArtist.name, track.name]);
+
+    const formattedLyrics = lyrics?.slice(0, 423).split('\n').map((line, index) => (
+        <p key={index}>{line}</p>
+    ));
 
     return (
         <>
@@ -77,9 +113,9 @@ export const TrackDetailsPage = () => {
                     />
                 }
                 content={
-                    <>
-
-                    </>
+                    <section>
+                        {formattedLyrics}
+                    </section>
                 }
             />
         </>
