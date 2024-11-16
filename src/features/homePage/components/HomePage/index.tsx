89c7893439cdb7542c403f7d4c5ef43908c26261
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-16 13:52:04 +0100

    declaring artists list configs and datas using useApiData in HomePage

--- src/features/homePage/components/HomePage/index.tsx
+++ src/features/homePage/components/HomePage/index.tsx
@@ -7,27 +7,32 @@ import { MainContent } from "../MainContent";
 import { useFetchAPI } from "../../../../common/hooks/useFetchAPI";
 import { useParams } from "react-router-dom";
 import { FetchStatus } from "../../../../common/types/FetchStatus";
+import { useApiData } from "../../../../common/hooks/useApiData";
 
 export const HomePage = () => {
     const { type } = useParams();
 
-    const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
-    const { fetch: fetchArtists, clear: clearArtists } = artistsActions;
-
-    const albumsStatus: FetchStatus = useSelector(albumsSelectors.selectStatus);
-    const artistsStatus: FetchStatus = useSelector(artistsSelectors.selectStatus);
-
     const albumsIDs = "382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc,4bNiBmPncdmzzWdeUSs7DF";
     const artistsIDs = "4tZwfgrHOc3mvqYlEYSvVi,3hteYQFiMFbJY7wS0xDymP,7CJgLPEqiIRuneZSolpawQ,0tdKRrbItnLj40yUFi23jx,1fxbULcd6ryMNc1usHoP0R,0MIG6gMcQTSvFbKvUwK0id,1Xyo4u8uXC1ZmMpatF05PJ";
 
-    const fetchStatus = useFetchStatus([albumsStatus, artistsStatus],);
+    const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
+
+    const {
+        configs: artistsDetailsListConfigs,
+        status: artistsDetailsListStatus,
+        datas: artistsDetailsList
+    } = useApiData(artistsActions, artistsSelectors, `artists?ids=${artistsIDs}`);
+
+    const albumsStatus: FetchStatus = useSelector(albumsSelectors.selectStatus);
+
+    const fetchStatus = useFetchStatus([albumsStatus, artistsDetailsListStatus]);
 
     useFetchAPI([
+        artistsDetailsListConfigs,
         { fetchAction: fetchAlbums, clearAction: clearAlbums, endpoint: `albums?ids=${albumsIDs}` },
-        { fetchAction: fetchArtists, clearAction: clearArtists, endpoint: `artists?ids=${artistsIDs}` },
     ]);
 
     return (
-        <Main useGradient={!type} fetchStatus={fetchStatus} content={<MainContent />} />
+        <Main useGradient={!type} fetchStatus={fetchStatus} content={<MainContent artistsDetailsList={artistsDetailsList?.artists} />} />
     );
 };
