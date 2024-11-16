7b66c957247d85fd9c934858a7c5798ef6ad59ae
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-16 13:55:25 +0100

    added styles for lyrics section

---
+++ src/features/trackDetailsPage/components/MainContent/Lyrics/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LyricsSection = styled.article`
+    z-index: 2;
+    position: relative;
+`;
+
+export const LyricsLine = styled.p`
+    margin: 0;
+    font-size: 14px;
+`;
