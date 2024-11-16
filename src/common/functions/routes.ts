591c57ecd402d4def6f4d2917739ab357ab4f416
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-16 13:25:29 +0100

    removed artistID parameter from toAlbum, created toTrack function which gets only trackID parameter

--- src/common/functions/routes.ts
+++ src/common/functions/routes.ts
@@ -12,5 +12,6 @@ type ToDetailsPage = (params: ToDetailsPageParams) => string;
 export const toHome: ToPage = ({ additionalPath = '' } = { additionalPath: '' }) => `/home/${additionalPath}`;
 export const toSearch: ToPage = () => "/search";
 
-export const toAlbum = ({ albumID, artistID }: { albumID: string, artistID: string } = { albumID: "", artistID: "" }) => `/album/${albumID}/artist/${artistID}`;
+export const toAlbum = ({ albumID }: { albumID: string } = { albumID: "" }) => `/album/${albumID}`;
 export const toArtist: ToDetailsPage = ({ id, additionalPath = '' } = { id: '', additionalPath: '' }) => `/artist/${id}/${additionalPath}`;
+export const toTrack = ({ trackID }: { trackID: string } = { trackID: "" }) => `/track/${trackID}`;
