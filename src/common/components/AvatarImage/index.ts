65eef47a6bbbc5a05940b00c8a7ab8d9b4ab9acb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-16 13:13:53 +0100

    added new props to AvatarImage styled-component, added before pseudo-class to set $darkened prop only to the background layer of the AvatarImage

--- src/common/components/AvatarImage/index.ts
+++ src/common/components/AvatarImage/index.ts
@@ -1,22 +1,54 @@
 import styled, { css } from "styled-components";
 
 interface AvatarImageProps {
-    $smaller?: boolean;
-    $useArtistPictureStyle?: boolean;
+     $smaller?: boolean;
+     $larger?: boolean;
+     $picture: string;
+     $useArtistPictureStyle?: boolean;
+     $darkened?: boolean;
 };
-
-export const AvatarImage = styled.img<AvatarImageProps>`
+export const AvatarImage = styled.div<AvatarImageProps>`
+    position: relative;
     width: 42px;
     height: 42px;
     border-radius: 5px;
     vertical-align: middle;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-   ${({ $smaller }) => $smaller && css`
+    ${({ $smaller }) => $smaller && css`
         width: 30px;
         height: 30px;
-   `};
+    `};
 
-   ${({ $useArtistPictureStyle }) => $useArtistPictureStyle && css`
+    ${({ $larger }) => $larger && css`
+        width: 75px;
+        height: 75px;
+    `};
+
+    ${({ $useArtistPictureStyle }) => $useArtistPictureStyle && css`
         border-radius: 50%;
-   `};
+    `};
+
+    &::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: ${({ $picture }) => `url(${$picture})`};
+        background-size: cover;
+        background-position: center;
+        border-radius: inherit;
+        ${({ $darkened }) => $darkened && css`
+            filter: brightness(50%);
+        `}
+    }
+
+    & > * {
+        position: relative;
+        z-index: 1;
+    }
 `;
