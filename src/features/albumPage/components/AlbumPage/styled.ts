e6085227dc72d470ca9a4c8ac0c264c0baa15bff
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-16 13:43:59 +0100

    added  $thinner prop to ArtistNameLink

--- src/features/albumPage/components/AlbumPage/styled.ts
+++ src/features/albumPage/components/AlbumPage/styled.ts
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const ArtistNameLink = styled(Link)`
+interface ArtistNameLinkProps {
+    $thinner?: boolean;
+}
+
+export const ArtistNameLink = styled(Link) <ArtistNameLinkProps>`
     color:${({ theme }) => theme.colors.white};
     text-decoration:none;
 
     &:hover{
         text-decoration: underline;
     };
+
+    ${({ $thinner }) => $thinner && css`
+        font-weight: 350;
+    `};
 `;
