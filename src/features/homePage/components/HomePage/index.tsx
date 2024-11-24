f266341bb7646c91402331095f01a09f6fe6b9ad
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-24 10:22:12 +0100

    replaced useApiData with testing useApiData2, which manage multiply elements in HomePage

--- src/features/homePage/components/HomePage/index.tsx
+++ src/features/homePage/components/HomePage/index.tsx
@@ -1,13 +1,11 @@
 import { Main } from "../../../../common/components/Main";
-import { useSelector } from "react-redux";
 import { albumsSelectors, albumsActions } from "../../slices/albumsSlice";
 import { artistsSelectors, artistsActions } from "../../slices/artistsSlice";
 import { useFetchStatus } from "../../../../common/hooks/useFetchStatuses";
 import { MainContent } from "../MainContent";
 import { useFetchAPI } from "../../../../common/hooks/useFetchAPI";
 import { useParams } from "react-router-dom";
-import { FetchStatus } from "../../../../common/types/FetchStatus";
-import { useApiData } from "../../../../common/hooks/useApiData";
+import { useApiData2 } from "../../../../common/hooks/useApiData";
 
 export const HomePage = () => {
     const { type } = useParams();
@@ -15,24 +13,40 @@ export const HomePage = () => {
     const albumsIDs = "382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc,4bNiBmPncdmzzWdeUSs7DF";
     const artistsIDs = "4tZwfgrHOc3mvqYlEYSvVi,3hteYQFiMFbJY7wS0xDymP,7CJgLPEqiIRuneZSolpawQ,0tdKRrbItnLj40yUFi23jx,1fxbULcd6ryMNc1usHoP0R,0MIG6gMcQTSvFbKvUwK0id,1Xyo4u8uXC1ZmMpatF05PJ";
 
-    const { fetch: fetchAlbums, clear: clearAlbums } = albumsActions;
-
-    const {
-        configs: artistsDetailsListConfigs,
-        status: artistsDetailsListStatus,
-        datas: artistsDetailsList
-    } = useApiData(artistsActions, artistsSelectors, `artists?ids=${artistsIDs}`);
-
-    const albumsStatus: FetchStatus = useSelector(albumsSelectors.selectStatus);
-
-    const fetchStatus = useFetchStatus([albumsStatus, artistsDetailsListStatus]);
-
-    useFetchAPI([
-        artistsDetailsListConfigs,
-        { fetchAction: fetchAlbums, clearAction: clearAlbums, endpoint: `albums?ids=${albumsIDs}` },
+    const apiResources = useApiData2([
+        {
+            action: artistsActions,
+            selectors: artistsSelectors,
+            endpoint: `artists?ids=${artistsIDs}`
+        },
+        {
+            action: albumsActions,
+            selectors: albumsSelectors,
+            endpoint: `albums?ids=${albumsIDs}`,
+        }
     ]);
 
+    const configs = apiResources.map(({ configs }: any) => configs);
+    const datas = apiResources.map(({ datas }: any) => datas)
+    const statuses = apiResources.map(({ status }: any) => status)
+
+    const popularArtistsList = datas?.[0]?.artists;
+    const popularAlbumsList = datas?.[1]?.albums;
+
+    const fetchStatus = useFetchStatus([...statuses]);
+
+    useFetchAPI([...configs]);
+
     return (
-        <Main useGradient={!type} fetchStatus={fetchStatus} content={<MainContent artistsDetailsList={artistsDetailsList?.artists} />} />
+        <Main
+            useGradient={!type}
+            fetchStatus={fetchStatus}
+            content={
+                <MainContent
+                    popularArtists={popularArtistsList}
+                    popularAlbums={popularAlbumsList}
+                />
+            }
+        />
     );
 };
