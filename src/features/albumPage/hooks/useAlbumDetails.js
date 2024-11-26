e15323dff775b02ccb871ecf5dd0baa84cd5bfa2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-26 09:27:39 +0100

    getting album's data, status states and API configuration using useApiResources in useAlbumDetails

--- src/features/albumPage/hooks/useAlbumDetails.js
+++ src/features/albumPage/hooks/useAlbumDetails.js
@@ -1,23 +1,21 @@
-import { useSelector } from "react-redux";
 import { useFetchAPI } from "../../../common/hooks/useFetchAPI";
 import { albumDetailsActions, albumDetailsSelectors } from "../slices/albumDetailsSlice";
 import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
+import { useApiResources } from "../../../common/hooks/useApiResources";
 
 export const useAlbumDetails = (id) => {
-    const { fetch: fetchAlbumDetails, clear: clearAlbumDetails } = albumDetailsActions;
-
-    useFetchAPI(
-        [{ fetchAction: fetchAlbumDetails, clearAction: clearAlbumDetails, endpoint: `albums/${id}` }],
-        [id],
-    );
-
-    const albumDetailsStatus = useSelector(albumDetailsSelectors.selectStatus);
-    const rawAlbumDetails = useSelector(albumDetailsSelectors.selectDatas)?.datas;
+    const { configs, datas: rawAlbumDetails, statuses: albumDetailsStatus } = useApiResources([{
+        action: albumDetailsActions,
+        selectors: albumDetailsSelectors,
+        endpoint: `albums/${id}`
+    }]);
 
     const filteredAlbumData = getSpecificKeys(
         rawAlbumDetails,
         ["name", "images", "type", "release_date", "copyrights", "total_tracks", "tracks", "artists"]
     );
+
+    useFetchAPI([...configs], [id]);
 
     return { filteredAlbumData, albumDetailsStatus };
 };
