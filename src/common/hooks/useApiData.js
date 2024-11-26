51c85b6e0cf093e5568e63fedde6b967429e3ad4
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-26 09:19:58 +0100

    Replaced useApiData with useApiResources, which handles creating configurations, managing status, and managing data for multiple API states, instead of handling only one state per hook call

--- src/common/hooks/useApiData.js
+++ src/common/hooks/useApiData.js
@@ -1,12 +1,30 @@
 import { useSelector } from "react-redux";
+import { formatApiResources } from "../functions/formatApiResources";
 
-export const useApiData = (artistActions, artistSelectors, endpoint) => {
-    const { fetch, clear } = artistActions;
+// export const useApiData = ({ action, selectors, endpoint }) => {
+//     const { fetch, clear } = action;
 
-    const status = useSelector(artistSelectors.selectStatus);
-    const datas = useSelector(artistSelectors.selectDatas)?.datas;
+//     const status = useSelector(selectors.selectStatus);
+//     const datas = useSelector(selectors.selectDatas)?.datas;
 
-    const configs = { fetchAction: fetch, clearAction: clear, endpoint };
+//     const configs = { fetchAction: fetch, clearAction: clear, endpoint };
 
-    return { configs, status, datas };
+//     return { configs, status, datas };
+// };
+
+export const useApiResources = (configsList) => {
+    const select = useSelector;
+    
+    const apiResources = configsList.map(({ action, selectors, endpoint }) => {
+        const { fetch, clear } = action;
+
+        const status = select(selectors.selectStatus);
+        const datas = select(selectors.selectDatas)?.datas;
+
+        const configs = { fetchAction: fetch, clearAction: clear, endpoint };
+
+        return { configs, status, datas };
+    });
+
+    return formatApiResources(apiResources)
 };
