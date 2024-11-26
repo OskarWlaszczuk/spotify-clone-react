213b1aa3f3105bd9d60c6028b67e6ec99e84ed1e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-26 09:15:17 +0100

    Filtering only necessary properties from rawAlbumDetails using getSpecificKeys function call, removed formatAlbumDetails declaration, since it's not used in useAlbumDetails

--- src/features/albumPage/hooks/useAlbumDetails.js
+++ src/features/albumPage/hooks/useAlbumDetails.js
@@ -1,34 +1,23 @@
 import { useSelector } from "react-redux";
 import { useFetchAPI } from "../../../common/hooks/useFetchAPI";
 import { albumDetailsActions, albumDetailsSelectors } from "../slices/albumDetailsSlice";
-import { getImage } from "../../../common/functions/getImage";
+import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
 
-export const useAlbumDetails = (albumID) => {
-
-    const formatAlbumDetails = (rawAlbumDetails) => {
-        return {
-            name: rawAlbumDetails?.name,
-            image: getImage(rawAlbumDetails?.images),
-            type: rawAlbumDetails?.album_type,
-            releaseDate: rawAlbumDetails?.release_date,
-            copyrights: rawAlbumDetails?.copyrights,
-            totalTracksNumber: rawAlbumDetails?.total_tracks,
-            tracksList: rawAlbumDetails?.tracks.items,
-            artistsList: rawAlbumDetails?.artists,
-        };
-    };
-
+export const useAlbumDetails = (id) => {
     const { fetch: fetchAlbumDetails, clear: clearAlbumDetails } = albumDetailsActions;
 
     useFetchAPI(
-        [{ fetchAction: fetchAlbumDetails, clearAction: clearAlbumDetails, endpoint: `albums/${albumID}` }],
-        [albumID],
+        [{ fetchAction: fetchAlbumDetails, clearAction: clearAlbumDetails, endpoint: `albums/${id}` }],
+        [id],
     );
 
     const albumDetailsStatus = useSelector(albumDetailsSelectors.selectStatus);
     const rawAlbumDetails = useSelector(albumDetailsSelectors.selectDatas)?.datas;
 
-    const formattedAlbumDetails = formatAlbumDetails(rawAlbumDetails);
+    const filteredAlbumData = getSpecificKeys(
+        rawAlbumDetails,
+        ["name", "images", "type", "release_date", "copyrights", "total_tracks", "tracks", "artists"]
+    );
 
-    return { formattedAlbumDetails, albumDetailsStatus };
+    return { filteredAlbumData, albumDetailsStatus };
 };
