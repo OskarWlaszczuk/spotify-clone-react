34ea53d55ff1adc2e06fa107aba4e5b40e8f1dd4
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-26 06:59:54 +0100

    filtering on album_group key instead of album_type in filterByAlbumGroup

--- src/common/functions/filterByAlbumGroup.js
+++ src/common/functions/filterByAlbumGroup.js
@@ -1,3 +1,3 @@
 import { isMatch } from "./isMatch"
 
-export const filterByAlbumGroup = (album, targetAlbumGroup) => album?.filter(({ album_type }) => isMatch(album_type, targetAlbumGroup));
+export const filterByAlbumGroup = (album, targetAlbumGroup) => album?.filter(({ album_group }) => isMatch(album_group, targetAlbumGroup));
