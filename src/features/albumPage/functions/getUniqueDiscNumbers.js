f733e6fd75f1659ef16e7d6c938291625b8596bd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 10:36:38 +0100

    Wrapped removeDuplicates's argument into object, passed albumTracksDiscNumbersDuplicatesList as list key in getUniqueDiscNumbers function.

--- src/features/albumPage/functions/getUniqueDiscNumbers.js
+++ src/features/albumPage/functions/getUniqueDiscNumbers.js
@@ -2,7 +2,7 @@ import { removeDuplicates } from "../../../common/functions/removeDuplicates";
 
 export const getUniqueDiscNumbers = (tracksList) => {
     const albumTracksDiscNumbersDuplicatesList = tracksList?.map(({ disc_number }) => disc_number);
-    const uniqueAlbumTracksDiscNumbers = removeDuplicates(albumTracksDiscNumbersDuplicatesList);
+    const uniqueAlbumTracksDiscNumbers = removeDuplicates({list:albumTracksDiscNumbersDuplicatesList});
 
     return uniqueAlbumTracksDiscNumbers;
 };
