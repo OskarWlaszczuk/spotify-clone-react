6dc5aa8742a9d958244d775a53ad88f62400d15c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 11:02:48 +0100

    When the lyrics are fetching, setting lyrics state to ""

--- src/features/trackDetailsPage/hooks/useLyrics.js
+++ src/features/trackDetailsPage/hooks/useLyrics.js
@@ -1,29 +1,26 @@
 import { useEffect, useState } from "react";
-import { error, initial, loading, success } from "../../../common/constants/fetchStatuses";
 
 export const useLyrics = (artist, track) => {
     const [lyrics, setLyrics] = useState(undefined);
-    const [lyricsFetchStatus, setLyricsFetchStatus] = useState(initial);
 
     useEffect(() => {
         const getLyricsForTrack = async (artist, track) => {
             try {
+                setLyrics("");
                 const response = await fetch(`http://localhost:5000/lyrics?artist=${artist}&track=${track}`);
                 const data = await response.json();
 
                 setLyrics(data.lyrics);
-                setLyricsFetchStatus(success);
-            } catch {
-                setLyricsFetchStatus(error);
+            } catch (error) {
+                console.error(error, "Błąd przy fetchoaniu napisów");
             }
         };
 
-        if (artist && track) {
+        if (!!artist && !!track) {
             getLyricsForTrack(artist, track);
-            setLyricsFetchStatus(loading);
         }
 
     }, [artist, track]);
 
-    return { lyrics, lyricsFetchStatus };
+    return { lyrics };
 };
