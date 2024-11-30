03008e392ea5899b5d48c4beca086c8bae892907
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 11:06:15 +0100

    Deleted seperated file for formatLyrics, moved the function to TrackLyricsSection's file, since it's only used there

--- src/features/trackDetailsPage/functions/formatLyrics.js
+++
@@ -1,7 +0,0 @@
-import { LyricsLine } from "../components/MainContent/Lyrics/styled";
-
-export const formatLyrics = (lyrics) => {
-    return lyrics?.split('\n').map((line, index) => (
-        <LyricsLine key={index}>{line}</LyricsLine>
-    ));
-};
