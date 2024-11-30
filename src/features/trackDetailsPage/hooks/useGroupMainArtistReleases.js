2fedde6e7d2f99c389572f3f552c4edae28e0230
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 11:03:30 +0100

    Created hook to group and generate unique IDs for main artist releases.

---
+++ src/features/trackDetailsPage/hooks/useGroupMainArtistReleases.js
@@ -0,0 +1,26 @@
+import { albumsParamDiscography, allReleaseParamDiscography, singleParamDiscography } from "../../../common/constants/params";
+import { filterReleasesByGroups } from "../../../common/functions/filterReleasesByGroups";
+import { removeDuplicates } from "../../../common/functions/removeDuplicates";
+import { useGenerateUniqueListId  } from "../../../common/hooks/useGenerateUniqueListId";
+
+export const useGroupMainArtistReleases = ({ mainArtistAllReleasesData, topTracksList,trackId }) => {
+
+    const mainArtistAllReleasesList = mainArtistAllReleasesData?.items;
+
+    const topTracksAsAlbumsList = topTracksList?.tracks.map(({ album }) => album);
+    const popularReleases = [...topTracksAsAlbumsList || [], ...mainArtistAllReleasesList || []];
+    const uniquePopularRelease = removeDuplicates({ list: popularReleases, key: "name" });
+    const [mainArtistAlbums, mainArtistSingles] = filterReleasesByGroups(mainArtistAllReleasesList, ["album", "single"]);
+
+    const updatedUniquePopularRelease = useGenerateUniqueListId (uniquePopularRelease,trackId);
+    const updatedMainArtistAlbums = useGenerateUniqueListId (mainArtistAlbums,trackId);
+    const updatedMainArtistSingles = useGenerateUniqueListId (mainArtistSingles,trackId);
+
+    const mainArtistGroupedReleasesList = [
+        { type: "Releases", list: updatedUniquePopularRelease, additionalPath: allReleaseParamDiscography },
+        { type: "Albums", list: updatedMainArtistAlbums, additionalPath: albumsParamDiscography },
+        { type: "Singles and EP's", list: updatedMainArtistSingles, additionalPath: singleParamDiscography },
+    ];
+
+    return mainArtistGroupedReleasesList
+};
