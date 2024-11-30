d4d3264244fc59593b991c76b363beeda0c41304
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 10:30:17 +0100

    Removed useMemoizeEndpoint hook, useDependentFetchAPI hook now mange multiple API fetches instead of only one. It minimize hook's calls and centralizes fetching dependent API in one place.

--- src/common/hooks/useDependentFetchAPI.js
+++ src/common/hooks/useDependentFetchAPI.js
@@ -1,41 +1,79 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchFromAPI } from "../functions/fetchFromAPI";
 import { error, initial, loading, success } from "../constants/fetchStatuses";
 import { useSelector } from "react-redux";
 import { selectAccessToken } from "../slices/authSlice";
 
-const useMemoizeEndpoint = (endpoint, dependencies) => useMemo(() => endpoint, dependencies);
+export const useDependentFetchAPI = ({ endpointsList, fetchCondition, dependencies = [] }) => {
 
-export const useDependentFetchAPI = ({ endpointConfig: { routePath, params }, fetchCondition, dependencies = [] }) => {
     const accessToken = useSelector(selectAccessToken);
-    const memoizedEndpoint = useMemoizeEndpoint(routePath, [params]);
 
-    const [datas, setDatas] = useState(null);
-    const [datasStatus, setDatasStatus] = useState(initial);
+    const [depentendApiData, setDepentendApiData] = useState(null);
+    const [depentendApiDataStatus, setDepentendApiDataStatus] = useState(initial);
 
     useEffect(() => {
         if (!fetchCondition || !accessToken) return;
 
-        const fetchArtistDetails = async () => {
-            setDatasStatus(loading);
+        const fetchDependData = async () => {
+            setDepentendApiDataStatus(loading);
             try {
-                const response = await fetchFromAPI({ endpoint: memoizedEndpoint, accessToken });
-                setDatas(response);
-                setDatasStatus(success);
+                const response = await Promise.all(endpointsList.map(({ endpoint }) => {
+                    return fetchFromAPI({ endpoint, accessToken })
+                }))
+
+                setDepentendApiData(response);
+                setDepentendApiDataStatus(success);
             } catch {
-                setDatasStatus(error);
+                setDepentendApiDataStatus(error);
             }
         };
 
-        fetchArtistDetails();
+        fetchDependData();
 
         return () => {
-            setDatas(null);
-            setDatasStatus(initial);
+            setDepentendApiData(null);
+            setDepentendApiDataStatus(initial);
         };
 
-    }, [...dependencies, accessToken, fetchCondition, memoizedEndpoint]);
+    }, [...dependencies, accessToken, fetchCondition]);
 
 
-    return { datas, datasStatus };
+    return {  depentendApiData,  depentendApiDataStatus };
 };
+
+// const useMemoizeEndpoint = (endpoint, dependencies) => useMemo(() => endpoint, dependencies);
+
+// export const useDependentFetchAPI = ({ endpointConfig: { routePath, params }, fetchCondition, dependencies = [] }) => {
+    
+//     const accessToken = useSelector(selectAccessToken);
+//     const memoizedEndpoint = useMemoizeEndpoint(routePath, params);
+
+//     const [data, setData] = useState(null);
+//     const [dataStatus, setDataStatus] = useState(initial);
+
+//     useEffect(() => {
+//         if (!fetchCondition || !accessToken) return;
+
+//         const fetchArtistDetails = async () => {
+//             setDataStatus(loading);
+//             try {
+//                 const response = await fetchFromAPI({ endpoint: memoizedEndpoint, accessToken });
+//                 setData(response);
+//                 setDataStatus(success);
+//             } catch {
+//                 setDataStatus(error);
+//             }
+//         };
+
+//         fetchArtistDetails();
+
+//         return () => {
+//             setData(null);
+//             setDataStatus(initial);
+//         };
+
+//     }, [...dependencies, accessToken, fetchCondition, memoizedEndpoint]);
+
+
+//     return { data, dataStatus };
+// };
