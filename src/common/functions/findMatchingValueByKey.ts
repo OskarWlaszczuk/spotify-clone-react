a0dc0eca9e6eb7896a11cfe336d59bc9c6fbaf2e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 09:56:56 +0100

    Body of findMatchingValueByKey function formatted.

--- src/common/functions/findMatchingValueByKey.ts
+++ src/common/functions/findMatchingValueByKey.ts
@@ -1,6 +1,9 @@
 import { DataByGroup } from "../interfaces/DataByGroup";
 import { isMatch } from "./isMatch";
 
-export const findMatchingValueByKey = <ValueType>(groups: DataByGroup<ValueType>[], targetKey: string): DataByGroup<ValueType> | undefined => (
-    groups.find(({ key }) => isMatch(key, targetKey))
-);
+export const findMatchingValueByKey = <ValueType>(
+    groups: DataByGroup<ValueType>[],
+    targetKey: string
+): DataByGroup<ValueType> | undefined => {
+    return groups.find(({ key }) => isMatch(key, targetKey))
+}
