4ee0269cf5c743eca61b56b3a6fb95ac97249e87
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-11-30 10:24:39 +0100

    Renamed useArtistPopularReleases to useArtistAllReleases

--- src/common/hooks/useArtistAllReleases.js
+++ src/common/hooks/useArtistAllReleases.js
@@ -5,7 +5,7 @@ export const useArtistAllReleases = (artistID) => {
     const { fetch: fetchMainArtistAllReleasesList, clear: clearMainArtistAllReleasesList } = artistAlbumsActions;
 
     const artistAllReleasesStatus = useSelector(artistAlbumsSelectors.selectStatus);
-    const artistAllReleasesList = useSelector(artistAlbumsSelectors.selectDatas)?.datas.items;
+    const artistAllReleasesList = useSelector(artistAlbumsSelectors.selectData)?.data.items;
 
     const configs = { fetchAction: fetchMainArtistAllReleasesList, clearAction: clearMainArtistAllReleasesList, endpoint: `artists/${artistID}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation&limit=50` };
 
