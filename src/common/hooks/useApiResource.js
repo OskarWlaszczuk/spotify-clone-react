0374e3b53fbb785300c275b659a9db04d9e44b20
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-03 09:35:22 +0100

    Renamed useApiResources to useApiResource. Now the hook manage selecting redux data and creating  config to fetching  for only one element. It's beacuse limitations and problems which result from using useSelector hook inside map iteration.

---
+++ src/common/hooks/useApiResource.js
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+export const useApiResource = ({ actions, selectors, endpoint }) => {
+
+    const { fetch, clear } = actions;
+    const { selectStatus, selectData } = selectors;
+
+    const apiStatus = useSelector(selectStatus);
+    const apiData = useSelector(selectData)?.data;
+
+    const configs = { fetchAction: fetch, clearAction: clear, endpoint };
+    return { configs, apiStatus, apiData };
+};
