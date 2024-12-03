1130e4f55d628b943949bdcecf604ce2540ccd85
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-03 09:48:00 +0100

    Removed "images" from keysToGetList argument of getSpecificKeys for filteredMainArtistData constant in getFilteredTrackData function

--- src/features/trackDetailsPage/functions/getFilteredTrackData.js
+++ src/features/trackDetailsPage/functions/getFilteredTrackData.js
@@ -3,7 +3,7 @@ import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
 export const getFilteredTrackData = (trackData) => {
     const filteredTrackData = getSpecificKeys(trackData, ["name", "type", "duration_ms", "popularity", "artists"]);
     const filteredAlbumData = getSpecificKeys(trackData?.album, ["name", "release_date", "id", "images"]);
-    const filteredMainArtistData = getSpecificKeys(trackData?.artists[0], ["name", "id", "images"]);
+    const filteredMainArtistData = getSpecificKeys(trackData?.artists[0], ["name", "id"]);
 
     return [...filteredTrackData, ...filteredAlbumData, ...filteredMainArtistData];
 };
