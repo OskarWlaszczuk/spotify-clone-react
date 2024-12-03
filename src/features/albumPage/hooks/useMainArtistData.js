ceaa839ea78041b956a65eec233e82699971408e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-03 08:19:53 +0100

    Merged useMainArtistData and useMainArtistReleases hooks into one common hook to limit useDependentFetchAPI calls and fetch main artist's API data like its details or releases in one place on album page.

--- src/features/albumPage/hooks/useMainArtistData.js
+++ src/features/albumPage/hooks/useMainArtistData.js
@@ -1,19 +1,35 @@
+import { useMemo } from "react";
+import { getArtistReleasesEndpointResource } from "../../../common/functions/getArtistReleasesEndpointResource";
 import { useDependentFetchAPI } from "../../../common/hooks/useDependentFetchAPI";
 import { useMemoizeEndpointsList } from "../../../common/hooks/useMemoizeEndpointsList";
 
-export const useMainArtistData = ({ mainArtistId, fetchCondition, dependencies }) => {
+export const useMainArtistData = ({ mainArtistId, fetchCondition, dependencies, isAlbumArtistsListLengthEqualsOne }) => {
+
+    const mainArtistReleasesMemoizedEndpoint = useMemoizeEndpointsList(
+        `artists/${mainArtistId}/${getArtistReleasesEndpointResource()}`,
+        [mainArtistId, getArtistReleasesEndpointResource()]
+    );
     const mainArtistMemoizedEndpoint = useMemoizeEndpointsList(`artists/${mainArtistId}`, [mainArtistId]);
 
+    const memoizedMainArtistEndpointsList = useMemo(() => (
+        [...mainArtistReleasesMemoizedEndpoint, ...mainArtistMemoizedEndpoint]
+    ), [mainArtistReleasesMemoizedEndpoint, mainArtistMemoizedEndpoint]);
+
     const {
-        depentendApiData: rawMainArtistData,
+        depentendApiData: mainArtistData,
         depentendApiDataStatus: mainArtistDataStatus
     } = useDependentFetchAPI({
-        endpointsList: mainArtistMemoizedEndpoint,
+        endpointsList: (
+            isAlbumArtistsListLengthEqualsOne ?
+                memoizedMainArtistEndpointsList :
+                mainArtistReleasesMemoizedEndpoint
+        ),
         fetchCondition,
         dependencies,
     });
 
-    const mainArtistImage = rawMainArtistData?.[0].images;
+    const mainArtistAllReleasesList = mainArtistData?.[0]?.items;
+    const mainArtistImage = mainArtistData?.[1]?.images;
 
-    return { mainArtistImage, mainArtistDataStatus };
+    return { mainArtistImage, mainArtistAllReleasesList, mainArtistDataStatus };
 };
