3bba534dc07a988dbee7e3eb24804e0eab50a589
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-04 20:13:35 +0100

    Replaced useMatchFullListDataByType with getFullListMatchedData call in HomePage, declared fullListsDataOptions as constant before passing it to getFullListMatchedData.

--- src/features/homePage/components/MainContent/index.js
+++ src/features/homePage/components/MainContent/index.js
@@ -1,28 +1,27 @@
 import { useParams } from "react-router-dom"
 import { toHome, toArtist, toAlbum } from "../../../../common/functions/routes";
-import { useMatchFullListDataByType } from "../../../../common/hooks/useMatchFullListDataByType";
+import { getFullListMatchedData } from "../../../../common/functions/getFullListMatchedData";
 import { fullListLinkText } from "../../../../common/constants/fullListLinkText ";
 import { popularAlbumsParam, popularArtistsParam } from "../../../../common/constants/params";
 import { useRenderTilesList } from "../../../../common/functions/useRenderTilesList";
-import { useGenerateUniqueListId  } from "../../../../common/hooks/useGenerateUniqueListId";
+import { useGenerateUniqueListId } from "../../../../common/hooks/useGenerateUniqueListId";
 
 export const MainContent = ({ popularArtists, popularAlbums }) => {
     const { type = "" } = useParams();
 
     const renderTilesList = useRenderTilesList();
-    const updatedPopularArtists = useGenerateUniqueListId (popularArtists);
-    const updatedPopularAlbums = useGenerateUniqueListId (popularAlbums);
+    const updatedPopularArtists = useGenerateUniqueListId(popularArtists);
+    const updatedPopularAlbums = useGenerateUniqueListId(popularAlbums);
 
     const popularAlbumsTitle = "Popular albums";
     const popularArtistsTitle = "Popular artists";
 
-    const { fullListContent, fullListTitle, isFullListArtistsList } = useMatchFullListDataByType(
-        [
-            { key: popularAlbumsParam, value: popularAlbums, title: popularAlbumsTitle, isArtistsList: false },
-            { key: popularArtistsParam, value: popularArtists, title: popularArtistsTitle, isArtistsList: true },
-        ],
-        type
-    );
+    const fullListsDataOptions = [
+        { key: popularAlbumsParam, value: popularAlbums, title: popularAlbumsTitle, isArtistsList: false },
+        { key: popularArtistsParam, value: popularArtists, title: popularArtistsTitle, isArtistsList: true },
+    ];
+
+    const { fullListContent, fullListTitle, isFullListArtistsList } = getFullListMatchedData(fullListsDataOptions, type);
 
     return (
         <>
