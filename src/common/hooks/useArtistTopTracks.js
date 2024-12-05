4cd74f8971bf5380fb0d7c353e9d8caa39fd010f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-05 19:29:13 +0100

    Replaced useDependentFetchAPI with useApiResource and useFetchAPI, removed unneccesery dependencies parameter in useArtistTopTracks.

--- src/common/hooks/useArtistTopTracks.js
+++ src/common/hooks/useArtistTopTracks.js
@@ -1,21 +1,27 @@
-import { useDependentFetchAPI } from "./useDependentFetchAPI";
-import { useMemoizeEndpointsList } from "./useMemoizeEndpointsList";
+import { artistTopTracksActions, artistTopTracksSelectors } from "../../features/artistDetailsPage/slices/artistTopTracksSlice";
+import { useApiResource } from "./useApiResource";
+import { useFetchAPI } from "./useFetchAPI";
 
-export const useArtistTopTracks = ({ artistId, dependencies = [] }) => {
-
-    const memoizedArtistTopTracksEndpoint = useMemoizeEndpointsList(`artists/${artistId}/top-tracks`, [artistId]);
+export const useArtistTopTracks = ({ artistId }) => {
 
     const {
-        depentendApiData: rawArtistTopTracksDataList,
-        depentendApiDataStatus: artistTopTracksDataListStatus
-    } = useDependentFetchAPI({
-        endpointsList: memoizedArtistTopTracksEndpoint,
-        fetchCondition: !!artistId,
-        dependencies: [...dependencies],
+        configs: artistTopTracksConfig,
+        apiStatus: artistTopTracksStatus,
+        rawApiData: artistTopTracks
+    } = useApiResource({
+        actions: artistTopTracksActions,
+        selectors: artistTopTracksSelectors,
+        endpoint: `artists/${artistId}/top-tracks`,
     });
 
-    const artistTopTracksList = rawArtistTopTracksDataList?.[0].tracks;
+    useFetchAPI({
+        fetchConfigs: [artistTopTracksConfig],
+        dependencies: [artistId],
+        fetchCondition: !!artistId
+    });
+
+    const artistTopTracksList = artistTopTracks?.tracks;
     const artistTopTracksAsAlbumsList = artistTopTracksList?.map(({ album }) => album);
 
-    return { artistTopTracksDataListStatus, artistTopTracksAsAlbumsList, artistTopTracksList };
+    return { artistTopTracksStatus, artistTopTracksAsAlbumsList, artistTopTracksList };
 };
