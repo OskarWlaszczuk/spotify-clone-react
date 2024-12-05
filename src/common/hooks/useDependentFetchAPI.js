7733d461169ae7dbf5c763381ef521dd07dd8583
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-05 19:30:15 +0100

    Temporarily commanted useDependentFetchAPI file

--- src/common/hooks/useDependentFetchAPI.js
+++ src/common/hooks/useDependentFetchAPI.js
@@ -1,41 +1,90 @@
-import { useEffect, useState } from "react";
-import { fetchFromAPI } from "../functions/fetchFromAPI";
-import { error, initial, loading, success } from "../constants/fetchStatuses";
-import { useSelector } from "react-redux";
-import { selectAccessToken } from "../slices/authSlice";
+// import { useEffect, useState } from "react";
+// import { fetchFromAPI } from "../functions/fetchFromAPI";
+// import { error, initial, loading, success } from "../constants/fetchStatuses";
+// import { useSelector } from "react-redux";
+// import { selectAccessToken } from "../slices/authSlice";
+// import { useFetchAPI } from "./useFetchAPI";
 
-export const useDependentFetchAPI = ({ endpointsList, fetchCondition, dependencies = [] }) => {
-    const accessToken = useSelector(selectAccessToken);
+// export const useDependentFetchAPI2 = ({ configsList = [], fetchCondition, dependencies }) => {
+//     const accessToken = useSelector(selectAccessToken);
 
-    const [depentendApiData, setDepentendApiData] = useState(null);
-    const [depentendApiDataStatus, setDepentendApiDataStatus] = useState(initial);
+//     useFetchAPI(configsList, dependencies, fetchCondition)
 
-    useEffect(() => {
-        if (!fetchCondition || !accessToken) return;
-
-        const fetchDependData = async () => {
-            setDepentendApiDataStatus(loading);
-            try {
-                const response = await Promise.all(endpointsList.map(({ endpoint }) => {
-                    return fetchFromAPI({ endpoint, accessToken })
-                }))
-
-                setDepentendApiData(response);
-                setDepentendApiDataStatus(success);
-            } catch {
-                setDepentendApiDataStatus(error);
-            }
-        };
-
-        fetchDependData();
-
-        return () => {
-            setDepentendApiData(null);
-            setDepentendApiDataStatus(initial);
-        };
-
-    }, [...dependencies, accessToken, fetchCondition, endpointsList]);
+//     // const [depentendApiData, setDepentendApiData] = useState(null);
+//     // const [depentendApiDataStatus, setDepentendApiDataStatus] = useState(initial);
 
 
-    return { depentendApiData, depentendApiDataStatus };
-};
+
+//     // useEffect(() => {
+//     //     if (!fetchCondition || !accessToken) return;
+
+//     //     const fetchDependData = async () => {
+//     //         setDepentendApiDataStatus(loading);
+//     //         try {
+//     //             const response = await Promise.all(endpointsList.map(({ endpoint }) => {
+//     //                 return fetchFromAPI({ endpoint, accessToken })
+//     //             }))
+
+//     //             setDepentendApiData(response);
+//     //             setDepentendApiDataStatus(success);
+//     //         } catch {
+//     //             setDepentendApiDataStatus(error);
+//     //         }
+//     //     };
+
+//     //     fetchDependData();
+
+//     //     return () => {
+//     //         setDepentendApiData(null);
+//     //         setDepentendApiDataStatus(initial);
+//     //     };
+
+//     // }, [...dependencies, accessToken, fetchCondition]);
+
+
+//     // return { depentendApiData, depentendApiDataStatus };
+
+// };
+
+
+
+
+
+
+
+
+// export const useDependentFetchAPI = ({ endpointsList, fetchCondition, dependencies = [] }) => {
+//     const accessToken = useSelector(selectAccessToken);
+
+//     const [depentendApiData, setDepentendApiData] = useState(null);
+//     const [depentendApiDataStatus, setDepentendApiDataStatus] = useState(initial);
+
+//     useEffect(() => {
+//         if (!fetchCondition || !accessToken) return;
+
+//         const fetchDependData = async () => {
+//             setDepentendApiDataStatus(loading);
+//             try {
+//                 const response = await Promise.all(endpointsList.map(({ endpoint }) => {
+//                     return fetchFromAPI({ endpoint, accessToken })
+//                 }))
+
+//                 setDepentendApiData(response);
+//                 setDepentendApiDataStatus(success);
+//             } catch {
+//                 setDepentendApiDataStatus(error);
+//             }
+//         };
+
+//         fetchDependData();
+
+//         return () => {
+//             setDepentendApiData(null);
+//             setDepentendApiDataStatus(initial);
+//         };
+
+//     }, [...dependencies, accessToken, fetchCondition]);
+
+
+//     return { depentendApiData, depentendApiDataStatus };
+// };
