75929a4b67784718c21bfad977ce692f70a1ec16
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-06 19:00:44 +0100

    Removed useGenerateUniqueListId's calls and updated lists constant retuned by this hook. Added instead extra listId property to each object in mainArtistGroupedReleasesList list

--- src/features/trackDetailsPage/hooks/useGroupMainArtistReleases.js
+++ src/features/trackDetailsPage/hooks/useGroupMainArtistReleases.js
@@ -1,25 +1,20 @@
 import { albumsParamDiscography, allReleaseParamDiscography, singleParamDiscography } from "../../../common/constants/params";
 import { filterReleasesByGroups } from "../../../common/functions/filterReleasesByGroups";
 import { removeDuplicates } from "../../../common/functions/removeDuplicates";
-import { useGenerateUniqueListId } from "../../../common/hooks/useGenerateUniqueListId";
 
 export const useGroupMainArtistReleases = ({ mainArtistAllReleasesData, topTracksList, trackId }) => {
-
     const mainArtistAllReleasesList = mainArtistAllReleasesData?.items;
 
     const topTracksAsAlbumsList = topTracksList?.tracks.map(({ album }) => album);
     const popularReleases = [...topTracksAsAlbumsList || [], ...mainArtistAllReleasesList || []];
     const uniquePopularRelease = removeDuplicates({ list: popularReleases, key: "name" });
+
     const [mainArtistAlbums, mainArtistSingles] = filterReleasesByGroups(mainArtistAllReleasesList, ["album", "single"]);
 
-    const updatedUniquePopularRelease = useGenerateUniqueListId(uniquePopularRelease);
-    const updatedMainArtistAlbums = useGenerateUniqueListId(mainArtistAlbums);
-    const updatedMainArtistSingles = useGenerateUniqueListId(mainArtistSingles);
-
     const mainArtistGroupedReleasesList = [
-        { type: "Releases", list: updatedUniquePopularRelease, additionalPath: allReleaseParamDiscography },
-        { type: "Albums", list: updatedMainArtistAlbums, additionalPath: albumsParamDiscography },
-        { type: "Singles and EP's", list: updatedMainArtistSingles, additionalPath: singleParamDiscography },
+        { type: "Releases", list: uniquePopularRelease, additionalPath: allReleaseParamDiscography, listId: 0 },
+        { type: "Albums", list: mainArtistAlbums, additionalPath: albumsParamDiscography, listId: 1 },
+        { type: "Singles and EP's", list: mainArtistSingles, additionalPath: singleParamDiscography, listId: 2 },
     ];
 
     return mainArtistGroupedReleasesList
