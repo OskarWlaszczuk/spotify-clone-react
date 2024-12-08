5cf34a880d977e9b74c7e766bfadaa633e4170e9
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-08 10:05:08 +0100

    Formatted code inside useCurrentCategoryData.

--- src/features/artistDetailsPage/hooks/useCurrentCategoryData.js
+++ src/features/artistDetailsPage/hooks/useCurrentCategoryData.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 export const useCurrentCategoryData = (initialCategoryData) => {
 
-    const [currentCategoryData, setCurrentCategoryData] = useState({ category: initialCategoryData.key, list: initialCategoryData.value });
+    const [currentCategoryData, setCurrentCategoryData] = useState({ 
+        category: initialCategoryData.key, 
+        list: initialCategoryData.value 
+    });
 
     return { currentCategoryData, setCurrentCategoryData };
 };
