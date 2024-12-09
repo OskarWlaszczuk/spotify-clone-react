83937b41bbf5e23e0ef188d0f0bd7765274848ae
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-09 18:58:39 +0100

    Moved convertToMinutesAndSeconds and convertMinutesToHours functions inside calculateTotalDuration.js file, because these functions are only used there as utility functions.

--- src/common/functions/convertMinutesToHours.ts
+++
@@ -1,6 +0,0 @@
-export const convertMinutesToHours = (timeInMinutes: number) => {
-    const hours = Math.floor(timeInMinutes / 60);
-    const remainingMinutes = (timeInMinutes % 60).toFixed(0);
-
-    return `${hours}hr${remainingMinutes}min`;
-};
--- src/features/albumPage/functions/calculateTotalDuration.js
+++ src/features/albumPage/functions/calculateTotalDuration.js
@@ -1,6 +1,24 @@
-import { convertMinutesToHours } from "../../../common/functions/convertMinutesToHours";
-import { convertToMinutesAndSeconds } from "../../../common/functions/convertToMinutesAndSeconds";
 import { fromMillisecondsToMinutes } from "../../../common/functions/fromMillisecondsToMinutes";
+
+const convertToMinutesAndSeconds = (time) => {
+    const minutes = Math.floor(time);
+    let seconds = Math.round((time % 1) * 100);
+
+    if (seconds >= 60) {
+        const extraMinutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        return `${minutes + extraMinutes} min ${seconds > 0 && seconds} sec`;
+    }
+
+    return `${minutes} min ${seconds} sec`;
+};
+
+const convertMinutesToHours = (timeInMinutes) => {
+    const hours = Math.floor(timeInMinutes / 60);
+    const remainingMinutes = (timeInMinutes % 60).toFixed(0);
+
+    return `${hours}hr${remainingMinutes}min`;
+};
 
 export const calculateTotalDuration = (tracksList) => {
     const albumTracksDurations = tracksList?.map(({ duration_ms }) => duration_ms);
