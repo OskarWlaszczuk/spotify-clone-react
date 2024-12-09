517fb7fd99a9686384be31dcf2c0f251ec9c71d3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-09 20:13:25 +0100

    Moved utility function replaceReleaseDateIfCurrentYear inside preparePopularReleases function file.

--- src/features/artistDetailsPage/functions/preparePopularReleases.ts
+++ src/features/artistDetailsPage/functions/preparePopularReleases.ts
@@ -2,18 +2,27 @@ import { isLatestReleased } from "../../../common/functions/isLatestReleased";
 import { removeDuplicates } from "../../../common/functions/removeDuplicates";
 import { sortFromOldestToNewest } from "../../../common/functions/sortFromOldestToNewest";
 import { WithReleaseDate } from "../../../common/interfaces/WithReleaseDate";
+import { newestItemReleaseDate } from "../constants/newestItemReleaseDate";
+
+ const replaceReleaseDateIfCurrentYear = <T extends WithReleaseDate>(listItem: T): T => {
+    return isLatestReleased(listItem) ?
+        { ...listItem, release_date: newestItemReleaseDate } :
+        listItem;
+};
 
 const setNewestPopularReleaseItemFirstIfIsLatestRelease = <T extends WithReleaseDate>(
     newestPopularReleaseItem: T | undefined,
     popularReleases: any
 ) => (
-    newestPopularReleaseItem && isLatestReleased(newestPopularReleaseItem)
-        ? [{ ...newestPopularReleaseItem }, ...(popularReleases?.slice() ?? [])]
+    !!newestPopularReleaseItem && isLatestReleased(newestPopularReleaseItem) ?
+        [replaceReleaseDateIfCurrentYear({ ...newestPopularReleaseItem }), ...(popularReleases?.slice() ?? [])]
         : popularReleases || []
 );
 
 export const preparePopularReleases = (topTracksAlbumsList: any, allReleasesWithoutAppearsOn: any) => {
     const newestTopTrackAlbumItem = sortFromOldestToNewest(topTracksAlbumsList)[0];
+    console.log(replaceReleaseDateIfCurrentYear(newestTopTrackAlbumItem))
+
     const updatedTopTracksAlbumsList = setNewestPopularReleaseItemFirstIfIsLatestRelease(
         newestTopTrackAlbumItem,
         topTracksAlbumsList
