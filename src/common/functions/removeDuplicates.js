971ad05f4e975106dd5b8d37d5067d962f95ffce
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-15 14:01:50 +0100

    Refactor `removeDuplicates` to TypeScript.
    Converted the `removeDuplicates` function from JavaScript to TypeScript for improved type safety and maintainability. Adjusted the implementation to include a generic interface for better flexibility and clarity.

--- src/common/functions/removeDuplicates.js
+++
@@ -1,8 +0,0 @@
-export const removeDuplicates = ({ list, key = "" }) => {
-    const caughtDuplicates = new Set();
-
-    return list?.filter(item => {
-        const keyValue = !!key ? item[key] : item;
-        return !caughtDuplicates.has(keyValue) && caughtDuplicates.add(keyValue);
-    });
-};
