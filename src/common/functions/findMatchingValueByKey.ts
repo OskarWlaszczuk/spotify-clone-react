6ef5c44134ac2aa7e1a6dd949bcb0a859d3ffbf8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-15 14:06:26 +0100

    Refactored filterReleasesByGroups.js file to ts. Updated `findMatchingValueByKey` with improved type definitions and added a new example implementation for clarity.

--- src/common/functions/findMatchingValueByKey.ts
+++ src/common/functions/findMatchingValueByKey.ts
@@ -1,9 +1,40 @@
-import { DataByGroup } from "../interfaces/DataByGroup";
+import { GroupData } from "../Interfaces/GroupData";
 import { isMatch } from "./isMatch";
 
-export const findMatchingValueByKey = <ValueType>(
-    groups: DataByGroup<ValueType>[],
-    targetKey: string
-): DataByGroup<ValueType> | undefined => {
-    return groups.find(({ key }) => isMatch(key, targetKey))
-}
+export const findMatchingValueByKey = (
+    groups: GroupData[],
+    targetKey: string,
+) => (
+    groups.find(({ key }) => isMatch(key, targetKey))
+);
+
+export interface GroupData2 {
+    key: string;
+    value: any;
+    title?: string;
+    isArtistsList?: boolean;
+};
+
+const exampleArray = [
+    {
+        key: "klucz1",
+        value: [1, 2, 3, 4],
+    },
+    {
+        key: "klucz2",
+        value: ["str1", "str2", "str3",],
+    },
+    {
+        key: "klucz3",
+        value: [null, undefined, true, false],
+    },
+]
+
+export const findMatchingValueByKey2 = (
+    groups: GroupData2[],
+    targetKey: string,
+) => (
+    groups.find(({ key }) => isMatch(key, targetKey))
+);
+
+findMatchingValueByKey2(exampleArray, "klucz2");
