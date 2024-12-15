96c73790d7cf15d69c254a569ef6ca6f77ca5268
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-15 13:47:51 +0100

    Refactor `useFetchStatuses` for type safety and readability.
    Updated the `checkFetchStatuses` function to use explicit `FetchStatus` types and renamed variables for clarity. Improved type definitions and consistency across the hook to ensure proper usage and alignment with TypeScript standards.

--- src/common/hooks/useFetchStatuses.ts
+++ src/common/hooks/useFetchStatuses.ts
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 import { initial, loading, success, error } from "../constants/fetchStatuses";
-import { FetchStatus } from "../types/FetchStatus";
+import { FetchStatus } from "../Types/FetchStatus";
+
+type isStatusMatched = boolean;
 
 const checkFetchStatuses = (
-    fetchStatuses: string[],
-    targetStatus: string,
-    matchAll: boolean = false
-): boolean => (
-    matchAll ?
+    fetchStatuses: FetchStatus[],
+    targetStatus: FetchStatus,
+    matchingAll: boolean = false
+): isStatusMatched => (
+    matchingAll ?
         fetchStatuses.every(fetchState => fetchState === targetStatus)
         : fetchStatuses.some(fetchState => fetchState === targetStatus)
 );
 
-export const useFetchStatus = (fetchStatuses: any[] = []): FetchStatus => {
+export const useFetchStatus = (fetchStatuses: FetchStatus[]): FetchStatus => {
     const [fetchStatus, setFetchStatus] = useState<FetchStatus>(initial);
 
     useEffect(() => {
-        const isInitial: boolean = checkFetchStatuses(fetchStatuses, initial);
-        const isLoading: boolean = checkFetchStatuses(fetchStatuses, loading);
-        const isError: boolean = checkFetchStatuses(fetchStatuses, error);
-        const isSucces: boolean = checkFetchStatuses(fetchStatuses, success, true);
+        const isInitial = checkFetchStatuses(fetchStatuses, initial);
+        const isLoading = checkFetchStatuses(fetchStatuses, loading);
+        const isError = checkFetchStatuses(fetchStatuses, error);
+        const isSuccess = checkFetchStatuses(fetchStatuses, success, true);
 
         if (isInitial) setFetchStatus(initial);
         if (isLoading) setFetchStatus(loading);
         if (isError) setFetchStatus(error);
-        if (isSucces) setFetchStatus(success);
+        if (isSuccess) setFetchStatus(success);
 
     }, [fetchStatuses]);
 
