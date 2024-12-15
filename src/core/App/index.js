68d0002c7b1765933f6526ff32280431b0459bc2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-15 14:11:02 +0100

    Normalize code formatting in App component files
    Adjusted spacing and formatting in `index.js` to improve consistency and adhere to style guidelines. No functional changes were made to the application logic.

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,29 +1,29 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import { NavigationPanel } from "./components/NavigationPanel";
-import { toHome, toSearch } from "../../common/functions/routes";
-import { HomePage } from "../../features/homePage/components/HomePage";
-import { Search } from "../../features/Search";
-import { AlbumPage } from "../../features/albumPage/components/AlbumPage";
-import { Library } from "./components/Library";
-import { ArtistDetailsPage } from "../../features/artistDetailsPage/components/ArtistDetailsPage";
-import { TrackDetailsPage } from "../../features/trackDetailsPage/components/TrackDetailsPage";
-import { useAuth } from "./hooks/useAuth";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+import {NavigationPanel} from "./components/NavigationPanel";
+import {toHome, toSearch} from "../../common/functions/routes";
+import {HomePage} from "../../features/homePage/components/HomePage";
+import {Search} from "../../features/Search";
+import {AlbumPage} from "../../features/albumPage/components/AlbumPage";
+import {Library} from "./components/Library";
+import {ArtistDetailsPage} from "../../features/artistDetailsPage/components/ArtistDetailsPage";
+import {TrackDetailsPage} from "../../features/trackDetailsPage/components/TrackDetailsPage";
+import {useAuth} from "./hooks/useAuth";
 
 export const App = () => {
-  useAuth();
-  
-  return (
-    <HashRouter>
-      <NavigationPanel />
-      <Library />
-      <Routes>
-        <Route path="/home/:type?" element={<HomePage />} />
-        <Route path={toSearch()} element={<Search />} />
-        <Route path="/track/:id" element={<TrackDetailsPage />} />
-        <Route path="/album/:id" element={<AlbumPage />} />
-        <Route path="/artist/:id/:type?" element={<ArtistDetailsPage />} />
-        <Route path="*" element={<Navigate to={toHome()} />} />
-      </Routes>
-    </HashRouter>
-  );
+    useAuth();
+
+    return (
+        <HashRouter>
+            <NavigationPanel/>
+            <Library/>
+            <Routes>
+                <Route path="/home/:type?" element={<HomePage/>}/>
+                <Route path={toSearch()} element={<Search/>}/>
+                <Route path="/track/:id" element={<TrackDetailsPage/>}/>
+                <Route path="/album/:id" element={<AlbumPage/>}/>
+                <Route path="/artist/:id/:type?" element={<ArtistDetailsPage/>}/>
+                <Route path="*" element={<Navigate to={toHome()}/>}/>
+            </Routes>
+        </HashRouter>
+    );
 };
