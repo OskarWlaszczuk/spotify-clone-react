3dc2a0e4efb5f205c2f0b889496fe0a563fbf0e7
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-15 14:25:16 +0100

    Remove unused isLatestReleased function
    The isLatestReleased function was deleted as it is no longer in use. Cleaning up unused code helps maintain readability and reduces potential confusion in the codebase.

--- src/common/functions/isLatestReleased.ts
+++
@@ -1,5 +0,0 @@
-import { WithReleaseDate } from "../interfaces/WithReleaseDate";
-
-export const isLatestReleased = <T extends WithReleaseDate>(album: T): boolean => (
-    new Date(album?.release_date).getFullYear() === new Date().getFullYear()
-);
