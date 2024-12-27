a06ebce380851ab0ed8ebd52c594190a96fc2486
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-27 15:24:10 +0100

    Code formatted in HomePage

--- src/features/homePage/components/HomePage/index.js
+++ src/features/homePage/components/HomePage/index.js
@@ -1,25 +1,25 @@
-import { Main } from "../../../../common/components/Main";
-import { useFetchStatus } from "../../../../common/hooks/useFetchStatuses";
-import { MainContent } from "./MainContent";
-import { useFetchAPI } from "../../../../common/hooks/useFetchAPI";
-import { useParams } from "react-router-dom";
-import { usePopularLists } from "../../hooks/usePopularLists";
-import { getSpecificKeys } from "../../../../common/functions/getSpecificKeys";
+import {Main} from "../../../../common/components/Main";
+import {useFetchStatus} from "../../../../common/hooks/useFetchStatuses";
+import {MainContent} from "./MainContent";
+import {useFetchAPI} from "../../../../common/hooks/useFetchAPI";
+import {useParams} from "react-router-dom";
+import {usePopularLists} from "../../hooks/usePopularLists";
+import {getSpecificKeys} from "../../../../common/functions/getSpecificKeys";
 
 export const HomePage = () => {
-    const { type } = useParams();
+    const {type} = useParams();
     const homeId = "home"
 
-    const { configs, apiStatuses, apiDataList } = usePopularLists();
+    const {configs, apiStatuses, apiDataList} = usePopularLists();
 
     const [
-        { albums: popularAlbumsList },
-        { artists: popularArtistsList },
+        {albums: popularAlbumsList},
+        {artists: popularArtistsList},
     ] = getSpecificKeys(apiDataList, ["albums", "artists"]);
 
     const fetchStatus = useFetchStatus([...apiStatuses]);
 
-    useFetchAPI({ fetchConfigs: [...configs], pageId: homeId });
+    useFetchAPI({fetchConfigs: [...configs], pageId: homeId});
 
     return (
         <Main
