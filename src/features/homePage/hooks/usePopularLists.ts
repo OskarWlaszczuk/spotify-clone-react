ae6b90b7106d32c3f8f4cf42960384363b6e8d35
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-12-30 16:46:23 +0100

    Refactores usePopularLists to TS. Added annotations to the popular lists constants and formatPopularListForFetch function parameter. Added new popular ids list with episodes ids.

---
+++ src/features/homePage/hooks/usePopularLists.ts
@@ -0,0 +1,69 @@
+import { getSeveralAlbumsListEndpoint, getSeveralArtistsListEndpoint } from "../../../common/functions/endpoints";
+import { useApiResource } from "../../../common/hooks/useApiResource";
+import { albumsActions, albumsSelectors } from "../../../common/slices/albumsSlice";
+import { artistsActions, artistsSelectors } from "../../../common/slices/artistsSlice";
+
+export const usePopularLists = () => {
+    type IdsList = readonly string[];
+    type FormattedIdsList = string;
+
+    const formatPopularListForFetch = (idsList: IdsList) => idsList.join(",");
+
+    const popularAlbumsIdsList: FormattedIdsList = formatPopularListForFetch([
+        "47tDANOMCmdRDI5CVcjNKY",
+        "7aJuG4TFXa2hmE4z1yxc3n",
+        "61ulfFSmmxMhc2wCdmdMkN",
+        "4cA2a9w1gSzJEIdbhwUrsH",
+        "0zqVGKhttQF25HwPeNcknO",
+        "3pbJC94hUZST7m2coeIY6I",
+        "02re9DV48w3DBMwnCR6S3Q",
+        "4cA2a9w1gSzJEIdbhwUrsH",
+    ]);
+
+    const popularArtistsIdsList: FormattedIdsList = formatPopularListForFetch([
+        "4iHNK0tOyZPYnBU7nGAgpQ",
+        "5lpH0xAS4fVfLkACg9DAuM",
+        "5WUlDfRSoLAfcVSX1WnrxN",
+        "66CXWjxzNUsdJxJ2JdwvnR",
+        "0TMvoNR0AIJV138mHY6jdE",
+        "0MIG6gMcQTSvFbKvUwK0id",
+        "1GxkXlMwML1oSg5eLPiAz3",
+        "38EmEgXkgK51MT2tPY0EoC",
+    ]);
+
+    const popularEpisodesIdsList: FormattedIdsList = formatPopularListForFetch([
+        "3oTmwclY2QIcCVid8dpDLe",
+        "3Kg2Qmr7x6yCLNCUwPd30k",
+        "4A7WCiltnjCAT57HhmO7xN",
+        "66CXWjxzNUsdJxJ2JdwvnR",
+        "0ZyH6ZQIxpfco1fJrq5Dmj",
+        "37AxLdZj0ymkjW9Uxs6TLu",
+        "2XDdKW7kbpABg3v5FltFri",
+    ]);
+
+    const {
+        configs: popularArtistsConfig,
+        apiStatus: popularArtistsStatus,
+        rawApiData: rawPopularArtistsList
+    } = useApiResource({
+        actions: artistsActions,
+        selectors: artistsSelectors,
+        endpoint: getSeveralArtistsListEndpoint({ id: popularArtistsIdsList }),
+    });
+
+    const {
+        configs: popularAlbumsConfig,
+        apiStatus: popularAlbumsStatus,
+        rawApiData: rawPopularAlbumsList
+    } = useApiResource({
+        actions: albumsActions,
+        selectors: albumsSelectors,
+        endpoint: getSeveralAlbumsListEndpoint({ id: popularAlbumsIdsList }),
+    });
+
+    const configs = [popularAlbumsConfig, popularArtistsConfig];
+    const apiStatuses = [popularAlbumsStatus, popularArtistsStatus];
+    const apiDataList = [rawPopularAlbumsList, rawPopularArtistsList]
+
+    return { configs, apiStatuses, apiDataList };
+};
