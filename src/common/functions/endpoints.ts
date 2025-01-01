5ee00c29810a0dfbbf202cf4c4d6155ab6a0545a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-01 14:02:27 +0100

    Created getShowDetailsEndpoint and getSeveralShowsListEndpoint functions to fetch show/shows details

--- src/common/functions/endpoints.ts
+++ src/common/functions/endpoints.ts
@@ -12,7 +12,11 @@ export const getArtistDetailsEndpoint: GetEndpointFunction = ({ id }) => `artist
 export const getSeveralArtistsListEndpoint: GetEndpointFunction = ({ id }) => `artists?ids=${id}`;
 
 export const getEpisodeDetailsEdnpoint: GetEndpointFunction = ({ id }) => `episodes/${id}`;
-export const geteSeveralEpisodesListEndpoint: GetEndpointFunction = ({ id }) => `episodes?ids=${id}`;
+export const getSeveralEpisodesListEndpoint: GetEndpointFunction = ({ id }) => `episodes?ids=${id}`;
+
+export const getShowDetailsEndpoint: GetEndpointFunction = ({ id }) => `shows/${id}`;
+export const getSeveralShowsListEndpoint: GetEndpointFunction = ({ id }) => `shows?ids=${id}`;
+
 
 export const getArtistTopTracksEndpoint: GetEndpointFunction = ({ id }) => `artists/${id}/top-tracks`;
 
