4737e5ffec2248b3a461c1a6db0da4fd45de8f65
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-07 19:55:02 +0100

    Removed largerAndDarkerIcon prop from StyledPlayIcon. Increased width and height.

--- src/common/components/StyledPlayIcon/index.tsx
+++ src/common/components/StyledPlayIcon/index.tsx
@@ -1,18 +1,8 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { ReactComponent as PlayIcon } from "../../icons/Play.svg";
 
-interface StyledPlayIconProps {
-    $largerAndDarkerIcon?: boolean;
-}
-
-export const StyledPlayIcon = styled(PlayIcon) <StyledPlayIconProps>`
-    width: 13px; 
-    height: 13px; 
-    fill: white; 
-
-    ${({ $largerAndDarkerIcon }) => $largerAndDarkerIcon && css`
-        fill: black;
-        width: 17px; 
-        height: 17px; 
-    `};
+export const StyledPlayIcon = styled(PlayIcon)`
+    fill: black;
+    width: 17px; 
+    height: 17px; 
 `;
