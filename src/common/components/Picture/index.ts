4ac03f537efcd22dfaef07a505be271ad03225f2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-07 19:49:59 +0100

    Added new prop noBorderRadius to Picture styled-component.

--- src/common/components/Picture/index.ts
+++ src/common/components/Picture/index.ts
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 interface PictureProps {
     $picture: string;
     $useArtistPictureStyle?: boolean;
+    $noBorderRadius?:boolean;
 }
 
 export const Picture = styled.div.attrs<PictureProps>(({ $picture }) => ({
@@ -10,20 +11,22 @@ export const Picture = styled.div.attrs<PictureProps>(({ $picture }) => ({
         backgroundImage: `url(${$picture})`,
     },
 })) <PictureProps>`
-  background-size: cover;
-  background-position: center;
-  border-radius: 8px;
-  padding-top: calc(100%* 585 / 582);
-  box-shadow: 0px 0px 30px 9px #00000061;
-  position: relative;
+    background-size: cover;
+    background-position: center;
+    border-radius: 8px;
+    padding-top: calc(100% * 585 / 582);
+    box-shadow: 0px 0px 30px 9px #00000061;
+    position: relative;
 
-  display: flex;
-  justify-content: end;
-  align-items: end;
+    display: flex;
+    justify-content: end;
+    align-items: end;
 
-  ${({ $useArtistPictureStyle }) =>
-        $useArtistPictureStyle &&
-        css`
-      border-radius: 50%;
+    ${({$useArtistPictureStyle}) =>$useArtistPictureStyle && css`
+         border-radius: 50%;
+    `};
+
+    ${({$noBorderRadius}) => $noBorderRadius && css`
+        border-radius: unset;
     `};
 `;
