418c547fe307d79b9a9ac4a180127f39b7087aba
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-07 19:53:50 +0100

    Added $smaller prop to Button styled-component of PlayPauseButton component

--- src/common/components/PlayPauseButton/styled.ts
+++ src/common/components/PlayPauseButton/styled.ts
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as PauseIcon } from "../../icons/Pause.svg";
 
+interface ButtonProps {
+    $smaller?: boolean;
+}
 
-export const Button = styled.button`
+export const Button = styled.button<ButtonProps>`
+    justify-self: end;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -16,8 +20,12 @@ export const Button = styled.button`
     &:hover{
         background-color : ${({ theme }) => theme.colors.mantis};
         cursor: pointer;
-       
     }
+
+    ${({ $smaller}) =>  $smaller && css`
+        width: 35px;
+        height: 35px;
+    `};
 `;
 
 export const StyledPauseIcon = styled(PauseIcon)`
