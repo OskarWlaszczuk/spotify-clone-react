260a5a667fc83672c080510aea2822b613690e4f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 09:22:47 +0100

    Added optional property facetType in ToPageParams interfaces in routes

--- src/common/functions/routes.ts
+++ src/common/functions/routes.ts
@@ -1,5 +1,6 @@
 interface ToPageParams {
-    additionalPath?: string | undefined | any;
+    fullListType?: string | undefined;
+    facetType?: string | undefined;
 };
 
 interface ToDetailsPageParams extends ToPageParams {
@@ -9,9 +10,9 @@ interface ToDetailsPageParams extends ToPageParams {
 type ToPage = (optionalParams?: ToPageParams) => string;
 type ToDetailsPage = (params: ToDetailsPageParams) => string;
 
-export const toHome: ToPage = ({ additionalPath = '' } = { additionalPath: '' }) => `/home/${additionalPath}`;
+export const toHome: ToPage = ({ fullListType = '', facetType = "" } = { fullListType: '', facetType: "" }) => `/home/${fullListType}${facetType}`;
 export const toSearch: ToPage = () => "/search";
 
 export const toAlbum = ({ id }: { id: string } = { id: "" }) => `/album/${id}`;
-export const toArtist: ToDetailsPage = ({ id, additionalPath = '' } = { id: '', additionalPath: '' }) => `/artist/${id}/${additionalPath}`;
+export const toArtist: ToDetailsPage = ({ id, fullListType = '' } = { id: '', fullListType: '' }) => `/artist/${id}/${fullListType}`;
 export const toTrack = ({ id }: { id: string } = { id: "" }) => `/track/${id}`;
