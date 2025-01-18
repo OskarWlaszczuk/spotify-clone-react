0c4d81339fcbd82f2ce3b9404ade301f6c7ead6f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 11:58:27 +0100

    Refactor useAlbumDetails hook for improved code consistency and formatting

--- src/features/albumPage/hooks/useAlbumDetails.js
+++ src/features/albumPage/hooks/useAlbumDetails.js
@@ -1,22 +1,22 @@
-import { useFetchAPI } from "../../../common/hooks/useFetchAPI";
-import { albumDetailsActions, albumDetailsSelectors } from "../../../common/slices/albumDetailsSlice";
-import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
-import { useApiResource } from "../../../common/hooks/useApiResource";
-import { getAlbumDetailsEndpoint } from "../../../common/functions/endpoints";
+import {useFetchAPI} from "../../../common/hooks/useFetchAPI";
+import {albumDetailsActions, albumDetailsSelectors} from "../../../common/slices/albumDetailsSlice";
+import {getSpecificKeys} from "../../../common/functions/getSpecificKeys";
+import {useApiResource} from "../../../common/hooks/useApiResource";
+import {getAlbumDetailsEndpoint} from "../../../common/functions/endpoints";
 
 export const useAlbumDetails = (albumId) => {
-    const { configs, rawApiData: albumDetails, apiStatus: albumDetailsStatus } = useApiResource({
+    const {configs, rawApiData: albumDetails, apiStatus: albumDetailsStatus} = useApiResource({
         actions: albumDetailsActions,
         selectors: albumDetailsSelectors,
-        endpoint: getAlbumDetailsEndpoint({ id: albumId }),
+        endpoint: getAlbumDetailsEndpoint({id: albumId}),
     });
-
+console.log(albumDetails)
     const filteredAlbumDetails = getSpecificKeys(
-        albumDetails,
-        ["name", "images", "type", "release_date", "copyrights", "total_tracks", "tracks", "artists"]
+        [albumDetails],
+        ["name", "images", "album_type", "release_date", "copyrights", "total_tracks", "tracks", "artists"]
     );
 
-    useFetchAPI({ fetchConfigs: [configs], pageId: albumId, dependencies: [albumId] });
+    useFetchAPI({fetchConfigs: [configs], pageId: albumId, dependencies: [albumId]});
 
-    return { filteredAlbumDetails, albumDetailsStatus };
+    return {filteredAlbumDetails, albumDetailsStatus};
 };
