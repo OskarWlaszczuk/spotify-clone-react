2d4f66b33e1a3c38bbc49e6c4b302a19ceeff162
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 18:42:13 +0100

    Add CategoryName type to unify ReleaseCategory and FacetCategory

---
+++ src/common/types/CategoryName.ts
@@ -0,0 +1,5 @@
+import { FacetCategory } from "../../features/homePage/constants/facetCategories";
+import { FacetParam } from "../../features/homePage/constants/facetParams";
+import { ReleaseCategory } from "./ReleaseCategory";
+
+export type CategoryName = ReleaseCategory | FacetCategory;
