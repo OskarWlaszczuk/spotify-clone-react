7e7f1fcc42e985a5aa12463e12ebcddb6ec6462b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 12:00:26 +0100

    Refactor getFilteredTrackData function to handle track and album data as arrays for improved consistency

--- src/features/trackDetailsPage/functions/getFilteredTrackData.js
+++ src/features/trackDetailsPage/functions/getFilteredTrackData.js
@@ -1,9 +1,9 @@
 import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
 
 export const getFilteredTrackData = (trackData) => {
-    const filteredTrackData = getSpecificKeys(trackData, ["name", "type", "duration_ms", "popularity", "artists"]);
-    const filteredAlbumData = getSpecificKeys(trackData?.album, ["name", "release_date", "id", "images"]);
-    const filteredMainArtistData = getSpecificKeys(trackData?.artists[0], ["name", "id"]);
+    const filteredTrackData = getSpecificKeys([trackData], ["name", "type", "duration_ms", "popularity", "artists"]);
+    const filteredAlbumData = getSpecificKeys([trackData?.album], ["name", "release_date", "id", "images"]);
+    const filteredMainArtistData = getSpecificKeys([trackData?.artists[0]], ["name", "id"]);
 
     return [...filteredTrackData, ...filteredAlbumData, ...filteredMainArtistData];
 };
