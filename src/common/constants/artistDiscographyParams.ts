1e2a630139bee463f660bb60be5e48fe4641034f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 09:04:16 +0100

    Ranemed params file to artistDiscographyParams and moved none-discography constants to seperated file.

--- src/common/constants/artistDiscographyParams.ts
+++ src/common/constants/artistDiscographyParams.ts
@@ -2,9 +2,3 @@ export const allReleaseParamDiscography = "discography-all";
 export const albumsParamDiscography = "discography-album";
 export const singleParamDiscography = "discography-single";
 export const compilationParamDiscography = "discography-compilation";
-
-export const relatedArtistsParam = "related";
-export const artistAppearsOnParam = "appears-on";
-
-export const popularAlbumsParam = "popular-albums";
-export const popularArtistsParam = "popular-artists";
