b8b8cc67f7cc7e5240c410ad0f4fe35bf4e2481b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 09:20:16 +0100

    getSpecificKeys function gets two generic type parameters and assigned the to function parameters. Added asertion to selectedKeysList constant with Pick<T, K> type

--- src/common/functions/getSpecificKeys.ts
+++ src/common/functions/getSpecificKeys.ts
@@ -1,39 +1,16 @@
-// export const getSpecificKeys = <T>(object: T, keysToGetList: string[]) => {
-//     const objectArray = Array(object);
+export const getSpecificKeys = <T, K extends keyof T>(object: T[], keysToGetList: K[]) => {
 
-//     const getNestedKey = (object: T, nestedKeyToGet: string) => (
-//         nestedKeyToGet.split('.').reduce(
-//             (currentObjectKey, keyToGet) => currentObjectKey && currentObjectKey[keyToGet], object
-//         )
-//     );
+    return object?.map((object) => {
+        const selectedKeysList = {} as Pick<T, K>;
 
-//     return objectArray?.map((objectKey) => {
-//         const selectedKeys = {};
-//         keysToGetList.forEach(key => {
-//             const selectedKeyValue = getNestedKey(objectKey, key);
-//             if (selectedKeyValue !== undefined) {
-//                 selectedKeys[key] = selectedKeyValue;
-//             }
-//         });
-//         return selectedKeys;
-//     });
-// };
-
-
-export const getSpecificKeys = <T>(object: T, keysToGetList: string[]) => {
-    const objectArray = Array.isArray(object) ? object : [object];
-
-    return objectArray?.map((objectKey: any) => {
-        const selectedKeysList: any = {};
-
-        keysToGetList.forEach(key => {
-            const selectedKey = objectKey?.[key];
+        keysToGetList.forEach((key) => {
+            const selectedKey = object?.[key];
 
             if (selectedKey !== undefined) {
                 selectedKeysList[key] = selectedKey;
             }
         });
+
         return selectedKeysList;
     });
 };
-
