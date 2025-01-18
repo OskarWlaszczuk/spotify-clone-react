8e3de5bd2f4ec587a9400da8a0b7af184187a349
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 11:47:25 +0100

    Dispatching access token only if it's null in useAuth

--- src/core/App/hooks/useAuth.js
+++ src/core/App/hooks/useAuth.js
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchAccessToken } from "../../../common/slices/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAccessToken, selectAccessToken } from "../../../common/slices/authSlice";
 
 export const useAuth = () => {
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
+  const dispatch = useDispatch();
+  const accessToken = useSelector(selectAccessToken);
+
+  useEffect(() => {
+    if (accessToken === null) {
       dispatch(fetchAccessToken());
-    }, [dispatch]);
-  }; 
+    }
+  }, [dispatch, accessToken]);
+}; 
