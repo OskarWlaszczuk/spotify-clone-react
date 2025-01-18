8de5e883cbe67a2da8695fae1589b45ee6583b12
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 12:05:20 +0100

    Add constants for popular artists, albums, episodes, and shows IDs

---
+++ src/features/homePage/constants/popularListsIds.ts
@@ -0,0 +1,40 @@
+export const popularArtistsIdsList = [
+    "0wm6v5GU6VbR1wtsm0YiTV",
+    "6QfFTZJHFSe9Xyes6DkAli",
+    "7CJgLPEqiIRuneZSolpawQ",
+    "6EB8VE9f7Ut6NOgviN6gDW",
+    "5NmRijhUHZnaADekOLcOyl",
+    "3sW41aChSc5bBow0Folc1S",
+    "0TMvoNR0AIJV138mHY6jdE",
+    "1GRpnNhXWlNMgnFf3NqEjv",
+];
+
+export const popularAlbumsIdsList = [
+    "47tDANOMCmdRDI5CVcjNKY",
+    "7aJuG4TFXa2hmE4z1yxc3n",
+    "61ulfFSmmxMhc2wCdmdMkN",
+    "4cA2a9w1gSzJEIdbhwUrsH",
+    "0zqVGKhttQF25HwPeNcknO",
+    "3pbJC94hUZST7m2coeIY6I",
+    "02re9DV48w3DBMwnCR6S3Q",
+    "4cA2a9w1gSzJEIdbhwUrsH",
+];
+
+export const popularEpisodesIdsList = [
+    "3oTmwclY2QIcCVid8dpDLe",
+    "3Kg2Qmr7x6yCLNCUwPd30k",
+    "4A7WCiltnjCAT57HhmO7xN",
+    "0ml54mQpzYhDAjhpAQrAHm",
+    "0ml54mQpzYhDAjhpAQrAHm",
+    "37AxLdZj0ymkjW9Uxs6TLu",
+    "2XDdKW7kbpABg3v5FltFri",
+];
+
+export const popularShowsIdsList = [
+    "79CkJF3UJTHFV8Dse3Oy0P",
+    "1aIVAabjRjnmiouX0zCzF3",
+    "2Eqx1kHjpiAOQ9oSxo8Tcg",
+    "2IaURnE3IsOifmReYREgho",
+    "7LViRQRexAuXzLqQQMd1QV",
+    "5mIRx8HQDVWtYb86WlQ4v9",
+];
