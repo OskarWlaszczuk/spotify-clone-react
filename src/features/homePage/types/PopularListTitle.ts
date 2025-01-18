5b28e308127870f1119bffff4b7f31a32e0b6d0a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 12:01:48 +0100

    Add PopularListTitle type to define titles for popular albums, artists, and episodes

---
+++ src/features/homePage/types/PopularListTitle.ts
@@ -0,0 +1,3 @@
+import {popularAlbumsTitle, popularArtistsTitle, popularEpisodesTitle} from "../constants/popularListsTitles";
+
+export type PopularListTitle = typeof popularAlbumsTitle | typeof popularArtistsTitle | typeof popularEpisodesTitle;
