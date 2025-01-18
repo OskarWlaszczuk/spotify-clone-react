5c1aee8937258ad926bacfdcff2314337358252c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 11:59:51 +0100

    Refactor MainContent component to improve rendering of related releases and enhance code readability

--- src/features/albumPage/components/AlbumPage/MainContent/index.js
+++ src/features/albumPage/components/AlbumPage/MainContent/index.js
@@ -1,9 +1,10 @@
 import { Copyrights } from "./Copyrights"
 import { Table } from "../../../../../common/components/Table"
-import { allReleaseParamDiscography } from "../../../../../common/constants/params";
+import { allReleaseParamDiscography } from "../../../../../common/constants/artistDiscographyParams";
 import { toAlbum, toArtist } from "../../../../../common/functions/routes";
 import { useRenderTilesList } from "../../../../../common/hooks/useRenderTilesList";
 import { getUniqueDiscNumbers } from "../../../functions/getUniqueDiscNumbers";
+import { getYear } from "../../../../../common/functions/getYear";
 
 export const MainContent = ({
     mainArtistData: {
@@ -19,27 +20,28 @@ export const MainContent = ({
 }) => {
     const renderTilesList = useRenderTilesList();
 
+    const sectionsDataToRender = [
+        {
+            title: `More by ${name}`,
+            list: releases,
+            toPageFunction: toAlbum,
+            fullListData: {
+                pathname: toArtist({
+                    id,
+                    additionalPath: allReleaseParamDiscography
+                }),
+                text: "Show discography"
+            },
+            listId: 0,
+            renderSubInfo: ({release_date}) => getYear(release_date),
+        }
+    ];
+
     return (
         <>
             <Table list={tracksList} useAlbumView discsNumbers={getUniqueDiscNumbers(tracksList)} />
             <Copyrights date={releaseDate} copyrights={copyrights} />
-            {
-                renderTilesList([
-                    {
-                        title: `More by ${name}`,
-                        list: releases,
-                        toPageFunction: toAlbum,
-                        fullListData: {
-                            pathname: toArtist({
-                                id,
-                                additionalPath: allReleaseParamDiscography
-                            }),
-                            text: "Show discography"
-                        },
-                        listId: 0,
-                    }
-                ])
-            }
+            {renderTilesList(sectionsDataToRender)}
         </>
-    )
-}
+    );
+};
