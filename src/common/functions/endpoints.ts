1671b5d51d7dd5e614006dd0701305f14e0f4c07
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 09:09:48 +0100

    Added getShowEpisodes to endpoints function

--- src/common/functions/endpoints.ts
+++ src/common/functions/endpoints.ts
@@ -1,9 +1,9 @@
 interface EndpointData {
     id: string;
-    isAppearOnReleasesInclude?: boolean;
+    includeAppearsOnReleases?: boolean;
 }
 
-type GetEndpointFunction = ({ id, isAppearOnReleasesInclude }: EndpointData) => string;
+type GetEndpointFunction = ({ id, includeAppearsOnReleases }: EndpointData) => string;
 
 export const getAlbumDetailsEndpoint: GetEndpointFunction = ({ id }) => `albums/${id}`;
 export const getSeveralAlbumsListEndpoint: GetEndpointFunction = ({ id }) => `albums?ids=${id}`;
@@ -16,15 +16,15 @@ export const getSeveralEpisodesListEndpoint: GetEndpointFunction = ({ id }) => `
 
 export const getShowDetailsEndpoint: GetEndpointFunction = ({ id }) => `shows/${id}`;
 export const getSeveralShowsListEndpoint: GetEndpointFunction = ({ id }) => `shows?ids=${id}`;
-
+export const getShowEpisodes: GetEndpointFunction = ({ id }) => `shows/${id}/episodes`;
 
 export const getArtistTopTracksEndpoint: GetEndpointFunction = ({ id }) => `artists/${id}/top-tracks`;
 
-export const getArtistReleasesEndpoint: GetEndpointFunction = ({ id, isAppearOnReleasesInclude = false }) => {
+export const getArtistReleasesEndpoint: GetEndpointFunction = ({ id, includeAppearsOnReleases = false }) => {
     const baseEndpoint = `artists/${id}/albums?include_groups=album%2Csingle%2Ccompilation&limit=50`;
     const withAppearsOn = `artists/${id}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation&limit=50`;
 
-    return isAppearOnReleasesInclude ? withAppearsOn : baseEndpoint;
+    return includeAppearsOnReleases ? withAppearsOn : baseEndpoint;
 };
 
 export const getTrackDetailsEndpoint: GetEndpointFunction = ({ id }) => `tracks/${id}`;
