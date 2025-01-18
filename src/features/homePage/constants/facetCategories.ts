d092c72ee0c0d8b9e068328e5f07d6510b79c0f8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-18 11:52:38 +0100

    Created alias type of FacetCategory

---
+++ src/features/homePage/constants/facetCategories.ts
@@ -0,0 +1,5 @@
+export const facetAllCategory = "all";
+export const facetMusicCategory = "music";
+export const facetPodcastsCategory = "podcasts";
+
+export type FacetCategory = typeof facetAllCategory | typeof facetMusicCategory | typeof facetPodcastsCategory 
