b244e53a570ca210fbd441530a76fb6aaf72df58
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-25 08:12:39 +0100

    In getFilteredTrackData: Removed filteredMainArtistData constant. Now the function returns object with declared higher constants, instead of merged array. Added [0] to the constants in order to get their objects.

--- src/features/trackDetailsPage/functions/getFilteredTrackData.js
+++ src/features/trackDetailsPage/functions/getFilteredTrackData.js
@@ -1,9 +1,8 @@
 import { getSpecificKeys } from "../../../common/functions/getSpecificKeys";
 
-export const getFilteredTrackData = (trackDetails) => {
-    const filteredTrackData = getSpecificKeys([trackDetails], ["name", "type", "duration_ms", "popularity", "artists"]);
-    const filteredAlbumData = getSpecificKeys([trackDetails?.album], ["name", "release_date", "id", "images"]);
-    const filteredMainArtistData = getSpecificKeys([trackDetails?.artists[0]], ["name", "id"]);
+export const getFilteredTrackData = (rawTrackDetails) => {
+    const trackDetails = getSpecificKeys([rawTrackDetails], ["name", "type", "duration_ms", "popularity", "artists"])[0];
+    const albumDetails = getSpecificKeys([trackDetails?.album], ["name", "release_date", "id", "images"])[0];
 
-    return [...filteredTrackData, ...filteredAlbumData, ...filteredMainArtistData];
+    return { trackDetails, albumDetails };
 };
