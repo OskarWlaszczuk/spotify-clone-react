3df5f4aed8ef9a00c5450912c7b09baf3e632ca9
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-25 08:13:29 +0100

    Refactor useFetchSeveralArtists to improve parameter naming and format IDs for fetch

--- src/common/hooks/useFetchSeveralArtists.js
+++ src/common/hooks/useFetchSeveralArtists.js
@@ -1,28 +1,28 @@
 import { getSeveralArtistsListEndpoint } from "../functions/endpoints";
+import { formatIdsForFetch } from "../functions/formatIdsForFetch";
 import { artistsActions, artistsSelectors } from "../slices/artistsSlice";
 import { useApiResource } from "./useApiResource";
 import { useFetchAPI } from "./useFetchAPI";
 
-export const useFetchSeveralArtists = ({ artistsIds, fetchCondition = true, dependencies = [] }) => {
+export const useFetchSeveralArtists = ({ IDs, fetchCondition = true, dependencies = [], pageID }) => {
+    const formattedIDs = formatIdsForFetch(IDs);
+
     const {
-        configs: artistsDetailsListConfig,
-        apiStatus: artistsDetailsListStatus,
-        rawApiData: artistsDetailsList
+        configs: artistsConfig,
+        apiStatus: artistsStatus,
+        rawApiData: artists
     } = useApiResource({
         actions: artistsActions,
         selectors: artistsSelectors,
-        endpoint: getSeveralArtistsListEndpoint({ id: artistsIds }),
+        endpoint: getSeveralArtistsListEndpoint({ id: formattedIDs }),
     });
 
     useFetchAPI({
-        fetchConfigs: [artistsDetailsListConfig],
+        fetchConfigs: [artistsConfig],
+        pageId: pageID,
         dependencies,
         fetchCondition,
-        pageId: artistsIds,
     });
 
-    return {
-        artistsDetailsListStatus,
-        artistsDetailsList,
-    };
+    return { artists, artistsStatus };
 };
