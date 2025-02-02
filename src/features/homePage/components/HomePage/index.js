92f315517e84eb34b8b975c96490162b04a11435
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-02-02 19:32:16 +0100

    In HomePage component: -updated call of renamed usePopularLists to useFetchPopularLists - fetch artists releases with useArtistsAlbumsList3 test hook - temporarily added commands to remove component content

--- src/features/homePage/components/HomePage/index.js
+++ src/features/homePage/components/HomePage/index.js
@@ -2,42 +2,36 @@ import { Main } from "../../../../common/components/Main";
 import { useFetchStatus } from "../../../../common/hooks/useFetchStatuses";
 import { MainContent } from "./MainContent";
 import { useParams } from "react-router-dom";
-import { usePopularLists } from "../../hooks/useFetchPopularLists";
-import { getSpecificKeys } from "../../../../common/functions/getSpecificKeys";
-import { useArtistsAlbumsList, useArtistsAlbumsList2 } from "../../../trackDetailsPage/hooks/useArtistsAlbumsList";
-import { popularArtistsIdsList, popularShowsIdsList } from "../../constants/popularListsIds";
-import { useFetchShows } from "../../../../common/hooks/useFetchShows";
+import { useFetchPopularLists } from "../../hooks/useFetchPopularLists";
+import { useArtistsAlbumsList, useArtistsAlbumsList3 } from "../../../trackDetailsPage/hooks/useArtistsAlbumsList";
+import { useApiResource } from "../../../../common/hooks/useApiResource";
+import { artistDetailsActions, artistDetailsSelectors } from "../../../../common/slices/artistDetailsSlice";
+import { getAlbumDetailsEndpoint, getArtistDetailsEndpoint } from "../../../../common/functions/endpoints";
+import { useFetch } from "../../../../common/hooks/useFetchAPI";
+import { useEffect, useMemo, useRef } from "react";
+import { albumDetailsActions, albumDetailsSelectors } from "../../../../common/slices/albumDetailsSlice";
+import { popularArtistsIdsList } from "../../constants/popularListsIds";
 
 export const HomePage = () => {
     const { fullListType } = useParams();
 
-    const { popularListsStatuses, popularLists } = usePopularLists();
-console.log(popularLists)
-    const [
-        { albums: popularAlbumsList },
-        { artists: popularArtistsDetailsList },
-        popularEpisodesList
-    ] = getSpecificKeys(popularLists, ["albums", "artists", "episodes"]);
+    const { popularListsStatuses, popularLists } = useFetchPopularLists();
+    
+    const { releasesGroupedByArtists, releasesGroupedByArtistsStatus } = useArtistsAlbumsList3({ artistsDetailsList: popularLists[0] });
+  
+    const fetchStatus = useFetchStatus([...popularListsStatuses, releasesGroupedByArtistsStatus]);
 
-    const {
-        artistsAllReleasesDataList,
-        artistsAllReleasesDataListStatus
-    } = useArtistsAlbumsList({
-        artistsIdsList: popularArtistsIdsList,
-        artistsDetailsList: popularArtistsDetailsList,
-    });
-// console.log(popularEpisodesList)
-    const fetchStatus = useFetchStatus([...popularListsStatuses, artistsAllReleasesDataListStatus]);
     return (
-        <Main
-            useGradient={!fullListType}
-            currentFetchStatus={fetchStatus}
-            content={
-                <MainContent
-                    popularLists={[popularAlbumsList, popularArtistsDetailsList, popularEpisodesList]}
-                    artistsAllReleasesDataList={artistsAllReleasesDataList}
-                />
-            }
-        />
+        // <Main
+        //     useGradient={!fullListType}
+        //     currentFetchStatus={fetchStatus}
+        //     content={
+        //         <MainContent
+        //             popularLists={popularLists}
+        //             artistsAllReleasesDataList={artistsReleases}
+        //         />
+        //     }
+        // />
+        <></>
     );
 };
