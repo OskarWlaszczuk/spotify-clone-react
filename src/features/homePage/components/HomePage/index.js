8eb8b3f39ffd2296e82321869e2e9b62504476b1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-02-03 10:24:19 +0100

    Refactor HomePage component to use useFetchMediaSortedByCreators for improved media fetching

--- src/features/homePage/components/HomePage/index.js
+++ src/features/homePage/components/HomePage/index.js
@@ -3,23 +3,19 @@ import { useFetchStatus } from "../../../../common/hooks/useFetchStatuses";
 import { MainContent } from "./MainContent";
 import { useParams } from "react-router-dom";
 import { useFetchPopularLists } from "../../hooks/useFetchPopularLists";
-import { useArtistsAlbumsList, useArtistsAlbumsList3 } from "../../../trackDetailsPage/hooks/useArtistsAlbumsList";
-import { useApiResource } from "../../../../common/hooks/useApiResource";
-import { artistDetailsActions, artistDetailsSelectors } from "../../../../common/slices/artistDetailsSlice";
-import { getAlbumDetailsEndpoint, getArtistDetailsEndpoint } from "../../../../common/functions/endpoints";
-import { useFetch } from "../../../../common/hooks/useFetchAPI";
-import { useEffect, useMemo, useRef } from "react";
-import { albumDetailsActions, albumDetailsSelectors } from "../../../../common/slices/albumDetailsSlice";
-import { popularArtistsIdsList } from "../../constants/popularListsIds";
+import { useFetchMediaSortedByCreators } from "../../../trackDetailsPage/hooks/useFetchMediaSortedByCreators";
 
 export const HomePage = () => {
     const { fullListType } = useParams();
 
     const { popularListsStatuses, popularLists } = useFetchPopularLists();
-    
-    const { releasesGroupedByArtists, releasesGroupedByArtistsStatus } = useArtistsAlbumsList3({ artistsDetailsList: popularLists[0] });
-  
-    const fetchStatus = useFetchStatus([...popularListsStatuses, releasesGroupedByArtistsStatus]);
+
+    const {
+        mediaSortedByCreator: releasesSortedByArtists,
+        mediaSortedByCreatorStatus: releasesSortedByArtistsStatus
+    } = useFetchMediaSortedByCreators({ creatorsDetails: popularLists[0] });
+
+    const fetchStatus = useFetchStatus([...popularListsStatuses, releasesSortedByArtistsStatus]);
 
     return (
         // <Main
