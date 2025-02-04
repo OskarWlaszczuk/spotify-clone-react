4606b0e1268cce51005e3dd55329f009d3b5ed03
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-02-04 11:20:22 +0100

    Add route for ShowPage to enable navigation to show details

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -8,6 +8,7 @@ import { Library } from "./components/Library";
 import { ArtistDetailsPage } from "../../features/artistDetailsPage/components/ArtistDetailsPage";
 import { TrackDetailsPage } from "../../features/trackDetailsPage/components/TrackDetailsPage";
 import { useAuth } from "./hooks/useAuth";
+import { ShowPage } from "../../features/showsPage/ShowPage";
 
 export const App = () => {
     useAuth();
@@ -21,6 +22,7 @@ export const App = () => {
                 <Route path={toSearch()} element={<Search />} />
                 <Route path="/track/:id" element={<TrackDetailsPage />} />
                 <Route path="/album/:id" element={<AlbumPage />} />
+                <Route path="/show/:id" element={<ShowPage />} />
                 <Route path="/artist/:id/:fullListType?" element={<ArtistDetailsPage />} />
                 <Route path="*" element={<Navigate to={toHome()} />} />
             </Routes>
