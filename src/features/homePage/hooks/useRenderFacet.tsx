4a7c618645912673daea1f0071b0a916bdd6d515
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-02-11 09:09:19 +0100

    In useRenderFacet hook: - moved facetCategoriesConfig declaration outside the hook body and pass it as parameter - removed facetParamsGroupedByCategories declaration - removed commented and deprecated link prop of CategoriesSwitchersSection - declared facetIndexBasedOnCurrentType to find through facetCategoriesConfig, the element that its categoryName matches the facetType

--- src/features/homePage/hooks/useRenderFacet.tsx
+++ src/features/homePage/hooks/useRenderFacet.tsx
@@ -1,51 +1,19 @@
-import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { CategoriesSwitchersSection } from "../../../common/components/CategoriesSwitchersSection";
 import { CategoryConfig } from "../../../common/components/CategoriesSwitchersSection/CategoryConfig";
-import { fullListLinkText } from "../../../common/constants/fullListLinkText ";
-import { findMatchingOptionByKey } from "../../../common/functions/findMatchingOptionByKey";
-import { toAlbum, toHome } from "../../../common/functions/routes";
 import { useRenderTilesList } from "../../../common/hooks/useRenderTilesList";
-import { AlbumItem } from "../../../common/Interfaces/AlbumItem";
-import { EpisodeItem } from "../../../common/Interfaces/EpisodeItem";
 import { useCurrentCategoryData } from "../../artistDetailsPage/hooks/useCurrentCategoryData";
-import { facetAllCategory, facetMusicCategory, facetPodcastsCategory } from "../constants/facetCategories";
-import { allFacetParam, musicFacetParam, podcastsFacetParam } from "../constants/facetParams";
-import { useParams } from "react-router-dom";
 
-export const useRenderFacet = (popularAlbums: AlbumItem[], popularEpisodes: EpisodeItem[]) => {
+export const useRenderFacet = (facetCategoriesConfig: CategoryConfig[]) => {
 
     const { facetType } = useParams();
 
-    const facetCategoriesConfig: CategoryConfig[] = [
-        {
-            categoryView: popularAlbums,
-            categoryName: facetAllCategory,
-            categorySwitcherContent: "All"
-        },
-        {
-            categoryView: popularAlbums,
-            categoryName: facetMusicCategory,
-            categorySwitcherContent: "Music"
-        },
-        {
-            categoryView: popularEpisodes,
-            categoryName: facetPodcastsCategory,
-            categorySwitcherContent: "Podcasts"
-        },
-    ];
-
-
+    const facetIndexBasedOnCurrentType = facetCategoriesConfig.findIndex(({ categoryName }) => categoryName === facetType) || 0;
     const { currentCategoryData, setCurrentCategoryData } = useCurrentCategoryData({
-        ...facetCategoriesConfig[1]
+        ...facetCategoriesConfig[facetIndexBasedOnCurrentType]
     });
+
     const renderTilesList = useRenderTilesList();
-
-    const facetParamsGroupedByCategories = [
-        { key: facetAllCategory, value: allFacetParam },
-        { key: facetMusicCategory, value: musicFacetParam },
-        { key: facetPodcastsCategory, value: podcastsFacetParam },
-    ];
-
 
     const renderFacet = () => {
         return renderTilesList([{
@@ -54,18 +22,9 @@ export const useRenderFacet = (popularAlbums: AlbumItem[], popularEpisodes: Epis
                     categoriesConfigs={facetCategoriesConfig}
                     setCurrentCategoryData={setCurrentCategoryData}
                     currentCategory={currentCategoryData.categoryName}
-                // link={
-                //     toHome({
-                //         facetType: findMatchingOptionByKey(
-                //             facetParamsGroupedByCategories,
-                //             currentCategoryData.categoryName
-                //         )?.value
-                //     })
-                // }
                 />
             ),
             list: currentCategoryData.categoryView,
-            toPageFunction: toAlbum,
             areHorizontatItems: true,
         }]);
     };
