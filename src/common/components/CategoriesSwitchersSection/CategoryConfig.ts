91a7132555b390ed642d6c0d87ae5c52ca1ad2ca
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-15 06:52:36 +0100

    Added optional property 'pathname' to CategoryConfig interface

--- src/common/components/CategoriesSwitchersSection/CategoryConfig.ts
+++ src/common/components/CategoriesSwitchersSection/CategoryConfig.ts
@@ -2,4 +2,5 @@ import { CategoryData } from "../../Interfaces/CategoryData";
 
 export interface CategoryConfig extends CategoryData {
     categorySwitcherContent: string;
+    pathname?: string;
 }
