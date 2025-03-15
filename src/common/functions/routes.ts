95c06cd71eb2849b5309692bba931b4f7302f274
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-15 07:01:30 +0100

    Refactor route parameters to use FacetCategory type and adjust toHome function for improved URL structure; added toEpisode function for episode routing

--- src/common/functions/routes.ts
+++ src/common/functions/routes.ts
@@ -1,6 +1,8 @@
+import { FacetCategory } from "../../features/homePage/constants/facetCategories";
+
 interface ToPageParams {
     fullListType?: string | undefined;
-    facetType?: string | undefined;
+    facetType?: FacetCategory;
 };
 
 interface ToDetailsPageParams extends ToPageParams {
@@ -10,10 +12,11 @@ interface ToDetailsPageParams extends ToPageParams {
 type ToPage = (optionalParams?: ToPageParams) => string;
 type ToDetailsPage = (params: ToDetailsPageParams) => string;
 
-export const toHome: ToPage = ({ fullListType = '', facetType = "" } = { fullListType: '', facetType: "" }) => `/home/${fullListType}${facetType}`;
+export const toHome: ToPage = ({ fullListType = '', facetType = "" } = { fullListType: '' }) => `/home/${facetType}${fullListType}`;
 export const toSearch: ToPage = () => "/search";
 
 export const toAlbum = ({ id }: { id: string } = { id: "" }) => `/album/${id}`;
 export const toArtist: ToDetailsPage = ({ id, fullListType = '' } = { id: '', fullListType: '' }) => `/artist/${id}/${fullListType}`;
 export const toTrack = ({ id }: { id: string } = { id: "" }) => `/track/${id}`;
 export const toShow: ToDetailsPage = ({ id }: { id: string } = { id: "" }) => `/show/${id}`;
+export const toEpisode: ToDetailsPage = ({ id }: { id: string } = { id: "" }) => `/episode/${id}`;
