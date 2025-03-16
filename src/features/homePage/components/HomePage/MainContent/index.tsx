8881420a83b0bdbabe37d6183646ffdec20e7f72
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-16 07:16:58 +0100

    Moved facetCategoriesConfig array inside useRenderFacet hook body and with its related data in order to organize facet rendering logic and keep it in one place.

--- src/features/homePage/components/HomePage/MainContent/index.tsx
+++ src/features/homePage/components/HomePage/MainContent/index.tsx
@@ -1,20 +1,9 @@
 import { useParams } from "react-router-dom"
 import { useRenderFacet } from "../../../hooks/useRenderFacet";
-import { ArtistItem } from "../../../../../common/Interfaces/ArtistItem";
 import { AlbumItem } from "../../../../../common/Interfaces/AlbumItem";
 import { EpisodeItem } from "../../../../../common/Interfaces/EpisodeItem";
-import { ShowItem } from "../../../../../common/Interfaces/ShowItem";
-import { toHome } from "../../../../../common/functions/routes";
-import { CategoryConfig } from "../../../../../common/components/CategoriesSwitchersSection/CategoryConfig";
-import { facetAllCategory, facetMusicCategory, facetPodcastsCategory } from "../../../constants/facetCategories";
 import { useSelectFacetConfigBasedOnType } from "../../../functions/selectFacetConfigBasedOnType";
-import { mixLists } from "../../../functions/mixLists";
-interface PopularLists {
-    artists: ArtistItem[];
-    albums: AlbumItem[];
-    episodes: EpisodeItem[];
-    shows: ShowItem[];
-}
+import { PopularLists } from "../../../interfaces/PopularLists";
 
 interface MainContentProps {
     mediaSortedByCreator: (AlbumItem[] | EpisodeItem[])[];
@@ -24,40 +13,15 @@ interface MainContentProps {
 
 export const MainContent = ({ mediaSortedByCreator, popularLists, newReleases }: MainContentProps) => {
     const { facetType } = useParams();
-    const { episodes, albums, artists, shows } = popularLists;
 
-    const allViewFacetList = mixLists(2, albums, shows, artists, episodes);
-    const musicViewList = mixLists(4, albums, artists);
-
-    const facetCategoriesConfig2: CategoryConfig[] = [
-        {
-            pathname: toHome({ facetType: facetAllCategory }),
-            categoryView: allViewFacetList,
-            categoryName: facetAllCategory,
-            categorySwitcherContent: "All"
-        },
-        {
-            pathname: toHome({ facetType: facetMusicCategory }),
-            categoryView: musicViewList,
-            categoryName: facetMusicCategory,
-            categorySwitcherContent: "Music"
-        },
-        {
-            pathname: toHome({ facetType: facetPodcastsCategory }),
-            categoryView: shows,
-            categoryName: facetPodcastsCategory,
-            categorySwitcherContent: "Podcasts"
-        },
-    ];
-
-    const renderFacet = useRenderFacet(facetCategoriesConfig2, facetType);
+    const renderFacet = useRenderFacet(popularLists, facetType);
 
     const selectFacetConfigBasedOnType = useSelectFacetConfigBasedOnType({
         currentFacetType: facetType,
         mediaSortedByCreator,
         creatorsDetails: popularLists,
         newReleases,
-    })
+    });
 
     return (
         <>
--- src/features/homePage/hooks/useRenderFacet.tsx
+++ src/features/homePage/hooks/useRenderFacet.tsx
@@ -1,9 +1,39 @@
 import { CategoriesSwitchersSection } from "../../../common/components/CategoriesSwitchersSection";
 import { CategoryConfig } from "../../../common/components/CategoriesSwitchersSection/CategoryConfig";
+import { toHome } from "../../../common/functions/routes";
 import { useRenderTilesList } from "../../../common/hooks/useRenderTilesList";
 import { useCurrentCategoryData } from "../../artistDetailsPage/hooks/useCurrentCategoryData";
+import { facetAllCategory, facetMusicCategory, facetPodcastsCategory } from "../constants/facetCategories";
+import { mixLists } from "../functions/mixLists";
+import { PopularLists } from "../interfaces/PopularLists";
 
-export const useRenderFacet = (facetCategoriesConfig: CategoryConfig[], facetType: string | undefined) => {
+export const useRenderFacet = (popularLists: PopularLists, facetType: string | undefined) => {
+
+    const { episodes, albums, artists, shows } = popularLists;
+
+    const allViewFacetList = mixLists(2, albums, shows, artists, episodes);
+    const musicViewList = mixLists(4, albums, artists);
+
+    const facetCategoriesConfig: CategoryConfig[] = [
+        {
+            pathname: toHome({ facetType: facetAllCategory }),
+            categoryView: allViewFacetList,
+            categoryName: facetAllCategory,
+            categorySwitcherContent: "All"
+        },
+        {
+            pathname: toHome({ facetType: facetMusicCategory }),
+            categoryView: musicViewList,
+            categoryName: facetMusicCategory,
+            categorySwitcherContent: "Music"
+        },
+        {
+            pathname: toHome({ facetType: facetPodcastsCategory }),
+            categoryView: shows,
+            categoryName: facetPodcastsCategory,
+            categorySwitcherContent: "Podcasts"
+        },
+    ];
 
     const facetIndexBasedOnCurrentType = facetCategoriesConfig.findIndex(({ categoryName }) => categoryName === facetType) || 0;
 
