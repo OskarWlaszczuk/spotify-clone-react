8208e1bd5fbe81380f316ddb06d65e87f05308d5
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-22 18:47:46 +0100

    Renamed targetAlbumGroupsToFilterList parameter of filterReleasesByGroups function to albumGroupNames

--- src/common/functions/filterReleasesByGroups.ts
+++ src/common/functions/filterReleasesByGroups.ts
@@ -2,9 +2,8 @@ import { AlbumItem } from "../Interfaces/AlbumItem";
 import { AlbumGroupName } from "../Types/AlbumGroupName";
 import { isMatch } from "./isMatch"
 
-export const filterReleasesByGroups = (releasesList: AlbumItem[], targetAlbumGroupsToFilterList: AlbumGroupName[]) => {
-    console.log(releasesList)
-    const releasesGrouped = Object.fromEntries(targetAlbumGroupsToFilterList.map(groupName => [
+export const filterReleasesByGroups = (releasesList: AlbumItem[], albumGroupNames: AlbumGroupName[]) => {
+    const releasesGrouped = Object.fromEntries(albumGroupNames.map(groupName => [
         groupName,
         releasesList?.filter(({ album_group }) => isMatch(album_group, groupName))
     ]));
