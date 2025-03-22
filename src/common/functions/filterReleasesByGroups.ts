f58c15107fdcf27854819106787ef9556edeb185
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-22 18:41:26 +0100

    Refactor filterReleasesByGroups function to return an object mapping album groups to their corresponding filtered releases, improving data structure and accessibility.

--- src/common/functions/filterReleasesByGroups.ts
+++ src/common/functions/filterReleasesByGroups.ts
@@ -2,12 +2,13 @@ import { AlbumGroupName } from "../Types/AlbumGroupName";
 import { isMatch } from "./isMatch"
 
 export const filterReleasesByGroups = <ReleasedItem extends { album_group: AlbumGroupName }>(
-    releasesList:ReleasedItem[],
-    targetAlbumGroupsToFilterList:AlbumGroupName[],
+    releasesList: ReleasedItem[],
+    targetAlbumGroupsToFilterList: AlbumGroupName[],
 ) => {
-    return targetAlbumGroupsToFilterList.map((targetAlbumGroup) => {
-        return releasesList?.filter(
-            ({ album_group }) => isMatch(album_group, targetAlbumGroup)
-        )
-    });
+    const releasesGrouped = Object.fromEntries(targetAlbumGroupsToFilterList.map(groupName => [
+        groupName,
+        releasesList?.filter(({ album_group }) => isMatch(album_group, groupName))
+    ]));
+    
+    return releasesGrouped;
 };
