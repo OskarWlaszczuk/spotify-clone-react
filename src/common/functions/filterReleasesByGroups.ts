16c14339603999e18024ede221c3a6e8859f006a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-03-22 18:45:15 +0100

    In filterReleasesByGroups function: removed generic type and replaced it AlbumItem[] because releasesList parameter always gets array of albums.

--- src/common/functions/filterReleasesByGroups.ts
+++ src/common/functions/filterReleasesByGroups.ts
@@ -1,14 +1,13 @@
+import { AlbumItem } from "../Interfaces/AlbumItem";
 import { AlbumGroupName } from "../Types/AlbumGroupName";
 import { isMatch } from "./isMatch"
 
-export const filterReleasesByGroups = <ReleasedItem extends { album_group: AlbumGroupName }>(
-    releasesList: ReleasedItem[],
-    targetAlbumGroupsToFilterList: AlbumGroupName[],
-) => {
+export const filterReleasesByGroups = (releasesList: AlbumItem[], targetAlbumGroupsToFilterList: AlbumGroupName[]) => {
+    console.log(releasesList)
     const releasesGrouped = Object.fromEntries(targetAlbumGroupsToFilterList.map(groupName => [
         groupName,
         releasesList?.filter(({ album_group }) => isMatch(album_group, groupName))
     ]));
-    
+
     return releasesGrouped;
 };
