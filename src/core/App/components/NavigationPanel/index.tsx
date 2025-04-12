68938c21b36e67afdb04e4e3ce7e9beb706adc1f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-04-12 17:39:29 +0200

    Passed fullListType property with facetAllCategory value to toHome function in NavigationPanel

--- src/core/App/components/NavigationPanel/index.tsx
+++ src/core/App/components/NavigationPanel/index.tsx
@@ -3,13 +3,14 @@ import { toHome } from "../../../../common/functions/routes";
 import { IconBox } from "../../../../common/components/IconBox";
 import { Input } from "./Input";
 import { StyledHomeIcon } from "./StyledHomeIcon";
+import { facetAllCategory } from "../../../../features/homePage/constants/facetCategories";
 
 export const NavigationPanel = () => {
 
     return (
         <NavPanel>
             <IconBox
-                toPagePath={toHome()}
+                toPagePath={toHome({ fullListType: facetAllCategory })}
                 Icon={StyledHomeIcon}
             />
             <Input />
