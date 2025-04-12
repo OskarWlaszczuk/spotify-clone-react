b2bab28e149e13c7a0a8099a39d03a8696d03d97
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-04-12 15:45:29 +0200

    Renamed sortFromOldestToNewest function to orderByReleaseDateNewestFirst

--- src/common/functions/orderByReleaseDateNewestFirst.ts
+++ src/common/functions/orderByReleaseDateNewestFirst.ts
@@ -1,6 +1,6 @@
 import { WithReleaseDate } from "../Interfaces/WithReleaseDate";
 
-export const sortFromOldestToNewest = <T extends WithReleaseDate>(array: T[] = []): T[] => (
+export const orderByReleaseDateNewestFirst = <T extends WithReleaseDate>(array: T[] = []): T[] => (
     [...array].sort(
         (a, b) => Number(new Date(b.release_date)) - Number(new Date(a.release_date))
     )
