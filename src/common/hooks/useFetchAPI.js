5e3bfe043c21a629a46297a5badd8f984d354ce8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-04-12 15:55:22 +0200

    Call cleaning function inside useEffect of useFetch. Removed previous testing versions of useFetch hook

--- src/common/hooks/useFetchAPI.js
+++ src/common/hooks/useFetchAPI.js
@@ -1,108 +1,23 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAccessToken } from "../slices/authSlice";
 import { useApiResource } from "./useApiResource";
 
 export const useFetch = ({ actions, selectors, endpoint, fetchCondition = true }) => {
     const { resourceConfig, APIFetchStatus, APIData } = useApiResource({ actions, selectors, endpoint });
-
     const dispatch = useDispatch();
     const accessToken = useSelector(selectAccessToken);
 
     useEffect(() => {
         const { fetchAction, clearAction, endpoint } = resourceConfig;
 
-        if (fetchCondition && !!accessToken && !APIData) {
+        if (fetchCondition && !!accessToken) {
             dispatch(fetchAction({ endpoint, accessToken }));
         }
 
-        // return () => dispatch(clearAction());
+        return () => dispatch(clearAction());
 
-    }, [dispatch, accessToken, resourceConfig, fetchCondition, APIData]);
+    }, [dispatch, accessToken, resourceConfig, fetchCondition]);
 
     return { APIFetchStatus, APIData };
 };
-
-
-
-
-
-
-
-
-
-export const useFetchAPI = ({
-    fetchConfigs,
-    pageId,
-    dependencies = [],
-    fetchCondition = true
-}) => {
-    const dispatch = useDispatch();
-    const accessToken = useSelector(selectAccessToken);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // const memoizedFetchConfigs = useMemo(() => fetchConfigs, [pageId]);
-    // const combinedDependencies = [dispatch, accessToken, ...dependencies, fetchCondition];
-
-    useEffect(() => {
-        if (!!accessToken && fetchCondition) {
-            fetchConfigs?.forEach(({ fetchAction, endpoint }) => {
-                try {
-                    dispatch(fetchAction({ endpoint, accessToken }));
-                } catch (error) {
-                    console.error(`Failed to fetch data from endpoint: ${endpoint}`, error);
-                }
-            });
-        }
-
-        return () => {
-            fetchConfigs?.forEach(({ clearAction }) => dispatch(clearAction()));
-        };
-
-    }, []);
-};
-
-
-export const useFetchAPI2 = ({ fetchConfigs, fetchCondition = true, pageID, dependencies = [] }) => {
-    const dispatch = useDispatch();
-    const accessToken = useSelector(selectAccessToken);
-
-    const memoizedFetchConfigs = useMemo(() => fetchConfigs, [pageID]);
-
-    useEffect(() => {
-        if (!!accessToken && fetchCondition) {
-            memoizedFetchConfigs?.forEach(({ fetchAction, endpoint }) => {
-                dispatch(fetchAction({ endpoint, accessToken }));
-            });
-        }
-
-        return () => {
-            memoizedFetchConfigs?.forEach(({ clearAction }) => dispatch(clearAction()));
-        };
-    }, [accessToken, fetchCondition, memoizedFetchConfigs, dispatch, ...dependencies]);
-};
-
-
-// export const useFetchAPI3 = ({ fetchConfigs }) => {
-//     const dispatch = useDispatch();
-//     const accessToken = useSelector(selectAccessToken);
-
-//     if (!!accessToken && !!fetchConfigs) {
-//         fetchConfigs.forEach(({ fetchAction, endpoint }) => {
-//             dispatch(fetchAction({ endpoint, accessToken }))
-//         });
-//     }
-// }
-
-// export const useFetchAPI2 = () => {
-//     const dispatch = useDispatch();
-//     const accessToken = useSelector(selectAccessToken);
-
-//     const fetchAPI = async ({ fetchAction, clearAction, endpoint }) => {
-//         const response = await dispatch(fetchAction({ endpoint, accessToken }));
-
-//         return response.data;
-//     };
-
-//     return fetchAPI;
-// };
