addf1f6bde5c404034ffabc9f8d16c060f154a93
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-04-12 17:45:52 +0200

    Removed destructuring grouped releases returned by groupReleases function in divideReleases.

--- src/features/artistDetailsPage/functions/divideReleases.ts
+++ src/features/artistDetailsPage/functions/divideReleases.ts
@@ -1,15 +1,15 @@
-import { filterReleasesByGroups } from "../../../common/functions/filterReleasesByGroups";
+import { groupReleases } from "../../../common/functions/groupReleases";
 import { AlbumItem } from "../../../common/Interfaces/AlbumItem";
 
 export const divideReleases = (artistAllReleas: AlbumItem[]) => {
-    const [albumsList, compilationsList, singlesList, appearsOnList] = filterReleasesByGroups(
+    const releasesGrouped = groupReleases(
         artistAllReleas,
         ["album", "compilation", "single", "appears_on"]
     );
-   
+
     const allReleasesWithoutAppearsOn = artistAllReleas?.filter(
         ({ album_group }: any) => album_group !== "appears_on"
     );
 
-    return { albumsList, compilationsList, singlesList, appearsOnList, allReleasesWithoutAppearsOn };
+    return { releasesGrouped, allReleasesWithoutAppearsOn };
 };
