dbed953071f83ae5aaec1fecc0ca88c37df00590
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-04-12 17:33:43 +0200

    Refactor API response interfaces to use consistent naming convention and improve type definitions

--- src/common/interfaces/ApiResponse.ts
+++ src/common/interfaces/ApiResponse.ts
@@ -4,12 +4,12 @@ import { EpisodeItem } from "./EpisodeItem";
 import { ShowItem } from "./ShowItem";
 import { TrackItem } from "./TrackItem";
 
-export interface ApiResponse<ApiDataType> {
-    data: ApiDataType;
+export interface APIData<APIDataType> {
+    data: APIDataType;
 }
 
-export type ApiArtistResponse = ApiResponse<ArtistItem>;
-export type ApiAlbumResponse = ApiResponse<AlbumItem>;
-export type ApiTrackResponse = ApiResponse<TrackItem>;
-export type ApiShowResponse = ApiResponse<ShowItem>;
-export type ApiEpisodeResponse = ApiResponse<EpisodeItem>;  
+export type APIArtistData = APIData<ArtistItem>;
+export type APIAlbumData = APIData<AlbumItem>;
+export type APITrackData = APIData<TrackItem>;
+export type APIShowData = APIData<ShowItem>;
+export type APIEpisodeData = APIData<EpisodeItem>;  
